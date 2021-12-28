fff0da4378dd356ebb0cce40be02801500c91722
Christian Kaatz <christian.kaatz@digitalservice.bund.de>
2021-12-28 09:04:31 +0100

    Add latest version of the component library

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,42 +1,31 @@
-import { Button } from "@digitalservice4germany/digital-service-library";
+import {
+  Layout,
+  Footer,
+  Button,
+} from "@digitalservice4germany/digital-service-library";
 
 export default function Index() {
   return (
-    <div className="w-full flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow bg-gray-900">
-      <div className="w-fixed w-full flex-shrink flex-grow-0 px-4">
-        <div className="sticky top-0 p-4 w-full h-full">
+    <Layout
+      footer={<Footer> Footer </Footer>}
+      sidebarNavigation={
+        <div className="h-full p-4 bg-white">
           <ul>
-            <li>
-              <a
-                target="_blank"
-                href="https://remix.run/tutorials/blog"
-                rel="noreferrer"
-              >
-                15m Quickstart Blog Tutorial
-              </a>
-            </li>
-            <li>
-              <a
-                target="_blank"
-                href="https://remix.run/tutorials/jokes"
-                rel="noreferrer"
-              >
-                Deep Dive Jokes App Tutorial
-              </a>
-            </li>
-            <li>
-              <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-                Remix Docs
-              </a>
-            </li>
+            <li>Übersicht</li>
           </ul>
         </div>
-      </div>
-      <main role="main" className="w-full flex-grow pt-1 px-3">
+      }
+      topNavigation={
+        <div className="p-4 bg-blue-300">
+          topNavigation (hidden on larger screens)
+        </div>
+      }
+    >
+      <div className="bg-beige-100 h-full p-4">
         <h1>Welcome to Remix</h1>
 
-        <Button label="Hi" />
-      </main>
-    </div>
+        <Button label="Hi" onClick={() => console.log("clicked")} />
+      </div>
+    </Layout>
   );
 }
