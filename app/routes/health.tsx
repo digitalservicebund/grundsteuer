702cda43c616f819c4f6ab9243d8a6996c66e659
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-01-04 20:14:46 +0100

    Show dynamic status in health route.

--- app/routes/health.tsx
+++ app/routes/health.tsx
@@ -1,8 +1,24 @@
+import { LoaderFunction, useLoaderData } from "remix";
+
+type LoaderData = { status: "up" | "down" };
+
+const HOST = "http://localhost:3000";
+export const loader: LoaderFunction = async () => {
+  try {
+    return await fetch(HOST + "/api/status").then((data) => data.json());
+  } catch (e) {
+    return { status: "down" };
+  }
+};
+
 export default function Health() {
+  const data = useLoaderData<LoaderData>();
   return (
     <div className="text-center bg-beige-100 h-full p-4">
       <h1 className="text-xl">System Status</h1>
-      <p>Der Service ist operational.</p>
+      <p>
+        Aktueller Status: <span className="font-bold">{data.status}</span>
+      </p>
     </div>
   );
 }
