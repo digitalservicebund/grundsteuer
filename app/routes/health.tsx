c19c68ca8e6dd1a32d170971903f87ae47c6a84f
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-01-10 17:41:14 +0100

    Add db status to health route, remove redundant "server" status.

--- app/routes/health.tsx
+++ app/routes/health.tsx
@@ -1,23 +1,30 @@
 import { LoaderFunction, useLoaderData } from "remix";
+import { db } from "../db/db.server";
 
-type LoaderData = { status: "up" | "down" };
+type SystemStatus = { db: string };
 
 export const loader: LoaderFunction = async () => {
-  const HOST = process.env.SERVER_HOST + ":" + process.env.SERVER_PORT;
-  try {
-    return await fetch(HOST + "/api/status").then((data) => data.json());
-  } catch (e) {
-    return { status: "down" };
-  }
+  const dbStatus = await pingDb();
+  return { db: dbStatus };
 };
 
+export async function pingDb() {
+  try {
+    await db.$queryRaw`SELECT 1 AS result`;
+    return "up";
+  } catch (e) {
+    console.error(e);
+    return "down";
+  }
+}
+
 export default function Health() {
-  const data = useLoaderData<LoaderData>();
+  const status = useLoaderData<SystemStatus>();
   return (
     <div className="text-center bg-beige-100 h-full p-4">
       <h1 className="text-xl">System Status</h1>
       <p>
-        Aktueller Status: <span className="font-bold">{data.status}</span>
+        Database: <span className="font-bold">{status.db}</span>
       </p>
     </div>
   );
