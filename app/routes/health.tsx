a103aa5deba2377c32741ef47870dc959d3cc7a7
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-01-21 13:30:50 +0100

    Display environment in health route.

--- app/routes/health.tsx
+++ app/routes/health.tsx
@@ -1,10 +1,10 @@
 import { LoaderFunction, useLoaderData } from "remix";
 
-type SystemStatus = { db: string };
+type SystemStatus = { db: string; env: string };
 
 export const loader: LoaderFunction = async () => {
   const dbStatus = await pingDb();
-  return { db: dbStatus };
+  return { db: dbStatus, env: process.env.APP_ENV };
 };
 
 export async function pingDb() {
@@ -16,6 +16,9 @@ export default function Health() {
   return (
     <div className="text-center bg-beige-100 h-full p-4">
       <h1 className="text-xl">System Status</h1>
+      <p>
+        Env: <span className="font-bold">{status.env}</span>
+      </p>
       <p>
         Database: <span className="font-bold">{status.db}</span>
       </p>
