31850379fef4d4837e089d3eb902f1cd6932bd75
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-01-21 11:11:10 +0100

    Show value on second step

--- app/routes/steps/step2.tsx
+++ app/routes/steps/step2.tsx
@@ -1,10 +1,20 @@
 import { Link } from "@remix-run/react";
+import { LoaderFunction, useLoaderData } from "remix";
+import { getFormDataCookie } from "~/cookies";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return await getFormDataCookie(request);
+};
 
 export default function Step2() {
+  const formData = useLoaderData();
   return (
     <div className="bg-beige-100 h-full p-4">
       <h1 className="mb-4 font-bold">Step 2</h1>
-      <Link to="/steps/step1">Zurück</Link>
+      Street: {formData.street}
+      <Link to="/steps/step1" className="mt-4 block">
+        Zurück
+      </Link>
     </div>
   );
 }
