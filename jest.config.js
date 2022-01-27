a7f11fa9dae25e84786ff19f82f0d8556eba833d
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-01-27 15:58:34 +0100

    Fix module name mapping in jest.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  moduleDirectories: ["node_modules", "<rootDir>/app"],
+  moduleNameMapper: {
+    "~/(.*)": "<rootDir>/app/$1",
+  },
 };
