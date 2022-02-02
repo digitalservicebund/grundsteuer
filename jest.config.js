2c0927f4944715152ded2284fa3d2a5cc9b7f052
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-02-02 13:43:45 +0100

    Make tests faster

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: "ts-jest",
   testEnvironment: "node",
   moduleDirectories: ["node_modules", "<rootDir>/app"],
   moduleNameMapper: {
