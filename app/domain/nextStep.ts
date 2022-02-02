82b0f93997ad3da018d09ce547d79dd16f4346d1
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-02-02 12:24:52 +0100

    Trade in comment for more verbose+readable code

--- app/domain/nextStep.ts
+++ app/domain/nextStep.ts
@@ -2,15 +2,6 @@ import type { Config } from "~/stepConfig";
 
 export const finalStepName = "zusammenfassung";
 
-/** Decide which step a user should visit next.
- * Algorithm is simple (at the moment :D):
- *   1. go through all possible steps from start to end
- *   2. take the first step which is
- *     a) not yet done and
- *     b) has no condition which evaluates to false
- *
- * If no steps are found, all steps are done and next state is "zusammenfassung".
- */
 export const nextStep = ({
   config,
   records,
@@ -18,12 +9,17 @@ export const nextStep = ({
   config: Config;
   records: any;
 }) => {
+  const visitedStepNames = Object.keys(records || {});
+
   return (
     config.steps.find(({ name, condition }) => {
-      return !(
-        Object.keys(records || {}).includes(name) ||
-        (condition && !condition(records))
-      );
+      const visited = visitedStepNames.includes(name);
+      if (visited) return false;
+
+      const unfulfilledCondition = condition && !condition(records);
+      if (unfulfilledCondition) return false;
+
+      return true;
     })?.name || finalStepName
   );
 };
