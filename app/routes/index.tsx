21108b6b0d756f774f99fd96d8356bc632bbfd12
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-02-11 15:18:50 +0100

    Fix link

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,7 +5,10 @@ export default function Index() {
     <div className="bg-beige-100 h-full p-4">
       <h1 className="mb-8 text-4xl font-bold">Welcome to Remix</h1>
 
-      <Link to="/steps/adresse" className="text-2xl underline text-blue-500">
+      <Link
+        to="/steps/legacy/adresse"
+        className="text-2xl underline text-blue-500"
+      >
         Fragebogen
       </Link>
     </div>
