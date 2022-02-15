0560cbc64e440c8a34e676b4cb2a486c9fa8d35b
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-02-15 17:50:20 +0100

    Add custom action function to repeatable step

--- app/routes/steps/repeated/item/$id.name.tsx
+++ app/routes/steps/repeated/item/$id.name.tsx
@@ -1,10 +1,82 @@
-import { Form, useActionData, useLoaderData } from "remix";
+import {
+  ActionFunction,
+  Form,
+  redirect,
+  useActionData,
+  useLoaderData,
+} from "remix";
 import { Button } from "@digitalservice4germany/digital-service-library";
-import { ConfigStepField } from "~/domain";
+import { ConfigStepField, ConfigStepFieldValidation } from "~/domain";
 import { StepTextField } from "~/components";
-import { render } from "~/routes/steps/_step";
+import { getCurrentState, render } from "~/routes/steps/_step";
+import { createResponseHeaders, getFormDataCookie } from "~/cookies";
+import GrundDataModel, { StepFormData } from "~/domain/model";
+import { createMachine } from "xstate";
+import { getMachineConfig, getStateNodeByPath } from "~/domain/steps";
+import { conditions } from "~/domain/conditions";
+import { validateField } from "~/domain/validation";
 
-export { action, loader, handle } from "./../../_step";
+export { loader, handle } from "./../../_step";
+
+export const action: ActionFunction = async ({ request, params }) => {
+  console.log("ACTION");
+  const cookie = await getFormDataCookie(request);
+
+  let currentState = getCurrentState(request);
+  const fieldValues: StepFormData = Object.fromEntries(
+    await request.formData()
+  ) as StepFormData;
+
+  // validate step-model
+  const machineWithoutData = createMachine(getMachineConfig(null) as any, {
+    guards: conditions,
+  });
+
+  const errors: Record<string, Array<string>> = {};
+  const state = getStateNodeByPath(machineWithoutData, currentState);
+  if (state?.meta?.stepDefinition) {
+    state.meta.stepDefinition.fields.forEach(
+      (field: ConfigStepFieldValidation) => {
+        const fieldErrorMessages = validateField(field, fieldValues);
+        if (fieldErrorMessages.length > 0)
+          errors[field.name] = fieldErrorMessages;
+      }
+    );
+    if (Object.keys(errors).length >= 1) return { errors };
+  }
+
+  // Add data to bigger model
+  const completeDataModel = new GrundDataModel(cookie.records);
+  completeDataModel.setStepData(currentState, fieldValues);
+
+  // Add bigger model to cookie
+  cookie.records = completeDataModel.sections;
+
+  const machineWithData = machineWithoutData.withContext(cookie.records);
+
+  completeDataModel.sections.repeated.currentIndex =
+    params.id as unknown as number;
+  currentState = currentState.replace(/\.\d+/, "");
+  console.log(currentState);
+  const nextState = machineWithData.transition(
+    currentState,
+    {
+      type: "NEXT",
+    },
+    completeDataModel
+  ).value;
+  console.log({ nextState });
+
+  const redirectUrl =
+    "/steps/" +
+    JSON.stringify(nextState).replace(/:/g, "/").replace(/[{}"]/g, "");
+  console.log({ redirectUrl });
+
+  const responseHeader: Headers = await createResponseHeaders(cookie);
+  return redirect(redirectUrl, {
+    headers: responseHeader,
+  });
+};
 
 export default function ItemName() {
   const { formData } = useLoaderData();
