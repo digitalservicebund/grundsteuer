6f02d377665918df240ea78181763010c35a56ff
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-02-18 14:31:37 +0100

    Also rename top level formular file

--- app/routes/formular.tsx
+++ app/routes/formular.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "remix";
 
-export default function Steps() {
+export default function Formular() {
   return (
     <div className="max-w-screen-md mx-auto p-16">
       <Outlet />
