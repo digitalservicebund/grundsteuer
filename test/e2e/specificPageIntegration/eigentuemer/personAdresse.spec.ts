4a990b67cddfd1f789ed3266e9505f2385491509
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-02-22 12:26:49 +0100

    Add tests for prev and next step of person adresse step

---
+++ test/e2e/specificPageIntegration/eigentuemer/personAdresse.spec.ts
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+import { submitBtnSelector } from "../../happypath.spec";
+
+describe("Person adresse route", () => {
+  describe("Without data set", () => {
+    beforeEach(() => {
+      cy.visit("/formular/eigentuemer/person/1/adresse");
+    });
+
+    it("Next page should be person telefonnummer", () => {
+      cy.get(submitBtnSelector).click();
+      cy.url().should(
+        "include",
+        "/formular/eigentuemer/person/1/telefonnummer"
+      );
+    });
+
+    it("Previous page should be person name", () => {
+      cy.get("a").contains("Zurück").click();
+      cy.url().should("include", "/formular/eigentuemer/person/1/name");
+    });
+  });
+});
