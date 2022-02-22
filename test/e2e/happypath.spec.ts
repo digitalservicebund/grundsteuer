19629fad0f790cbded95a8c273551cc02ad28f7f
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-02-22 12:23:40 +0100

    Add tests for prev and next step of person name step

--- test/e2e/happypath.spec.ts
+++ test/e2e/happypath.spec.ts
@@ -91,216 +91,225 @@ const inputData = {
   },
 };
 
-const submitBtnSelector = "#nextButton";
+export const submitBtnSelector = "#nextButton";
 
-it("Enter data for two eigentuemer", () => {
-  cy.visit("/formular/eigentuemer/anzahl");
-  cy.get("#anzahl").clear().type(inputData.eigentuemer.anzahl.anzahl);
-  cy.get(submitBtnSelector).click();
-  cy.get("label[for=areVerheiratet-true]").click();
-  cy.get(submitBtnSelector).click();
-  // PERSON 1
-  cy.get("#anrede").clear().type(inputData.eigentuemer.person1.name.anrede);
-  cy.get("#titel").clear().type(inputData.eigentuemer.person1.name.titel);
-  cy.get("#name").clear().type(inputData.eigentuemer.person1.name.name);
-  cy.get("#vorname").clear().type(inputData.eigentuemer.person1.name.vorname);
-  cy.get(submitBtnSelector).click();
-  cy.get("#strasse")
-    .clear()
-    .type(inputData.eigentuemer.person1.adresse.strasse);
-  cy.get("#hausnummer")
-    .clear()
-    .type(inputData.eigentuemer.person1.adresse.hausnummer);
-  cy.get("#plz").clear().type(inputData.eigentuemer.person1.adresse.plz);
-  cy.get("#ort").clear().type(inputData.eigentuemer.person1.adresse.ort);
-  cy.get(submitBtnSelector).click();
-  cy.get("#telefonnummer")
-    .clear()
-    .type(inputData.eigentuemer.person1.telefonnummer.telefonnummer);
-  cy.get(submitBtnSelector).click();
-  cy.get("#steuerId")
-    .clear()
-    .type(inputData.eigentuemer.person1.steuerId.steuerId);
-  cy.get(submitBtnSelector).click();
-  cy.get("#hasVertreter-true").click();
-  cy.get(submitBtnSelector).click();
-  cy.get("#anrede")
-    .clear()
-    .type(inputData.eigentuemer.person1.vertreter.name.anrede);
-  cy.get("#titel")
-    .clear()
-    .type(inputData.eigentuemer.person1.vertreter.name.titel);
-  cy.get("#name")
-    .clear()
-    .type(inputData.eigentuemer.person1.vertreter.name.name);
-  cy.get("#vorname")
-    .clear()
-    .type(inputData.eigentuemer.person1.vertreter.name.vorname);
-  cy.get(submitBtnSelector).click();
-  cy.get("#strasse")
-    .clear()
-    .type(inputData.eigentuemer.person1.vertreter.adresse.strasse);
-  cy.get("#hausnummer")
-    .clear()
-    .type(inputData.eigentuemer.person1.vertreter.adresse.hausnummer);
-  cy.get("#plz")
-    .clear()
-    .type(inputData.eigentuemer.person1.vertreter.adresse.plz);
-  cy.get("#ort")
-    .clear()
-    .type(inputData.eigentuemer.person1.vertreter.adresse.ort);
-  cy.get(submitBtnSelector).click();
-  cy.get("#telefonnummer")
-    .clear()
-    .type(inputData.eigentuemer.person1.vertreter.telefonnummer.telefonnummer);
-  cy.get(submitBtnSelector).click();
-  cy.get("#zaehler").clear().type(inputData.eigentuemer.person1.anteil.zaehler);
-  cy.get("#nenner").clear().type(inputData.eigentuemer.person1.anteil.nenner);
-  cy.get(submitBtnSelector).click();
+describe("Happy Path", () => {
+  it("Enter data for two eigentuemer", () => {
+    cy.visit("/formular/eigentuemer/anzahl");
+    cy.get("#anzahl").clear().type(inputData.eigentuemer.anzahl.anzahl);
+    cy.get(submitBtnSelector).click();
+    cy.get("label[for=areVerheiratet-true]").click();
+    cy.get(submitBtnSelector).click();
+    // PERSON 1
+    cy.get("#anrede").clear().type(inputData.eigentuemer.person1.name.anrede);
+    cy.get("#titel").clear().type(inputData.eigentuemer.person1.name.titel);
+    cy.get("#name").clear().type(inputData.eigentuemer.person1.name.name);
+    cy.get("#vorname").clear().type(inputData.eigentuemer.person1.name.vorname);
+    cy.get(submitBtnSelector).click();
+    cy.get("#strasse")
+      .clear()
+      .type(inputData.eigentuemer.person1.adresse.strasse);
+    cy.get("#hausnummer")
+      .clear()
+      .type(inputData.eigentuemer.person1.adresse.hausnummer);
+    cy.get("#plz").clear().type(inputData.eigentuemer.person1.adresse.plz);
+    cy.get("#ort").clear().type(inputData.eigentuemer.person1.adresse.ort);
+    cy.get(submitBtnSelector).click();
+    cy.get("#telefonnummer")
+      .clear()
+      .type(inputData.eigentuemer.person1.telefonnummer.telefonnummer);
+    cy.get(submitBtnSelector).click();
+    cy.get("#steuerId")
+      .clear()
+      .type(inputData.eigentuemer.person1.steuerId.steuerId);
+    cy.get(submitBtnSelector).click();
+    cy.get("#hasVertreter-true").click();
+    cy.get(submitBtnSelector).click();
+    cy.get("#anrede")
+      .clear()
+      .type(inputData.eigentuemer.person1.vertreter.name.anrede);
+    cy.get("#titel")
+      .clear()
+      .type(inputData.eigentuemer.person1.vertreter.name.titel);
+    cy.get("#name")
+      .clear()
+      .type(inputData.eigentuemer.person1.vertreter.name.name);
+    cy.get("#vorname")
+      .clear()
+      .type(inputData.eigentuemer.person1.vertreter.name.vorname);
+    cy.get(submitBtnSelector).click();
+    cy.get("#strasse")
+      .clear()
+      .type(inputData.eigentuemer.person1.vertreter.adresse.strasse);
+    cy.get("#hausnummer")
+      .clear()
+      .type(inputData.eigentuemer.person1.vertreter.adresse.hausnummer);
+    cy.get("#plz")
+      .clear()
+      .type(inputData.eigentuemer.person1.vertreter.adresse.plz);
+    cy.get("#ort")
+      .clear()
+      .type(inputData.eigentuemer.person1.vertreter.adresse.ort);
+    cy.get(submitBtnSelector).click();
+    cy.get("#telefonnummer")
+      .clear()
+      .type(
+        inputData.eigentuemer.person1.vertreter.telefonnummer.telefonnummer
+      );
+    cy.get(submitBtnSelector).click();
+    cy.get("#zaehler")
+      .clear()
+      .type(inputData.eigentuemer.person1.anteil.zaehler);
+    cy.get("#nenner").clear().type(inputData.eigentuemer.person1.anteil.nenner);
+    cy.get(submitBtnSelector).click();
 
-  // PERSON 2
-  cy.url().should("include", "/formular/eigentuemer/person/2/");
-  cy.get("#anrede").clear().type(inputData.eigentuemer.person2.name.anrede);
-  cy.get("#titel").clear().type(inputData.eigentuemer.person2.name.titel);
-  cy.get("#name").clear().type(inputData.eigentuemer.person2.name.name);
-  cy.get("#vorname").clear().type(inputData.eigentuemer.person2.name.vorname);
-  cy.get(submitBtnSelector).click();
-  cy.get("#strasse")
-    .clear()
-    .type(inputData.eigentuemer.person2.adresse.strasse);
-  cy.get("#hausnummer")
-    .clear()
-    .type(inputData.eigentuemer.person2.adresse.hausnummer);
-  cy.get("#zusatzangaben")
-    .clear()
-    .type(inputData.eigentuemer.person2.adresse.zusatzangaben);
-  cy.get("#plz").clear().type(inputData.eigentuemer.person2.adresse.plz);
-  cy.get("#ort").clear().type(inputData.eigentuemer.person2.adresse.ort);
-  cy.get(submitBtnSelector).click();
-  cy.get("#telefonnummer")
-    .clear()
-    .type(inputData.eigentuemer.person2.telefonnummer.telefonnummer);
-  cy.get(submitBtnSelector).click();
-  cy.get("#steuerId")
-    .clear()
-    .type(inputData.eigentuemer.person2.steuerId.steuerId);
-  cy.get(submitBtnSelector).click();
-  cy.get("#hasVertreter-false").click();
-  cy.get(submitBtnSelector).click();
-  cy.get("#zaehler").clear().type(inputData.eigentuemer.person2.anteil.zaehler);
-  cy.get("#nenner").clear().type(inputData.eigentuemer.person2.anteil.nenner);
-  cy.get(submitBtnSelector).click();
+    // PERSON 2
+    cy.url().should("include", "/formular/eigentuemer/person/2/");
+    cy.get("#anrede").clear().type(inputData.eigentuemer.person2.name.anrede);
+    cy.get("#titel").clear().type(inputData.eigentuemer.person2.name.titel);
+    cy.get("#name").clear().type(inputData.eigentuemer.person2.name.name);
+    cy.get("#vorname").clear().type(inputData.eigentuemer.person2.name.vorname);
+    cy.get(submitBtnSelector).click();
+    cy.get("#strasse")
+      .clear()
+      .type(inputData.eigentuemer.person2.adresse.strasse);
+    cy.get("#hausnummer")
+      .clear()
+      .type(inputData.eigentuemer.person2.adresse.hausnummer);
+    cy.get("#zusatzangaben")
+      .clear()
+      .type(inputData.eigentuemer.person2.adresse.zusatzangaben);
+    cy.get("#plz").clear().type(inputData.eigentuemer.person2.adresse.plz);
+    cy.get("#ort").clear().type(inputData.eigentuemer.person2.adresse.ort);
+    cy.get(submitBtnSelector).click();
+    cy.get("#telefonnummer")
+      .clear()
+      .type(inputData.eigentuemer.person2.telefonnummer.telefonnummer);
+    cy.get(submitBtnSelector).click();
+    cy.get("#steuerId")
+      .clear()
+      .type(inputData.eigentuemer.person2.steuerId.steuerId);
+    cy.get(submitBtnSelector).click();
+    cy.get("#hasVertreter-false").click();
+    cy.get(submitBtnSelector).click();
+    cy.get("#zaehler")
+      .clear()
+      .type(inputData.eigentuemer.person2.anteil.zaehler);
+    cy.get("#nenner").clear().type(inputData.eigentuemer.person2.anteil.nenner);
+    cy.get(submitBtnSelector).click();
 
-  // GRUNDSTUECK
-  cy.url().should("include", "/formular/grundstueck");
+    // GRUNDSTUECK
+    cy.url().should("include", "/formular/grundstueck");
 
-  // ZUSAMMENFASSUNG
-  cy.visit("/formular/zusammenfassung");
-  cy.contains("Anzahl: " + inputData.eigentuemer.anzahl.anzahl);
-  cy.contains("Verheiratet: Ja");
-  cy.get("#person-0").contains(
-    "Anrede: " + inputData.eigentuemer.person1.name.anrede
-  );
-  cy.get("#person-0").contains(
-    "Titel: " + inputData.eigentuemer.person1.name.titel
-  );
-  cy.get("#person-0").contains(
-    "Name: " + inputData.eigentuemer.person1.name.name
-  );
-  cy.get("#person-0").contains(
-    "Vorname: " + inputData.eigentuemer.person1.name.vorname
-  );
-  cy.get("#person-0").contains(
-    "Straße: " + inputData.eigentuemer.person1.adresse.strasse
-  );
-  cy.get("#person-0").contains(
-    "Hausnummer: " + inputData.eigentuemer.person1.adresse.hausnummer
-  );
-  cy.get("#person-0").contains(
-    "PLZ: " + inputData.eigentuemer.person1.adresse.plz
-  );
-  cy.get("#person-0").contains(
-    "Ort: " + inputData.eigentuemer.person1.adresse.ort
-  );
-  cy.get("#person-0").contains(
-    "Telefonnummer: " +
-      inputData.eigentuemer.person1.telefonnummer.telefonnummer
-  );
-  cy.get("#person-0").contains(
-    "Steuer-ID: " + inputData.eigentuemer.person1.steuerId.steuerId
-  );
-  cy.get("#person-0").contains("Gesetzlicher Vertreter: Ja");
-  cy.get("#person-0-vertreter").contains(
-    "Anrede: " + inputData.eigentuemer.person1.vertreter.name.anrede
-  );
-  cy.get("#person-0-vertreter").contains(
-    "Titel: " + inputData.eigentuemer.person1.vertreter.name.titel
-  );
-  cy.get("#person-0-vertreter").contains(
-    "Name: " + inputData.eigentuemer.person1.vertreter.name.name
-  );
-  cy.get("#person-0-vertreter").contains(
-    "Vorname: " + inputData.eigentuemer.person1.vertreter.name.vorname
-  );
-  cy.get("#person-0-vertreter").contains(
-    "Straße: " + inputData.eigentuemer.person1.vertreter.adresse.strasse
-  );
-  cy.get("#person-0-vertreter").contains(
-    "Hausnummer: " + inputData.eigentuemer.person1.vertreter.adresse.hausnummer
-  );
-  cy.get("#person-0-vertreter").contains(
-    "PLZ: " + inputData.eigentuemer.person1.vertreter.adresse.plz
-  );
-  cy.get("#person-0-vertreter").contains(
-    "Ort: " + inputData.eigentuemer.person1.vertreter.adresse.ort
-  );
-  cy.get("#person-0-vertreter").contains(
-    "Telefonnummer: " +
-      inputData.eigentuemer.person1.vertreter.telefonnummer.telefonnummer
-  );
-  cy.get("#person-0").contains(
-    "Anteil Zähler: " + inputData.eigentuemer.person1.anteil.zaehler
-  );
-  cy.get("#person-0").contains(
-    "Anteil Nenner: " + inputData.eigentuemer.person1.anteil.nenner
-  );
+    // ZUSAMMENFASSUNG
+    cy.visit("/formular/zusammenfassung");
+    cy.contains("Anzahl: " + inputData.eigentuemer.anzahl.anzahl);
+    cy.contains("Verheiratet: Ja");
+    cy.get("#person-0").contains(
+      "Anrede: " + inputData.eigentuemer.person1.name.anrede
+    );
+    cy.get("#person-0").contains(
+      "Titel: " + inputData.eigentuemer.person1.name.titel
+    );
+    cy.get("#person-0").contains(
+      "Name: " + inputData.eigentuemer.person1.name.name
+    );
+    cy.get("#person-0").contains(
+      "Vorname: " + inputData.eigentuemer.person1.name.vorname
+    );
+    cy.get("#person-0").contains(
+      "Straße: " + inputData.eigentuemer.person1.adresse.strasse
+    );
+    cy.get("#person-0").contains(
+      "Hausnummer: " + inputData.eigentuemer.person1.adresse.hausnummer
+    );
+    cy.get("#person-0").contains(
+      "PLZ: " + inputData.eigentuemer.person1.adresse.plz
+    );
+    cy.get("#person-0").contains(
+      "Ort: " + inputData.eigentuemer.person1.adresse.ort
+    );
+    cy.get("#person-0").contains(
+      "Telefonnummer: " +
+        inputData.eigentuemer.person1.telefonnummer.telefonnummer
+    );
+    cy.get("#person-0").contains(
+      "Steuer-ID: " + inputData.eigentuemer.person1.steuerId.steuerId
+    );
+    cy.get("#person-0").contains("Gesetzlicher Vertreter: Ja");
+    cy.get("#person-0-vertreter").contains(
+      "Anrede: " + inputData.eigentuemer.person1.vertreter.name.anrede
+    );
+    cy.get("#person-0-vertreter").contains(
+      "Titel: " + inputData.eigentuemer.person1.vertreter.name.titel
+    );
+    cy.get("#person-0-vertreter").contains(
+      "Name: " + inputData.eigentuemer.person1.vertreter.name.name
+    );
+    cy.get("#person-0-vertreter").contains(
+      "Vorname: " + inputData.eigentuemer.person1.vertreter.name.vorname
+    );
+    cy.get("#person-0-vertreter").contains(
+      "Straße: " + inputData.eigentuemer.person1.vertreter.adresse.strasse
+    );
+    cy.get("#person-0-vertreter").contains(
+      "Hausnummer: " +
+        inputData.eigentuemer.person1.vertreter.adresse.hausnummer
+    );
+    cy.get("#person-0-vertreter").contains(
+      "PLZ: " + inputData.eigentuemer.person1.vertreter.adresse.plz
+    );
+    cy.get("#person-0-vertreter").contains(
+      "Ort: " + inputData.eigentuemer.person1.vertreter.adresse.ort
+    );
+    cy.get("#person-0-vertreter").contains(
+      "Telefonnummer: " +
+        inputData.eigentuemer.person1.vertreter.telefonnummer.telefonnummer
+    );
+    cy.get("#person-0").contains(
+      "Anteil Zähler: " + inputData.eigentuemer.person1.anteil.zaehler
+    );
+    cy.get("#person-0").contains(
+      "Anteil Nenner: " + inputData.eigentuemer.person1.anteil.nenner
+    );
 
-  cy.get("#person-1").contains(
-    "Anrede: " + inputData.eigentuemer.person2.name.anrede
-  );
-  cy.get("#person-1").contains(
-    "Titel: " + inputData.eigentuemer.person2.name.titel
-  );
-  cy.get("#person-1").contains(
-    "Name: " + inputData.eigentuemer.person2.name.name
-  );
-  cy.get("#person-1").contains(
-    "Vorname: " + inputData.eigentuemer.person2.name.vorname
-  );
-  cy.get("#person-1").contains(
-    "Straße: " + inputData.eigentuemer.person2.adresse.strasse
-  );
-  cy.get("#person-1").contains(
-    "Hausnummer: " + inputData.eigentuemer.person2.adresse.hausnummer
-  );
-  cy.get("#person-1").contains(
-    "PLZ: " + inputData.eigentuemer.person2.adresse.plz
-  );
-  cy.get("#person-1").contains(
-    "Ort: " + inputData.eigentuemer.person2.adresse.ort
-  );
-  cy.get("#person-1").contains(
-    "Telefonnummer: " +
-      inputData.eigentuemer.person2.telefonnummer.telefonnummer
-  );
-  cy.get("#person-1").contains(
-    "Steuer-ID: " + inputData.eigentuemer.person2.steuerId.steuerId
-  );
-  cy.get("#person-1").contains("Gesetzlicher Vertreter: Nein");
-  cy.get("#person-1").contains(
-    "Anteil Zähler: " + inputData.eigentuemer.person2.anteil.zaehler
-  );
-  cy.get("#person-1").contains(
-    "Anteil Nenner: " + inputData.eigentuemer.person2.anteil.nenner
-  );
+    cy.get("#person-1").contains(
+      "Anrede: " + inputData.eigentuemer.person2.name.anrede
+    );
+    cy.get("#person-1").contains(
+      "Titel: " + inputData.eigentuemer.person2.name.titel
+    );
+    cy.get("#person-1").contains(
+      "Name: " + inputData.eigentuemer.person2.name.name
+    );
+    cy.get("#person-1").contains(
+      "Vorname: " + inputData.eigentuemer.person2.name.vorname
+    );
+    cy.get("#person-1").contains(
+      "Straße: " + inputData.eigentuemer.person2.adresse.strasse
+    );
+    cy.get("#person-1").contains(
+      "Hausnummer: " + inputData.eigentuemer.person2.adresse.hausnummer
+    );
+    cy.get("#person-1").contains(
+      "PLZ: " + inputData.eigentuemer.person2.adresse.plz
+    );
+    cy.get("#person-1").contains(
+      "Ort: " + inputData.eigentuemer.person2.adresse.ort
+    );
+    cy.get("#person-1").contains(
+      "Telefonnummer: " +
+        inputData.eigentuemer.person2.telefonnummer.telefonnummer
+    );
+    cy.get("#person-1").contains(
+      "Steuer-ID: " + inputData.eigentuemer.person2.steuerId.steuerId
+    );
+    cy.get("#person-1").contains("Gesetzlicher Vertreter: Nein");
+    cy.get("#person-1").contains(
+      "Anteil Zähler: " + inputData.eigentuemer.person2.anteil.zaehler
+    );
+    cy.get("#person-1").contains(
+      "Anteil Nenner: " + inputData.eigentuemer.person2.anteil.nenner
+    );
+  });
 });
---
+++ test/e2e/specificPageIntegration/eigentuemer/personName.spec.ts
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+import { submitBtnSelector } from "../../happypath.spec";
+
+describe("Person name route", () => {
+  describe("Without data set", () => {
+    beforeEach(() => {
+      cy.visit("/formular/eigentuemer/person/1/name");
+    });
+
+    it("Next page should be person adresse", () => {
+      cy.get(submitBtnSelector).click();
+      cy.url().should("include", "/formular/eigentuemer/person/1/adresse");
+    });
+
+    it("Previous page should be anzahl eigentuemer", () => {
+      cy.get("a").contains("Zurück").click();
+      cy.url().should("include", "/formular/eigentuemer/anzahl");
+    });
+  });
+
+  describe("With two eigentuemer set", () => {
+    beforeEach(() => {
+      cy.visit("/formular/eigentuemer/anzahl");
+      cy.get("#anzahl").clear().type("2");
+      cy.get(submitBtnSelector).click();
+      cy.visit("/formular/eigentuemer/person/1/name");
+    });
+
+    it("Next page should be person adresse", () => {
+      cy.get(submitBtnSelector).click();
+      cy.url().should("include", "/formular/eigentuemer/person/1/adresse");
+    });
+
+    it("Previous page should be verheiratet", () => {
+      cy.get("a").contains("Zurück").click();
+      cy.url().should("include", "/formular/eigentuemer/verheiratet");
+    });
+  });
+});
