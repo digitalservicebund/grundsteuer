c05e4ac0ab5c408ddd91a7865dc6ada00d732543
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-02-26 17:30:12 +0100

    Allow other pages to have a different layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,70 +1,37 @@
 import {
-  json,
   Links,
   LinksFunction,
   LiveReload,
-  LoaderFunction,
   Meta,
   MetaFunction,
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
-  useMatches,
 } from "remix";
-import {
-  Footer,
-  Layout,
-} from "@digitalservice4germany/digital-service-library";
 import { useRemixI18Next } from "remix-i18next";
-import { i18Next } from "~/i18n.server";
-import { getFormDataCookie } from "~/cookies";
-import SidebarNavigation from "~/components/SidebarNavigation";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: "/tailwind.css" }];
 };
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
-
-export const loader: LoaderFunction = async ({ request }) => {
-  const cookie = await getFormDataCookie(request);
-  return json({
-    data: cookie.records,
-    i18n: await i18Next.getTranslations(request, ["common"]),
-  });
+  return { title: "Grundsteuererklärung für Privateigentum" };
 };
 
 export default function App() {
-  const matchingRoutes = useMatches();
-  const data = useLoaderData().data;
   useRemixI18Next("de");
 
   return (
     <html lang="de">
       <head>
         <meta charSet="utf-8" />
-        <title>Grundsteuer</title>
+        <title>Grundsteuererklärung für Privateigentum</title>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        <Layout
-          footer={<Footer> Footer </Footer>}
-          sidebarNavigation={
-            <SidebarNavigation matchingRoutes={matchingRoutes} data={data} />
-          }
-          topNavigation={
-            <div className="p-4 bg-blue-300">
-              topNavigation (hidden on larger screens)
-            </div>
-          }
-        >
-          <Outlet />
-        </Layout>
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/formular.tsx
+++ app/routes/formular.tsx
@@ -1,9 +1,38 @@
-import { Outlet } from "remix";
+import { json, LoaderFunction, Outlet, useLoaderData, useMatches } from "remix";
+import {
+  Footer,
+  Layout,
+} from "@digitalservice4germany/digital-service-library";
+import { i18Next } from "~/i18n.server";
+import { getFormDataCookie } from "~/cookies";
+import SidebarNavigation from "~/components/SidebarNavigation";
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const cookie = await getFormDataCookie(request);
+  return json({
+    data: cookie.records,
+    i18n: await i18Next.getTranslations(request, ["common"]),
+  });
+};
 
 export default function Formular() {
+  const matchingRoutes = useMatches();
+  const data = useLoaderData().data;
   return (
-    <div className="max-w-screen-md mx-auto p-16">
-      <Outlet />
-    </div>
+    <Layout
+      footer={<Footer> Footer </Footer>}
+      sidebarNavigation={
+        <SidebarNavigation matchingRoutes={matchingRoutes} data={data} />
+      }
+      topNavigation={
+        <div className="p-4 bg-blue-300">
+          <SidebarNavigation matchingRoutes={matchingRoutes} data={data} />
+        </div>
+      }
+    >
+      <div className="max-w-screen-md mx-auto p-16">
+        <Outlet />
+      </div>
+    </Layout>
   );
 }
