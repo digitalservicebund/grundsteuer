b3a848dcff839e154b730592bc9adae1e3bf225f
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-03-01 17:36:02 +0100

    Attempt to display personal data only if the required field exists.

--- app/routes/formular/zusammenfassung.tsx
+++ app/routes/formular/zusammenfassung.tsx
@@ -39,74 +39,85 @@ export default function Zusammenfassung() {
             )}
           </ul>
           {data.eigentuemer.person && (
-            <h3 className="font-bold text-xl mb-1">Personen</h3>
+            <>
+              <h3 className="font-bold text-xl mb-1">Personen</h3>
+
+              {data.eigentuemer.person.map((person: any, index: number) => {
+                const personKey = "person-" + index;
+                return (
+                  <div
+                    className="bg-gray-100 mb-3"
+                    key={personKey}
+                    id={personKey}
+                  >
+                    <h4 className="font-bold">Person {index + 1}</h4>
+                    <ul>
+                      <li>Anrede: {person.persoenlicheAngaben?.anrede}</li>
+                      <li>Titel: {person.persoenlicheAngaben?.titel}</li>
+                      <li>Name: {person.persoenlicheAngaben?.name}</li>
+                      <li>Vorname: {person.persoenlicheAngaben?.vorname}</li>
+                      <li>
+                        Geburtsdatum: {person.persoenlicheAngaben?.geburtsdatum}
+                      </li>
+                      <li>Straße: {person.adresse?.strasse}</li>
+                      <li>Hausnummer: {person.adresse?.hausnummer}</li>
+                      <li>Ort: {person.adresse?.ort}</li>
+                      <li>PLZ: {person.adresse?.plz}</li>
+                      <li>Zusatzangaben: {person.adresse?.zusatzangaben} </li>
+                      <li>Postfach: {person.adresse?.postfach}</li>
+                      <li>
+                        Telefonnummer: {person.telefonnummer?.telefonnummer}
+                      </li>
+                      <li>Steuer-ID: {person.steuerId?.steuerId}</li>
+                      <li>
+                        Gesetzlicher Vertreter:{" "}
+                        {person.gesetzlicherVertreter?.hasVertreter === "true"
+                          ? "Ja"
+                          : "Nein"}{" "}
+                      </li>
+                      {person.vertreter && (
+                        <div
+                          className="bg-gray-300 mx-4"
+                          id={personKey + "-vertreter"}
+                        >
+                          <h5 className="font-bold">Gesetzlicher Vertreter</h5>
+                          <ul>
+                            <li>Anrede: {person.vertreter.name?.anrede}</li>
+                            <li>Titel: {person.vertreter.name?.titel}</li>
+                            <li>Name: {person.vertreter.name?.name}</li>
+                            <li>Vorname: {person.vertreter.name?.vorname}</li>
+                            <li>Straße: {person.vertreter.adresse?.strasse}</li>
+                            <li>
+                              Hausnummer: {person.vertreter.adresse?.hausnummer}
+                            </li>
+                            <li>
+                              Zusatzangaben:{" "}
+                              {person.vertreter.adresse?.zusatzangaben}
+                            </li>
+                            <li>
+                              Postfach: {person.vertreter.adresse?.postfach}
+                            </li>
+                            <li>PLZ: {person.vertreter.adresse?.plz}</li>
+                            <li>Ort: {person.vertreter.adresse?.ort}</li>
+                            <li>
+                              Telefonnummer:{" "}
+                              {person.vertreter.telefonnummer?.telefonnummer}
+                            </li>
+                          </ul>
+                        </div>
+                      )}
+                      {person.anteil && (
+                        <>
+                          <li>Anteil Zähler: {person.anteil.zaehler}</li>
+                          <li>Anteil Nenner: {person.anteil.nenner}</li>
+                        </>
+                      )}
+                    </ul>
+                  </div>
+                );
+              })}
+            </>
           )}
-          {data.eigentuemer.person.map((person: any, index: number) => {
-            const personKey = "person-" + index;
-            return (
-              <div className="bg-gray-100 mb-3" key={personKey} id={personKey}>
-                <h4 className="font-bold">Person {index + 1}</h4>
-                <ul>
-                  <li>Anrede: {person.persoenlicheAngaben?.anrede}</li>
-                  <li>Titel: {person.persoenlicheAngaben?.titel}</li>
-                  <li>Name: {person.persoenlicheAngaben?.name}</li>
-                  <li>Vorname: {person.persoenlicheAngaben?.vorname}</li>
-                  <li>
-                    Geburtsdatum: {person.persoenlicheAngaben?.geburtsdatum}
-                  </li>
-                  <li>Straße: {person.adresse?.strasse}</li>
-                  <li>Hausnummer: {person.adresse?.hausnummer}</li>
-                  <li>Ort: {person.adresse?.ort}</li>
-                  <li>PLZ: {person.adresse?.plz}</li>
-                  <li>Zusatzangaben: {person.adresse?.zusatzangaben} </li>
-                  <li>Postfach: {person.adresse?.postfach}</li>
-                  <li>Telefonnummer: {person.telefonnummer?.telefonnummer}</li>
-                  <li>Steuer-ID: {person.steuerId?.steuerId}</li>
-                  <li>
-                    Gesetzlicher Vertreter:{" "}
-                    {person.gesetzlicherVertreter?.hasVertreter === "true"
-                      ? "Ja"
-                      : "Nein"}{" "}
-                  </li>
-                  {person.vertreter && (
-                    <div
-                      className="bg-gray-300 mx-4"
-                      id={personKey + "-vertreter"}
-                    >
-                      <h5 className="font-bold">Gesetzlicher Vertreter</h5>
-                      <ul>
-                        <li>Anrede: {person.vertreter.name?.anrede}</li>
-                        <li>Titel: {person.vertreter.name?.titel}</li>
-                        <li>Name: {person.vertreter.name?.name}</li>
-                        <li>Vorname: {person.vertreter.name?.vorname}</li>
-                        <li>Straße: {person.vertreter.adresse?.strasse}</li>
-                        <li>
-                          Hausnummer: {person.vertreter.adresse?.hausnummer}
-                        </li>
-                        <li>
-                          Zusatzangaben:{" "}
-                          {person.vertreter.adresse?.zusatzangaben}
-                        </li>
-                        <li>Postfach: {person.vertreter.adresse?.postfach}</li>
-                        <li>PLZ: {person.vertreter.adresse?.plz}</li>
-                        <li>Ort: {person.vertreter.adresse?.ort}</li>
-                        <li>
-                          Telefonnummer:{" "}
-                          {person.vertreter.telefonnummer?.telefonnummer}
-                        </li>
-                      </ul>
-                    </div>
-                  )}
-                  {person.anteil && (
-                    <>
-                      <li>Anteil Zähler: {person.anteil.zaehler}</li>
-                      <li>Anteil Nenner: {person.anteil.nenner}</li>
-                    </>
-                  )}
-                </ul>
-              </div>
-            );
-          })}
         </>
       )}
       <pre className="max-w-screen-md overflow-hidden">
