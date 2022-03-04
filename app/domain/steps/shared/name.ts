9de884e3bf59995d9a012b088ae6913ccfc3159d
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-03-04 09:49:35 +0100

    Add anrede todo comment

--- app/domain/steps/shared/name.ts
+++ app/domain/steps/shared/name.ts
@@ -9,6 +9,7 @@ export type NameFields = {
 
 export const stepDefinition: StepDefinition = {
   fields: {
+    // TODO when Anrede becomes a dropdown: use these values: 'no_anrede' ,'herr', 'frau' to be compatible with erica
     anrede: { validations: {} },
     titel: { validations: {} },
     name: { validations: {} },
