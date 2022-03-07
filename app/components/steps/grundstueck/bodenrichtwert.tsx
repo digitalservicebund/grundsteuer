2c481a25e024adf96dd4e885698233ca73eb3ed5
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-03-07 16:27:13 +0100

    Fix showing too many flurstuecke in bodenrichtwert step

--- app/components/steps/grundstueck/bodenrichtwert.tsx
+++ app/components/steps/grundstueck/bodenrichtwert.tsx
@@ -9,6 +9,7 @@ const Bodenrichtwert: StepComponentFunction = ({
 }) => {
   const adresseData = allData?.grundstueck?.adresse;
   const flurstueckData = allData?.grundstueck?.flurstueck;
+  const flurstueckAnzahl = allData?.grundstueck?.anzahl?.anzahl;
   return (
     <div>
       <div className="mb-16">
@@ -47,56 +48,58 @@ const Bodenrichtwert: StepComponentFunction = ({
       )}
       {flurstueckData && (
         <div data-testid="grundstueck-flurstuecke">
-          {flurstueckData.map((flurstueck, index) => {
-            if (!flurstueck.angaben) {
-              return;
-            }
-            return (
-              <div key={index} className="bg-gray-100 p-4 mb-4">
-                <h2 className="font-bold">
-                  {i18n.specifics.flurstueckHeading} {index + 1}
-                </h2>
-                <ul>
-                  <li>
-                    {i18n.specifics.grundbuchblatt}:{" "}
-                    {flurstueck.angaben.grundbuchblattnummer}
-                  </li>
-                  <li>
-                    {i18n.specifics.gemarkung}: {flurstueck.angaben.gemarkung}
-                  </li>
-                  {flurstueck.angaben.flur && (
+          {flurstueckData
+            .slice(0, Number(flurstueckAnzahl || 1))
+            .map((flurstueck, index) => {
+              if (!flurstueck?.angaben) {
+                return;
+              }
+              return (
+                <div key={index} className="bg-gray-100 p-4 mb-4">
+                  <h2 className="font-bold">
+                    {i18n.specifics.flurstueckHeading} {index + 1}
+                  </h2>
+                  <ul>
                     <li>
-                      {i18n.specifics.flur}: {flurstueck.angaben.flur}
+                      {i18n.specifics.grundbuchblatt}:{" "}
+                      {flurstueck.angaben.grundbuchblattnummer}
                     </li>
-                  )}
-                  {flurstueck.angaben.flurstueckZaehler && (
                     <li>
-                      {i18n.specifics.flurstueckZaehler}:{" "}
-                      {flurstueck.angaben.flurstueckZaehler}
+                      {i18n.specifics.gemarkung}: {flurstueck.angaben.gemarkung}
                     </li>
-                  )}
-                  {flurstueck.angaben.flurstueckNenner && (
-                    <li>
-                      {i18n.specifics.flurstueckNenner}:{" "}
-                      {flurstueck.angaben.flurstueckNenner}
-                    </li>
-                  )}
-                  {flurstueck.angaben.wirtschaftlicheEinheitZaehler && (
-                    <li>
-                      {i18n.specifics.wirtschaftlicheEinheitZaehler}:{" "}
-                      {flurstueck.angaben.wirtschaftlicheEinheitZaehler}
-                    </li>
-                  )}
-                  {flurstueck.angaben.wirtschaftlicheEinheitNenner && (
-                    <li>
-                      {i18n.specifics.wirtschaftlicheEinheitNenner}:{" "}
-                      {flurstueck.angaben.wirtschaftlicheEinheitNenner}
-                    </li>
-                  )}
-                </ul>
-              </div>
-            );
-          })}
+                    {flurstueck.angaben.flur && (
+                      <li>
+                        {i18n.specifics.flur}: {flurstueck.angaben.flur}
+                      </li>
+                    )}
+                    {flurstueck.angaben.flurstueckZaehler && (
+                      <li>
+                        {i18n.specifics.flurstueckZaehler}:{" "}
+                        {flurstueck.angaben.flurstueckZaehler}
+                      </li>
+                    )}
+                    {flurstueck.angaben.flurstueckNenner && (
+                      <li>
+                        {i18n.specifics.flurstueckNenner}:{" "}
+                        {flurstueck.angaben.flurstueckNenner}
+                      </li>
+                    )}
+                    {flurstueck.angaben.wirtschaftlicheEinheitZaehler && (
+                      <li>
+                        {i18n.specifics.wirtschaftlicheEinheitZaehler}:{" "}
+                        {flurstueck.angaben.wirtschaftlicheEinheitZaehler}
+                      </li>
+                    )}
+                    {flurstueck.angaben.wirtschaftlicheEinheitNenner && (
+                      <li>
+                        {i18n.specifics.wirtschaftlicheEinheitNenner}:{" "}
+                        {flurstueck.angaben.wirtschaftlicheEinheitNenner}
+                      </li>
+                    )}
+                  </ul>
+                </div>
+              );
+            })}
         </div>
       )}
     </div>
