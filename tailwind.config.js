3b7ccdf2c242c3871830fd9ade25322a6023cb30
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-03-09 20:36:06 +0100

    Use Tailwind config from style-dictionary

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  presets: [
-    require("@digitalservice4germany/digital-service-library").TailwindConfig,
-  ],
+  presets: [require("@digitalservice4germany/style-dictionary/tailwind")],
   content: [
     "./app/**/*.{ts,tsx}",
     "./node_modules/@digitalservice4germany/digital-service-library/dist/esm/index.js",
