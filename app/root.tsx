b86861bd9e0e642fc767fa8e431c5fc7174bb99a
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-03-11 13:37:23 +0100

    Set body background color

--- app/root.tsx
+++ app/root.tsx
@@ -47,7 +47,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-gray-100">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
