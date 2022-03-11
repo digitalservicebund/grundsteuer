b94979c9a0e6b9e0846eb28b2737cbf91033712a
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-03-11 14:01:01 +0100

    Add logos

--- app/routes/kitchensink.tsx
+++ app/routes/kitchensink.tsx
@@ -3,9 +3,15 @@ import AddIcon from "~/components/icons/mui/Add";
 import RemoveIcon from "~/components/icons/mui/Remove";
 import FileCopyIcon from "~/components/icons/mui/FileCopy";
 
+import digitalserviceLogoImage from "~/assets/images/digitalservice-logo.svg";
+import bmfLogoImage from "~/assets/images/bmf-logo.png";
+
 export default function KitchenSink() {
   return (
     <div className="text-orange-500">
+      <img src={digitalserviceLogoImage} />
+      <img src={bmfLogoImage} className="w-[298px]" />
+
       <p className="font-sans">
         font-sans <strong>bold</strong>{" "}
         <em>
@@ -19,6 +25,7 @@ export default function KitchenSink() {
         </em>
       </p>
       <p className="font-condensed">font-condensed</p>
+
       <CloseIcon className="w-24 h-24 fill-current" />
       <AddIcon className="w-24 h-24 fill-blue-500" />
       <RemoveIcon className="w-24 h-24 fill-black" />
