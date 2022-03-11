b6cf349c6088350fcf4bf10a9c59bad1deb76945
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-03-11 18:37:37 +0100

    Use ts-jest

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,9 @@ module.exports = {
   testPathIgnorePatterns: ["<rootDir>/test/e2e"],
   setupFiles: ["<rootDir>/private/jest/setup.js"],
   setupFilesAfterEnv: ["<rootDir>/private/jest/setupAfterEnv.js"],
+  transform: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/private/jest/fileTransformer.js",
+  },
+  preset: "ts-jest",
 };
