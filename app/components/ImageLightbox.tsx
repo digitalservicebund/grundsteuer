3fa02f8ed2ac7689c099310b154081e9d1d96564
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-03-18 14:02:37 +0100

    Revert "Make only icon clickable"
    This reverts commit 6b807f45ddcc3882e1954470f2a99c4e7f1066d8.

--- app/components/ImageLightbox.tsx
+++ app/components/ImageLightbox.tsx
@@ -18,36 +18,40 @@ export const ImageLightbox = ({
   const [helpOpen, setHelpOpen] = useState(false);
   return (
     <>
-      <div className="relative">
-        <img src={thumbnail} alt={altText} className="w-full h-auto relative" />
-        <button
-          type="button"
-          onClick={() => setHelpOpen(true)}
-          className="pr-32 pb-32 bottom-0 right-0 absolute"
-          data-testid="enlarge-button"
-        >
+      <button
+        type="button"
+        onClick={() => setHelpOpen(true)}
+        data-testid="enlarge-button"
+      >
+        <div className="relative">
+          <img
+            src={thumbnail}
+            alt={altText}
+            className="w-full h-auto relative"
+          />
           <img
             src={enlargeIcon}
             alt={"Vergrößern"}
+            className="pr-32 pb-32 bottom-0 right-0 absolute"
             data-testid="enlarge-icon"
           />
-        </button>
-      </div>
+        </div>
 
-      <ImgsViewer
-        imgs={[{ src: image, caption: altText, alt: altText }]}
-        isOpen={helpOpen}
-        onClose={() => {
-          setHelpOpen(false);
-        }}
-        showImgCount={false}
-        showThumbnails={false}
-        theme={{
-          footer: {
-            "text-align": "center",
-          },
-        }}
-      />
+        <ImgsViewer
+          imgs={[{ src: image, caption: altText, alt: altText }]}
+          isOpen={helpOpen}
+          onClose={() => {
+            setHelpOpen(false);
+          }}
+          showImgCount={false}
+          showThumbnails={false}
+          theme={{
+            footer: {
+              "text-align": "center",
+            },
+          }}
+        />
+      </button>
     </>
   );
 };
