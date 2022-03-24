0587000cfdea2bfa5c789b85c3031b28a051553e
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-03-24 16:31:26 +0100

    Change bruchteilsgemeinschaft icons

--- app/components/icons/mui/House.tsx
+++ app/components/icons/mui/House.tsx
@@ -3,36 +3,20 @@ import { SVGProps } from "react";
 
 const House = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width="27"
-    height="27"
-    viewBox="0 0 27 27"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     <path
-      d="M3.85938 10.8203V21.8917C3.85938 22.6807 4.49897 23.3203 5.28795 23.3203H21.7165C22.5055 23.3203 23.1451 22.6807 23.1451 21.8917V10.8203"
-      stroke="black"
-      strokeWidth="2"
-      strokeLinecap="round"
+      d="M19 9.8V4.5H16V7.1L12 3.5L2 12.5H5V20.5H11V14.5H13V20.5H19V12.5H22L19 9.8ZM17 18.5H15V12.5H9V18.5H7V10.69L12 6.19L17 10.69V18.5Z"
+      fill="black"
     />
     <path
-      d="M1 13.5L12.9949 1.50508C13.2739 1.22613 13.7261 1.22613 14.0051 1.50508L26 13.5"
-      stroke="black"
-      strokeWidth="2"
-      strokeLinecap="round"
-    />
-    <path
-      d="M19.3906 6.53599V4.21456C19.3906 4.01732 19.5505 3.85742 19.7478 3.85742H21.3549C21.5522 3.85742 21.7121 4.01732 21.7121 4.21456V8.50028"
-      stroke="black"
-      strokeWidth="2"
-      strokeLinecap="round"
-    />
-    <path
-      d="M11.3594 23.3217V16.1789C11.3594 15.5871 11.8391 15.1074 12.4308 15.1074H15.2879C15.8797 15.1074 16.3594 15.5871 16.3594 16.1789V23.3217"
-      stroke="black"
-      strokeWidth="2"
-      strokeLinecap="round"
+      d="M10 10.5H14C14 9.4 13.1 8.5 12 8.5C10.9 8.5 10 9.4 10 10.5Z"
+      fill="black"
     />
   </svg>
 );
