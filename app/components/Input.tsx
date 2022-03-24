6670d9885ea515c6feed91adf31949120d6a83de
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-03-24 15:02:40 +0100

    Improve Input component with more options and styling

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -2,33 +2,46 @@ import React from "react";
 import classNames from "classnames";
 
 export interface InputProps extends React.ComponentPropsWithoutRef<"input"> {
-  /** small input field */
-  small?: boolean;
-  /** very small input field */
-  tiny?: boolean;
+  labelText?: string;
+  errorMessage?: string;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  const { small, tiny, className } = props;
+  const { className, labelText, errorMessage, name, id, ...inputProps } = props;
+
   return (
-    <input
-      {...props}
-      ref={ref}
-      className={classNames(
-        "block w-full",
-        "bg-white text-black",
-        "border-2 border-darkGrey-1000",
-        "hover:border-dustyGrey",
-        "focus:border-darkGrey-900 focus:outline focus:outline-4 focus:outline-darkGreen",
-        "disabled:text-darkGrey-800 disabled:border-darkGrey-400",
-        {
-          "px-3.5 py-4 text-lg leading-7": !small && !tiny,
-          "px-3.5 py-3 text-base leading-5": small,
-          "px-3.5 py-2 text-sm leading-5": tiny,
-        },
-        className
+    <>
+      {labelText && (
+        <label
+          htmlFor={id || name}
+          className={classNames("block mb-8", {
+            "text-gray-600": inputProps.disabled,
+          })}
+        >
+          {labelText}
+        </label>
       )}
-    />
+      <input
+        {...inputProps}
+        ref={ref}
+        name={name}
+        id={id || name}
+        className={classNames(
+          "block w-full",
+          "bg-white text-black",
+          "border-2 border-blue-800",
+          "hover:outline hover:outline-4 hover:outline-blue-800 hover:outline-offset-[-4px]",
+          "focus:outline focus:outline-4 focus:outline-blue-800 focus:outline-offset-[-4px]",
+          "disabled:!text-gray-600 disabled:!border-gray-600 disabled:!outline-none",
+          "px-24 min-h-[4rem] text-18 leading-24",
+          {
+            "border-red-800 bg-red-200": errorMessage,
+          },
+          className
+        )}
+      />
+      {errorMessage && <div className="text-red-800 mt-8">{errorMessage}</div>}
+    </>
   );
 });
 
