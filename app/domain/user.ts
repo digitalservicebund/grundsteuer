56b57c14fee0abc45f7e3c8dbaf1d6688ebe871b
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-03-29 12:34:51 +0200

    Persist new user.

--- app/domain/user.ts
+++ app/domain/user.ts
@@ -1,9 +1,30 @@
+import { db } from "~/db/db.server";
+import bcrypt from "bcryptjs";
+
 export const createUser = async (email: string, password: string) => {
-  // hash pw
-  return {}; // user
+  const hash = bcrypt.hashSync(password, 10);
+
+  try {
+    await db.user.create({
+      data: {
+        email: email,
+        password: hash,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    throw new Error("Internal server error.");
+  }
 };
 
 export const userExists = async (email: string) => {
-  // toLowerCase
-  return false;
+  try {
+    const user = await db.user.findFirst({
+      where: { email: email },
+    });
+    return !!user;
+  } catch (e) {
+    console.error(e);
+    throw new Error("Internal server error.");
+  }
 };
