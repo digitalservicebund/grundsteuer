15ac212247f7ef04e4c105b6a8176c017ba35f8c
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-03-31 18:01:34 +0200

    Add e2e tests for login page.

---
+++ test/e2e/anmelden.spec.ts
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+// @ts-check
+describe("/anmelden", () => {
+  it("simple success path", () => {
+    cy.visit("/anmelden");
+    cy.get("[name=email]").type("foo@bar.com");
+    cy.get("[name=password]").type("12345678");
+    cy.get("form button").click();
+    cy.url().should("include", "/formular/welcome");
+  });
+
+  it("should fail on wrong username", () => {
+    cy.visit("/anmelden");
+    cy.get("[name=email]").type("bar@bar.com");
+    cy.get("[name=password]").type("12345678");
+    cy.get("form button").click();
+    cy.contains("E-Mail-Adresse und/oder Passwort falsch.");
+  });
+
+  it("should fail on wrong password", () => {
+    cy.visit("/anmelden");
+    cy.get("[name=email]").type("foo@bar.com");
+    cy.get("[name=password]").type("123456789");
+    cy.get("form button").click();
+    cy.contains("E-Mail-Adresse und/oder Passwort falsch.");
+  });
+});
+
+export {};
