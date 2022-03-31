0661e2099cd408bbc2bdbeed3b746e4d2215c993
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-03-31 13:05:06 +0200

    Add more e2e tests for registration.

--- test/e2e/registrieren.spec.ts
+++ test/e2e/registrieren.spec.ts
@@ -17,6 +17,54 @@ describe("/registrieren", () => {
     cy.get("form button").click();
     cy.contains("Bitte überprüfen Sie die Schreibweise der E-Mail-Adresse.");
   });
+
+  it("password too short", () => {
+    const email = "bar@bar.com";
+    const password = "7654321";
+    cy.visit("/registrieren");
+    cy.get("[name=email]").type(email);
+    cy.get("[name=emailRepeated]").type(email);
+    cy.get("[name=password]").type(password);
+    cy.get("[name=passwordRepeated]").type(password);
+    cy.get("form button").click();
+    cy.contains("Passwort ist zu kurz");
+  });
+
+  it("existing user", () => {
+    const email = "foo@bar.com";
+    const password = "87654321";
+    cy.visit("/registrieren");
+    cy.get("[name=email]").type(email);
+    cy.get("[name=emailRepeated]").type(email);
+    cy.get("[name=password]").type(password);
+    cy.get("[name=passwordRepeated]").type(password);
+    cy.get("form button").click();
+    cy.contains("Es existiert bereits");
+  });
+
+  it("incorrectly repeated email", () => {
+    const email = "bar@bar.com";
+    const password = "87654321";
+    cy.visit("/registrieren");
+    cy.get("[name=email]").type(email);
+    cy.get("[name=emailRepeated]").type("bar@foo.com");
+    cy.get("[name=password]").type(password);
+    cy.get("[name=passwordRepeated]").type(password);
+    cy.get("form button").click();
+    cy.contains("E-Mail-Adresse genau so ein");
+  });
+});
+
+it("incorrectly repeated password", () => {
+  const email = "bar@bar.com";
+  const password = "87654321";
+  cy.visit("/registrieren");
+  cy.get("[name=email]").type(email);
+  cy.get("[name=emailRepeated]").type(email);
+  cy.get("[name=password]").type(password);
+  cy.get("[name=passwordRepeated]").type("12345678");
+  cy.get("form button").click();
+  cy.contains("stimmen nicht");
 });
 
 export {};
