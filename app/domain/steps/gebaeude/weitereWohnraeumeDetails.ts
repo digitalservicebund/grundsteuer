53dc3e1b511df77745b9677c59a85d631df30034
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-04-04 08:25:54 +0200

    Validate weitere Wohnraeume

--- app/domain/steps/gebaeude/weitereWohnraeumeDetails.ts
+++ app/domain/steps/gebaeude/weitereWohnraeumeDetails.ts
@@ -7,7 +7,25 @@ export type GebaeudeWeitereWohnraeumeDetailsFields = {
 
 export const gebaeudeWeitereWohnraeumeDetails: StepDefinition = {
   fields: {
-    anzahl: { validations: {} },
-    flaeche: { validations: {} },
+    anzahl: {
+      validations: {
+        required: {},
+        onlyDecimal: {},
+        maxLength: {
+          maxLength: 3,
+          msg: "Die Zahl darf maximal 3 Ziffern beinhalten.",
+        },
+      },
+    },
+    flaeche: {
+      validations: {
+        required: {},
+        onlyDecimal: {},
+        maxLength: {
+          maxLength: 6,
+          msg: "Die Zahl darf maximal 6 Ziffern beinhalten.",
+        },
+      },
+    },
   },
 };
