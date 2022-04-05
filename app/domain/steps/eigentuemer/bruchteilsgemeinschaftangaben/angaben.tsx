48484878d20ff12b77534702cde7e17589672ba2
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-04-05 12:32:25 +0200

    Validate bruchteilsgemeinschaft

--- app/domain/steps/eigentuemer/bruchteilsgemeinschaftangaben/angaben.tsx
+++ app/domain/steps/eigentuemer/bruchteilsgemeinschaftangaben/angaben.tsx
@@ -8,34 +8,73 @@ export type EigentuemerBruchteilsgemeinschaftAngabenFields = AdresseFields & {
 export const eigentuemerBruchteilsgemeinschaftAngaben: StepDefinition = {
   fields: {
     name: {
-      validations: {},
+      validations: {
+        required: {},
+        maxLength: {
+          // Name 1+2
+          maxLength: 50,
+          msg: "Der Name darf maximal 50 Zeichen umfassen",
+        },
+      },
     },
     strasse: {
       validations: {
-        requiredIf: {
-          dependentField: "hausnummer",
-          msg: "musst du eingeben",
+        eitherOr: {
+          dependentField: "postfach",
+          msg: "Geben Sie nur genau eine Adresse an (entweder Straßenadresse oder Postfachadresse)",
         },
         maxLength: {
-          maxLength: 200,
-          msg: "zu lang",
+          maxLength: 25,
+          msg: "Die Angabe darf maximal 25 Zeichen beinhalten",
         },
       },
     },
     hausnummer: {
       validations: {
-        requiredIf: {
-          dependentField: "strasse",
-          msg: "musst du eingeben",
-        },
-        maxLength: {
-          maxLength: 12,
-          msg: "zu lang",
+        hausnummer: {},
+        forbiddenIf: {
+          dependentField: "postfach",
+          msg: "Darf bei einer Postfachadresse nicht angegeben werden",
         },
       },
     },
-    postfach: { validations: {} },
-    plz: { validations: {} },
-    ort: { validations: {} },
+    postfach: {
+      validations: {
+        eitherOr: {
+          dependentField: "strasse",
+          msg: "Geben Sie nur genau eine Adresse an (entweder Straßenadresse oder Postfachadresse)",
+        },
+        onlyDecimal: {},
+        maxLength: {
+          maxLength: 6,
+          msg: "Darf bis zu 6 Ziffern lang sein",
+        },
+      },
+    },
+    plz: {
+      validations: {
+        required: {},
+        onlyDecimal: {},
+        minLength: {
+          minLength: 5,
+          msg: "Muss genau 5 Zeichen lang sein",
+        },
+        maxLength: {
+          maxLength: 5,
+          msg: "Muss genau 5 Zeichen lang sein",
+        },
+      },
+    },
+    ort: {
+      validations: {
+        validations: {
+          required: {},
+          maxLength: {
+            maxLength: 25,
+            msg: "Die Angabe darf maximal 25 Zeichen beinhalten",
+          },
+        },
+      },
+    },
   },
 };
