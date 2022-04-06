886e201bf9e91b1ba37e0e4dbe5d80742f5e3951
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-04-06 18:20:07 +0200

    Fix formatting in ericaClient files

--- app/erica/ericaClient.test.ts
+++ app/erica/ericaClient.test.ts
@@ -1,38 +1,29 @@
-import {postToErica} from "./ericaClient";
+import { postToErica } from "./ericaClient";
 
-const mockFetchReturn201 = jest.fn(() => Promise.resolve(
-        {
-            status: 201,
-            headers: new Map([["location", "createdLocation"]]),
-            json: () => Promise.resolve({}),
-        }
-    ), ) as jest.Mock
-
-
+const mockFetchReturn201 = jest.fn(() =>
+  Promise.resolve({
+    status: 201,
+    headers: new Map([["location", "createdLocation"]]),
+    json: () => Promise.resolve({}),
+  })
+) as jest.Mock;
 
 describe("postToErica", () => {
-    describe("with no ERICA_URL in env", () => {
-        it("should fail", async () => {
-            await expect(postToErica("someEndpoint")).rejects.toThrow();
-        });
+  describe("with no ERICA_URL in env", () => {
+    it("should fail", async () => {
+      await expect(postToErica("someEndpoint")).rejects.toThrow();
+    });
+  });
+
+  describe("with no ERICA_URL in env", () => {
+    beforeEach(() => {
+      process.env.ERICA_URL = "localhost:8000";
     });
 
-    describe("with no ERICA_URL in env", () => {
-        beforeEach(() => {
-            process.env.ERICA_URL = "localhost:8000";
-        });
-
-        it("should return location if receives a 201 from endpoint", async () => {
-            jest.spyOn(global, "fetch").mockImplementation(
-                mockFetchReturn201
-            )
-            const result = await postToErica("someEndpoint");
-            expect(result).toEqual(
-                "createdLocation"
-            );
-        });
+    it("should return location if receives a 201 from endpoint", async () => {
+      jest.spyOn(global, "fetch").mockImplementation(mockFetchReturn201);
+      const result = await postToErica("someEndpoint");
+      expect(result).toEqual("createdLocation");
     });
+  });
 });
-
-
-
