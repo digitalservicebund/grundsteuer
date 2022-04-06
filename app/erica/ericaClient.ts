00877f869a61c60e47f9cb032094f7552b8ac313
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-04-06 18:15:41 +0200

    Add function to post to Erica

---
+++ app/erica/ericaClient.ts
@@ -0,0 +1,17 @@
+import invariant from "tiny-invariant";
+
+const postToErica = async (endpoint: string) => {
+
+    invariant(
+        typeof process.env.ERICA_URL === "string",
+        "environment variable ERICA_URL is not set"
+    );
+
+    const response = await fetch(`process.env.ERICA_URL/${endpoint}`)
+
+    if (response.status == 201) {
+        return response.headers.get('location');
+    }
+}
+
+export { postToErica };
