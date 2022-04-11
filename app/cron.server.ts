b974ae69af8c8c7d0efe8963db08b95878c5b163
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-04-11 17:36:55 +0200

    Add cron job for deleting expired FSC requests.

---
+++ app/cron.server.ts
@@ -0,0 +1,25 @@
+import cron from "node-cron";
+import { db } from "~/db/db.server";
+
+const scheduleFscCleanup = (cronExpression: string) => {
+  console.info(
+    "Schedule deleting expired FSC requests with cron expression: %s",
+    cronExpression
+  );
+  cron.schedule(cronExpression, async () => deleteExpiredFscs());
+};
+
+const deleteExpiredFscs = async () => {
+  const now = new Date();
+  const oneYearAgo = new Date(now.setFullYear(now.getFullYear() - 1));
+  const queryResult = await db.fscRequest.deleteMany({
+    where: {
+      createdAt: {
+        lte: oneYearAgo,
+      },
+    },
+  });
+  console.log("Deleted %d expired FSC requests.", queryResult.count);
+};
+
+export const jobs = { scheduleFscCleanup };
--- server.js
+++ server.js
@@ -4,39 +4,48 @@ import dotenv from "dotenv-safe";
 import morgan from "morgan";
 import { createRequestHandler } from "@remix-run/express";
 import * as serverBuild from "@remix-run/dev/server-build";
+import { jobs } from "./app/cron.server";
 
 // Fail fast on missing configuration parameters
 dotenv.config();
 
-const app = express();
+const appMode = process.env.APP_MODE;
 
-app.use(compression());
+// cron mode is intended for running cron jobs only. The app will not serve any HTTP requests in this mode.
+if (appMode === "cron") {
+  // run every hour
+  jobs.scheduleFscCleanup("0 * * * *");
+} else {
+  const app = express();
 
-// http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
-app.disable("x-powered-by");
+  app.use(compression());
 
-// Remix fingerprints its assets so we can cache forever.
-app.use(
-  "/build",
-  express.static("public/build", { immutable: true, maxAge: "1y" })
-);
+  // http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
+  app.disable("x-powered-by");
 
-// Everything else (like favicon.ico) is cached for an hour. You may want to be
-// more aggressive with this caching.
-app.use(express.static("public", { maxAge: "1h" }));
+  // Remix fingerprints its assets so we can cache forever.
+  app.use(
+    "/build",
+    express.static("public/build", { immutable: true, maxAge: "1y" })
+  );
 
-app.use(morgan("tiny"));
+  // Everything else (like favicon.ico) is cached for an hour. You may want to be
+  // more aggressive with this caching.
+  app.use(express.static("public", { maxAge: "1h" }));
 
-app.all(
-  "*",
-  createRequestHandler({
-    build: serverBuild,
-    mode: process.env.NODE_ENV,
-  })
-);
+  app.use(morgan("tiny"));
 
-const port = process.env.PORT || 3000;
+  app.all(
+    "*",
+    createRequestHandler({
+      build: serverBuild,
+      mode: process.env.NODE_ENV,
+    })
+  );
 
-app.listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
-});
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+  });
+}
