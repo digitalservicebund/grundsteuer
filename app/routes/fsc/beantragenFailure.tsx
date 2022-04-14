01bb6107be5510c0f7601b1aed4c9bdb54439a33
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-04-14 18:33:56 +0200

    Add background check for erica

---
+++ app/routes/fsc/beantragenFailure.tsx
@@ -0,0 +1,12 @@
+import { LoaderFunction } from "remix";
+
+export const loader: LoaderFunction = async () => {
+  return "Failure FSC Beantragen";
+};
+
+
+
+export default function Start() {
+
+  return "Failure FSC Beantragen";
+}
---
+++ app/routes/fsc/beantragenSuccess.tsx
@@ -0,0 +1,10 @@
+import { LoaderFunction } from "remix";
+
+export const loader: LoaderFunction = async () => {
+  return "Success FSC Beantragen";
+};
+
+export default function Start() {
+
+  return "Success FSC Beantragen";
+}
