9ade498c8c51bf517e544a9235e46eab5707ca34
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-04-14 18:33:56 +0200

    Add background check for erica

--- app/routes/fsc/beantragen.tsx
+++ app/routes/fsc/beantragen.tsx
@@ -2,53 +2,123 @@ import { useEffect } from "react";
 import {
   ActionFunction,
   Form,
+  json,
   LoaderFunction,
+  redirect, Session,
   useActionData,
+  useFetcher,
   useLoaderData,
 } from "remix";
 import { authenticator } from "~/auth.server";
-import { Button, FormGroup, Input, SimplePageLayout } from "~/components";
+import {Button, FormGroup, Input, SimplePageLayout, Spinner} from "~/components";
+import {
+  requestNewFreischaltCode,
+  retrieveAntragsId,
+} from "~/erica/freischaltCode";
+
+import { commitSession, getSession } from "~/session.server";
+import is from "@sindresorhus/is";
+import truthy = is.truthy;
+
+
+const isEricaRequestInProgress = (session: Session) => {
+  return truthy(session.get("ericaRequestId"));
+}
+
+const wasEricaRequestSuccessful = (session: Session) => {
+  return truthy(session.get("elsterRequestId"));
+}
+
 
 export const loader: LoaderFunction = async ({ request }) => {
-  // logic
-  return { loader: true };
+  const session = await getSession(request.headers.get("Cookie"));
+
+  if (wasEricaRequestSuccessful(session)) {
+    return redirect("/fsc/beantragenSuccess");
+  }
+
+  if (isEricaRequestInProgress(session)) {
+    try {
+      const elsterRequestId = await retrieveAntragsId(session.get("ericaRequestId"));
+      if (elsterRequestId) {
+        session.set("elsterRequestId", elsterRequestId);
+        session.unset("ericaRequestId");
+      }
+    } catch (Error) {
+      session.unset("ericaRequestId");
+      return redirect("/fsc/beantragenFailure", {
+      headers: {
+        "Set-Cookie": await commitSession(session),
+      }});
+    }
+  }
+
+  return json({
+    inProgress: truthy(session.get("ericaRequestId"))
+  }, {
+    headers: {
+      "Set-Cookie": await commitSession(session),
+    },
+  });
 };
+
 
 export const action: ActionFunction = async ({ request }) => {
-  const user = await authenticator.isAuthenticated(request, {
+  await authenticator.isAuthenticated(request, {
     failureRedirect: "/anmelden",
   });
-  console.log({ user }, user.state, user.ericaRequestId);
-  // check: state of user
-  // redirect if all done
+  const session = await getSession(request.headers.get("Cookie"));
 
-  // erica request in progress
+  if (wasEricaRequestSuccessful(session)) {
+    return redirect("/fsc/beantragenSuccess");
+  }
 
-  const formData = await request.formData();
-  const steuerId = formData.get("steuerId");
-  const geburtsdatum = formData.get("geburtsdatum");
-  // validate steuerId && geburtstag -> return { errors }
+  if (!isEricaRequestInProgress(session)) {
+    const formData = await request.formData();
+    const steuerId = formData.get("steuerId");
+    const geburtsdatum = formData.get("geburtsdatum");
 
-  // erica request request
+    if (steuerId && geburtsdatum) {
+      const ericaRequestId = await requestNewFreischaltCode(
+        steuerId.toString(),
+        geburtsdatum?.toString()
+      );
+      session.set("ericaRequestId", ericaRequestId);
+    }
 
-  return {
-    errors: { geburtsdatum: "fehlt" },
-    action: true,
-    geburtsdatum,
-    steuerId,
-  };
+  }
+  return json(
+    {
+      inProgress: truthy(session.get("ericaRequestId"))
+    },
+    {
+      headers: {
+        "Set-Cookie": await commitSession(session),
+      },
+    }
+  );
 };
 
-export default function FscBeantragen() {
+export default function Redirect() {
   const loaderData = useLoaderData();
   const actionData = useActionData();
 
+  // We need to fetch data to check the result with Elster
+  const fetcher = useFetcher();
   useEffect(() => {
-    document.title = "Hallo";
+    const interval = setInterval(() => {
+      if (document.visibilityState === "visible") {
+        fetcher.load("/fsc/beantragen");
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
   }, []);
 
+  const spinnerElement = actionData?.inProgress || loaderData?.inProgress ? <Spinner /> : null;
   return (
     <SimplePageLayout>
+      {spinnerElement}
       <pre>{JSON.stringify(loaderData, null, 2)}</pre>
       <pre>{JSON.stringify(actionData, null, 2)}</pre>
       <br />
