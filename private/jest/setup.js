5e08f9c0b02e916c4c5c520444b7feb83f3edcdf
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-04-18 19:38:11 +0200

    Fix warning about missing i18next instance in tests

--- private/jest/setup.js
+++ private/jest/setup.js
@@ -1,5 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+const i18n = require("i18next");
+const { initReactI18next } = require("react-i18next");
 const { installGlobals } = require("@remix-run/node");
+/* eslint-enable @typescript-eslint/no-var-requires */
+
+i18n.use(initReactI18next).init({
+  fallbackLng: "de",
+  supportedLanguages: ["de"],
+  i18nextOptions: { returnObjects: true },
+});
 
 // Add globals to be able to access Request/Response etc. in the tests
 installGlobals();
