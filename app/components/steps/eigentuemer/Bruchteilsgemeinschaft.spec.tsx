7836242eb833274b21fb3618a86d1c31cfd934c8
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-05-02 13:05:49 +0200

    Fix import.

--- app/components/steps/eigentuemer/Bruchteilsgemeinschaft.spec.tsx
+++ app/components/steps/eigentuemer/Bruchteilsgemeinschaft.spec.tsx
@@ -1,7 +1,7 @@
 import { getI18nObject, grundModelFactory } from "test/factories";
 import { render, screen } from "@testing-library/react";
 import { eigentuemerBruchteilsgemeinschaft } from "~/domain/steps/eigentuemer/bruchteilsgemeinschaft";
-import Bruchteilsgemeinschaft from "~/components/steps/eigentuemer/bruchteilsgemeinschaft";
+import Bruchteilsgemeinschaft from "~/components/steps/eigentuemer/Bruchteilsgemeinschaft";
 import { I18nObject } from "~/i18n/getStepI18n";
 
 const missingNameText = "Bitte geben Sie die Adresse des Grundstücks an.";
