229141460d5358be1e278d24d02e44e4ea44e8c3
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-05-04 17:52:15 +0200

    Add fsc eingeben functioniality

--- app/routes/fsc/eingeben/index.tsx
+++ app/routes/fsc/eingeben/index.tsx
@@ -1,17 +1,214 @@
-import { LoaderFunction } from "@remix-run/node";
-import { Button, ContentContainer } from "~/components";
+import {
+  ActionFunction,
+  json,
+  LoaderFunction,
+  redirect,
+} from "@remix-run/node";
+import {
+  Button,
+  ButtonContainer,
+  ContentContainer,
+  FormGroup,
+  Headline,
+  Input,
+  Spinner,
+} from "~/components";
+import {
+  Form,
+  useActionData,
+  useFetcher,
+  useLoaderData,
+} from "@remix-run/react";
+import invariant from "tiny-invariant";
+import { getErrorMessageForFreischaltcode } from "~/domain/validation";
+import { removeUndefined } from "~/util/removeUndefined";
+import {
+  activateFreischaltCode,
+  checkFreischaltcodeActivation,
+} from "~/erica/freischaltCodeAktivieren";
+import {
+  deleteEricaRequestIdFscAktivieren,
+  findUserByEmail,
+  saveEricaRequestIdFscAktivieren,
+  setUserIdentified,
+  User,
+} from "~/domain/user";
+import { authenticator } from "~/auth.server";
+import { useEffect } from "react";
 
-export const loader: LoaderFunction = async () => {
+const isEricaRequestInProgress = async (userData: User) => {
+  return Boolean(userData.ericaRequestIdFscAktivieren);
+};
+
+const wasEricaRequestSuccessful = async (userData: User) => {
+  return userData.identified;
+};
+
+const getEricaRequestIdFscAktivieren = async (userData: User) => {
+  invariant(
+    userData.ericaRequestIdFscAktivieren,
+    "ericaRequestIdFscAktivieren is null"
+  );
+  return userData.ericaRequestIdFscAktivieren;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await authenticator.isAuthenticated(request, {
+    failureRedirect: "/anmelden",
+  });
+  const userData: User | null = await findUserByEmail(user.email);
+  invariant(
+    userData,
+    "expected a matching user in the database from a user in a cookie session"
+  );
+
+  if (await wasEricaRequestSuccessful(userData)) {
+    return redirect("/fsc/eingeben/erfolgreich");
+  }
+
+  if (await isEricaRequestInProgress(userData)) {
+    const elsterRequestIdOrError = await checkFreischaltcodeActivation(
+      await getEricaRequestIdFscAktivieren(userData)
+    );
+    if (elsterRequestIdOrError) {
+      if (typeof elsterRequestIdOrError == "boolean") {
+        await setUserIdentified(user.email, true);
+        await deleteEricaRequestIdFscAktivieren(user.email);
+      } else if (elsterRequestIdOrError?.errorType == "EricaUserInputError") {
+        await deleteEricaRequestIdFscAktivieren(user.email);
+        return {
+          error: true,
+          showSpinner: false,
+        };
+      } else {
+        await deleteEricaRequestIdFscAktivieren(user.email);
+        throw new Error(elsterRequestIdOrError?.errorType);
+      }
+    }
+  }
+
+  const inProgress = await isEricaRequestInProgress(userData);
+
+  return {
+    error: false,
+    showSpinner: inProgress,
+  };
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const user = await authenticator.isAuthenticated(request, {
+    failureRedirect: "/anmelden",
+  });
+  const userData: User | null = await findUserByEmail(user.email);
+  invariant(
+    userData,
+    "expected a matching user in the database from a user in a cookie session"
+  );
+  invariant(
+    userData.fscRequest.length >= 1,
+    "expected an fscRequest in database for user"
+  );
+  const elsterRequestId = userData.fscRequest[0].requestId;
+
+  if (await wasEricaRequestSuccessful(userData)) {
+    return redirect("/fsc/eingeben/erfolgreich");
+  }
+
+  const ericaRequestIsInProgress = await isEricaRequestInProgress(userData);
+
+  if (ericaRequestIsInProgress) {
+    return {};
+  }
+
+  const formData = await request.formData();
+  const freischaltCode = formData.get("freischaltCode");
+
+  invariant(
+    typeof freischaltCode === "string",
+    "expected formData to include freischaltCode field of type string"
+  );
+
+  const normalizedFreischaltCode = freischaltCode.replace(/[\s/]/g, "");
+
+  const errors = {
+    freischaltCode: await getErrorMessageForFreischaltcode(
+      normalizedFreischaltCode
+    ),
+  };
+
+  const errorsExist = errors.freischaltCode;
+
+  if (errorsExist) {
+    return json({
+      errors: removeUndefined(errors),
+    });
+  }
+
+  const ericaRequestId = await activateFreischaltCode(
+    normalizedFreischaltCode,
+    elsterRequestId
+  );
+  await saveEricaRequestIdFscAktivieren(user.email, ericaRequestId);
+
   return {};
 };
 
 export default function FscEingeben() {
+  const loaderData = useLoaderData();
+  const actionData = useActionData();
+  const errors = actionData?.errors;
+
+  // We need to fetch data to check the result with Elster
+  const fetcher = useFetcher();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetcher.load("/fsc/eingeben?index");
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <ContentContainer size="sm">
-      <pre className="mb-32">TODO (STL-2042)</pre>
-      <Button look="secondary" to="/formular/welcome">
-        Später eingeben
-      </Button>
+      <pre>{JSON.stringify({ loaderData }, null, 2)}</pre>
+      <pre>{JSON.stringify({ actionData }, null, 2)}</pre>
+
+      <Headline>Bitte geben Sie Ihren Freischaltcode ein</Headline>
+
+      {loaderData?.error && (
+        <div className="p-16 mb-32 bg-red-200 border-2 border-red-800">
+          Es ist ein Fehler aufgetreten.
+        </div>
+      )}
+
+      <Form method="post">
+        <div>
+          <FormGroup>
+            <Input
+              name="freischaltCode"
+              label="Freischaltcode"
+              error={errors?.freischaltCode}
+            />
+          </FormGroup>
+        </div>
+        <ButtonContainer forceMultiline>
+          <Button>Freischaltcode speichern</Button>
+          <Button look="secondary" to="/formular/welcome">
+            Zurück
+          </Button>
+        </ButtonContainer>
+      </Form>
+      {loaderData?.showSpinner && (
+        <Spinner
+          initialText={"Ihr Freischaltcode wird überprüft."}
+          waitingText={
+            "Das Überprüfen dauert gerade leider etwas länger. Bitte verlassen Sie diese Seite nicht."
+          }
+          longerWaitingText={
+            "Wir überprüfen weiter Ihren Freischaltcode. Bitte verlassen Sie diese Seite nicht."
+          }
+        />
+      )}
     </ContentContainer>
   );
 }
---
+++ test/e2e/fscAktivieren.spec.ts
@@ -0,0 +1,106 @@
+/// <reference types="../../cypress/support" />
+
+const validFreischaltCode = "ABCD-1234-EFGH";
+
+describe("/eingeben", () => {
+  beforeEach(() => {
+    cy.request("GET", "http://localhost:8000/reset");
+    cy.task("dbRemoveFsc", "foo@bar.com");
+    cy.task("dbRemoveEricaRequestIdAktivieren", "foo@bar.com");
+    cy.task("setUserIdentifiedAttribute", {
+      userEmail: "foo@bar.com",
+      identified: false,
+    });
+    cy.task("addFscRequestId", {
+      userEmail: "foo@bar.com",
+      fscRequestId: "foo",
+    });
+    cy.login();
+  });
+
+  afterEach(() => {
+    cy.request("GET", "http://localhost:8000/reset");
+    cy.task("dbRemoveFsc", "foo@bar.com");
+    cy.task("dbRemoveEricaRequestIdAktivieren", "foo@bar.com");
+    cy.task("setUserIdentifiedAttribute", {
+      userEmail: "foo@bar.com",
+      identified: false,
+    });
+  });
+
+  it("should redirect to success page if mockErica returns success", () => {
+    cy.request("GET", "http://localhost:8000/triggerDirectResponse");
+    cy.request("GET", "http://localhost:8000/triggerSuccess");
+    cy.visit("/fsc/eingeben");
+    cy.get("[name=freischaltCode]").type(validFreischaltCode);
+    cy.get("form button").click();
+    cy.url().should("include", "/fsc/eingeben/erfolgreich");
+  });
+
+  it("should show spinner if data is correct and mockErica returns no result", () => {
+    cy.request("GET", "http://localhost:8000/triggerDelayedResponse");
+    cy.visit("/fsc/eingeben");
+    cy.get("[name=freischaltCode]").type(validFreischaltCode);
+    cy.get("form button").click();
+    cy.contains("Ihr Freischaltcode wird überprüft.");
+  });
+
+  it("should show error messages and no spinner if user input is invalid", () => {
+    cy.request("GET", "http://localhost:8000/triggerDelayedResponse");
+    cy.visit("/fsc/eingeben");
+    cy.get("[name=freischaltCode]").type("invalid");
+    cy.get("form button").click();
+    cy.contains(
+      "Bitte geben Sie Ihren Freischaltcode im folgenden Format ein AAAA-AAAA-AAAA."
+    );
+    cy.contains("Ihr Freischaltcode wird überprüft.").should("not.exist");
+  });
+
+  it("should show spinner if ericaRequestId already in database", () => {
+    cy.task("addEricaRequestIdFscAktivieren", {
+      userEmail: "foo@bar.com",
+      ericaRequestId: "foo",
+    });
+    cy.visit("/fsc/eingeben");
+    cy.contains("Ihr Freischaltcode wird überprüft.");
+  });
+
+  it("should show error message and no spinner if mockErica returns failure", () => {
+    cy.request("GET", "http://localhost:8000/triggerDirectResponse");
+    cy.request("GET", "http://localhost:8000/triggerFailure");
+    cy.visit("/fsc/eingeben");
+    cy.get("[name=freischaltCode]").type(validFreischaltCode);
+    cy.get("form button").click();
+    cy.contains("Es ist ein Fehler aufgetreten");
+    cy.contains("Ihr Freischaltcode wird überprüft.").should("not.exist");
+  });
+
+  it("should redirect to success page if it was already successful", () => {
+    cy.task("setUserIdentifiedAttribute", {
+      userEmail: "foo@bar.com",
+      identified: true,
+    });
+    cy.visit("/fsc/eingeben");
+    cy.url().should("include", "/fsc/eingeben/erfolgreich");
+  });
+
+  it("should show 500 page if failure and unexpected error", () => {
+    //Because we do not reload the page we need to intercept the call in the background
+    cy.intercept(
+      "GET",
+      "/fsc/eingeben?index=&_data=routes%2Ffsc%2Feingeben%2Findex"
+    ).as("eingeben");
+    cy.request("GET", "http://localhost:8000/triggerDirectResponse");
+    cy.request("GET", "http://localhost:8000/triggerFailure");
+    cy.request("GET", "http://localhost:8000/triggerUnexpectedFailure");
+    cy.visit("/fsc/eingeben");
+    cy.get("[name=freischaltCode]").type(validFreischaltCode);
+    cy.get("form button").click();
+    cy.wait("@eingeben").then((intercept) => {
+      expect(intercept.response?.statusCode == 500);
+    });
+    cy.contains("Da ist etwas schiefgelaufen");
+  });
+});
+
+export {};
