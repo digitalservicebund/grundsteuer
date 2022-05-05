94406423e234c3434a9b5c7e05b06e7c1ed53eb2
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-05-05 15:31:57 +0200

    Improve slash alignment.

--- app/components/form/InputFraction.tsx
+++ app/components/form/InputFraction.tsx
@@ -12,8 +12,8 @@ export default function InputFraction(props: {
   return (
     <fieldset className={cssClasses}>
       <div className="inline-block w-full">{zaehler}</div>
-      <div className="flex items-center self-end min-h-[4rem] mb-24">
-        <Slash className="inline-block mx-10" role="img" />
+      <div className="self-end min-h-[3rem]">
+        <Slash className="inline-block mx-16" role="img" />
       </div>
       <div className="inline-block w-full">{nenner}</div>
     </fieldset>
