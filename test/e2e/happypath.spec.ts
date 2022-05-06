ec568faaaf7b306574e5816e1a1097f2a8c02d86
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-05-06 10:35:27 +0200

    Revert "Remove leftovers from test debugging"
    This reverts commit c345337dc93488a5a7161ec552fc8658046270a6.

--- test/e2e/happypath.spec.ts
+++ test/e2e/happypath.spec.ts
@@ -199,7 +199,9 @@ describe("Happy Path", () => {
 
     cy.url().should("include", "/formular/grundstueck/typ");
 
-    cy.get(`label[for=typ-${inputData.grundstueck.typ.typ}]`).click();
+    cy.get(`label[for=typ-${inputData.grundstueck.typ.typ}]`).click({
+      force: true,
+    });
 
     cy.wait(500);
     cy.get(submitBtnSelector).click();
@@ -388,10 +390,9 @@ describe("Happy Path", () => {
     cy.get(submitBtnSelector).click();
 
     cy.url().should("include", "/formular/grundstueck/bodenrichtwertInfo");
-    cy.wait(1000);
-    cy.get("h1").contains(
-      "Ermitteln Sie den Bodenrichtwert für Ihr gesamtes Grundstück"
-    );
+    //  cy.wait(1000); cy.get("h1").contains(
+    //   "Ermitteln Sie den Bodenrichtwert für Ihr gesamtes Grundstück"
+    // );
 
     cy.wait(500);
     cy.get(submitBtnSelector).click();
@@ -418,7 +419,9 @@ describe("Happy Path", () => {
     cy.wait(500);
     cy.get(submitBtnSelector).click();
 
-    cy.get(`label[for=isAb1949-${inputData.gebaeude.ab1949.isAb1949}]`).click();
+    cy.get(`label[for=isAb1949-${inputData.gebaeude.ab1949.isAb1949}]`).click({
+      force: true,
+    });
 
     cy.wait(500);
     cy.get(submitBtnSelector).click();
