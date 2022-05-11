ae641d432364c9ad5a510b00b6ffd3a7f530dceb
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-05-11 10:29:14 +0200

    Implement asymmetric encryption.

--- app/audit/crypto.test.ts
+++ app/audit/crypto.test.ts
@@ -1,5 +1,21 @@
 import * as crypto from "crypto";
-import { decryptSym, encryptSym } from "~/audit/crypto";
+import {
+  decryptData,
+  decryptSym,
+  decryptWithPrivateKey,
+  encryptData,
+  encryptSym,
+  encryptWithPublicKey,
+} from "~/audit/crypto";
+import * as fs from "fs";
+import { Buffer } from "buffer";
+
+const PUBLIC_KEY = Buffer.from(
+  fs.readFileSync("test/resources/public.pem", { encoding: "utf-8" })
+);
+const PRIVATE_KEY = Buffer.from(
+  fs.readFileSync("test/resources/key.pem", { encoding: "utf-8" })
+);
 
 describe("symmetric encprytion", () => {
   it("should produce correct ciphertext", () => {
@@ -7,7 +23,6 @@ describe("symmetric encprytion", () => {
     const message = "burn after reading";
 
     const ciphertext = encryptSym(key, message);
-
     expect(ciphertext).not.toEqual(message);
 
     const deciphered = decryptSym(key, ciphertext);
@@ -20,7 +35,6 @@ describe("symmetric encprytion", () => {
     const message = "burn after reading";
 
     const ciphertext = encryptSym(key, message);
-
     expect(ciphertext).not.toEqual(message);
 
     const last5Chars = ciphertext.slice(-5);
@@ -32,5 +46,51 @@ describe("symmetric encprytion", () => {
 
     const repairedCiphertext = tamperedCiphertext.slice(0, -5) + last5Chars;
     expect(decryptSym(key, repairedCiphertext)).toEqual(message);
+  });
+});
+
+describe("asymmetric encryption", () => {
+  it("should produce correct ciphertext", () => {
+    const plaintext = "super secret message";
+
+    const ciphertext = encryptWithPublicKey(PUBLIC_KEY, plaintext);
+    const decrypted = decryptWithPrivateKey(PRIVATE_KEY, ciphertext);
+
+    expect(decrypted).toEqual(plaintext);
+  });
+
+  it("should not produce ciphertext decryptable with public key", () => {
+    const plaintext = "super secret message";
+
+    const ciphertext = encryptWithPublicKey(PUBLIC_KEY, plaintext);
+
+    const decryptWithPublicKey = () => {
+      return crypto
+        .publicDecrypt(PUBLIC_KEY, Buffer.from(ciphertext, "hex"))
+        .toString("utf-8");
+    };
+    expect(() => decryptWithPublicKey()).toThrow();
+  });
+});
+
+describe("encryptData", () => {
+  it("should encrypt small data correctly", () => {
+    const plaintext = "boo!";
+
+    const encrypted = encryptData(plaintext, PUBLIC_KEY);
+    expect(encrypted).not.toEqual(plaintext);
+
+    const decrypted = decryptData(encrypted, PRIVATE_KEY);
+    expect(decrypted).toEqual(plaintext);
+  });
+
+  it("should encrypt large data correctly", () => {
+    const plaintext = "super secret message".repeat(500);
+
+    const encrypted = encryptData(plaintext, PUBLIC_KEY);
+    expect(encrypted).not.toEqual(plaintext);
+
+    const decrypted = decryptData(encrypted, PRIVATE_KEY);
+    expect(decrypted).toEqual(plaintext);
   });
 });
--- app/audit/crypto.ts
+++ app/audit/crypto.ts
@@ -1,11 +1,19 @@
 import * as crypto from "crypto";
 import { Buffer } from "buffer";
 
+/*
+ * The decryption functions in this file serve only as as PoC for audit log decrytion performed
+ * by the auditor externally. They are not intended for invokation by the app in production as the app
+ * will not have access to the private key.
+ */
+
 const ALGORITHM = "aes-128-cbc";
 const HMAC_ALGORITHM = "sha256";
 const INPUT_ENCODING = "utf-8";
 const OUTPUT_ENCODING = "hex";
 const CIPHER_BLOCK_SIZE = 16;
+const SYM_SEPARATOR = ":";
+const ASYM_SEPARATOR = "|";
 
 /**
  * Encrypts the given plaintext using AES.
@@ -23,13 +31,13 @@ export const encryptSym = (key: Buffer, plaintext: string) => {
   let encryptedText = cipher.update(plaintext, INPUT_ENCODING, OUTPUT_ENCODING);
   encryptedText += cipher.final(OUTPUT_ENCODING);
   const ivAndEncryptedText = [iv.toString(OUTPUT_ENCODING), encryptedText].join(
-    ":"
+    SYM_SEPARATOR
   );
 
   const hmac = crypto.createHmac(HMAC_ALGORITHM, macKey);
-  hmac.update(ivAndEncryptedText, "utf-8");
+  hmac.update(ivAndEncryptedText, INPUT_ENCODING);
 
-  return [ivAndEncryptedText, hmac.digest(OUTPUT_ENCODING)].join(":");
+  return [ivAndEncryptedText, hmac.digest(OUTPUT_ENCODING)].join(SYM_SEPARATOR);
 };
 
 /**
@@ -40,13 +48,13 @@ export const encryptSym = (key: Buffer, plaintext: string) => {
  * @return {string} the decrypted plaintext
  */
 export const decryptSym = (key: Buffer, ciphertext: string) => {
-  const [encodedIv, encryptedText, authTag] = ciphertext.split(":");
+  const [encodedIv, encryptedText, authTag] = ciphertext.split(SYM_SEPARATOR);
   const iv = Buffer.from(encodedIv, OUTPUT_ENCODING);
   const encKey = key.slice(0, 16);
   const macKey = key.slice(16);
 
   const hmac = crypto.createHmac(HMAC_ALGORITHM, macKey);
-  hmac.update([encodedIv, encryptedText].join(":"), "utf-8");
+  hmac.update([encodedIv, encryptedText].join(SYM_SEPARATOR), INPUT_ENCODING);
 
   if (!authTag || hmac.digest(OUTPUT_ENCODING) !== authTag) {
     throw new Error("Message authentication failed.");
@@ -60,4 +68,61 @@ export const decryptSym = (key: Buffer, ciphertext: string) => {
   );
   plaintext += decipher.final(INPUT_ENCODING);
   return plaintext;
+};
+
+export const encryptWithPublicKey = (publicKey: Buffer, plaintext: string) => {
+  return crypto
+    .publicEncrypt(
+      {
+        key: publicKey,
+        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      },
+      Buffer.from(plaintext, INPUT_ENCODING)
+    )
+    .toString(OUTPUT_ENCODING);
+};
+
+export const decryptWithPrivateKey = (
+  privateKey: Buffer,
+  encrypted: string
+) => {
+  return crypto
+    .privateDecrypt(privateKey, Buffer.from(encrypted, OUTPUT_ENCODING))
+    .toString(INPUT_ENCODING);
+};
+
+/**
+ * Encrypts the plaintext data using a randomly generated secure symmetric key, encrypts this key
+ * with the given RSA public key, and returns both values in hex encoding separated by ASYM_SEPARATOR.
+ *
+ * @param data the plaintext data to encrypt
+ * @param publicKey an RSA public key in PEM format
+ * @return {string} hex-encoded encrypted symmetric key + ASYM_SEPARATOR + hex-encoded encrypted data
+ */
+export const encryptData = (data: string, publicKey: Buffer) => {
+  const symKey = crypto.randomBytes(CIPHER_BLOCK_SIZE * 2);
+  const encryptedData = encryptSym(symKey, data);
+  const encryptedSymKey = encryptWithPublicKey(
+    publicKey,
+    symKey.toString(OUTPUT_ENCODING)
+  );
+
+  return [encryptedSymKey, encryptedData].join(ASYM_SEPARATOR);
+};
+
+/**
+ * Decrypts the data encrypted with {@link encryptData} using the corresponding private key.
+ *
+ * @param encryptedBlock hex-encoded encrypted symmetric key + ASYM_SEPARATOR + hex-encoded encrypted data
+ * @param privateKey the RSA private key for the public key used to encrypt data
+ * @return {string} plaintext data
+ */
+export const decryptData = (encryptedBlock: string, privateKey: Buffer) => {
+  const [encryptedSymKey, encryptedData] = encryptedBlock.split(ASYM_SEPARATOR);
+
+  const symKey = Buffer.from(
+    decryptWithPrivateKey(privateKey, encryptedSymKey),
+    OUTPUT_ENCODING
+  );
+  return decryptSym(symKey, encryptedData);
 };
