66f7799a957691e77e7db56153390b968636754b
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-05-16 19:58:33 +0200

    Style Select component

--- app/components/form/Select.tsx
+++ app/components/form/Select.tsx
@@ -39,13 +39,13 @@ export default function Select(props: SelectProps) {
       id={id}
       defaultValue={value || defaultValue}
       className={classNames(
-        "block w-full",
+        "select block w-full",
         "bg-white text-black",
         "border-2 border-blue-800",
         "hover:outline hover:outline-4 hover:outline-blue-800 hover:outline-offset-[-4px]",
         "focus:outline focus:outline-4 focus:outline-blue-800 focus:outline-offset-[-4px]",
         "disabled:!text-gray-600 disabled:!border-gray-600 disabled:!outline-none",
-        "px-24 min-h-[4rem] text-18 leading-24",
+        "px-24 min-h-[4rem] text-18",
         {
           "border-red-800 bg-red-200": error,
         }
