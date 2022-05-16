9aa77875534621f612e5b2b3f090c475845cf71a
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-05-16 14:45:58 +0200

    Parameterize audit export file.

--- audit-export.server.ts
+++ audit-export.server.ts
@@ -1,9 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import fs from "fs";
 
-const EXPORT_FILENAME = "encrypted_logs";
-
-async function exportAuditLogs(from: string, to: string) {
+async function exportAuditLogs(from: string, to: string, pathToFile: string) {
   console.log(`Exporting audit logs from ${from} to ${to}`);
   const db = new PrismaClient();
   const auditLogs = await db.auditLog.findMany({
@@ -14,23 +12,23 @@ async function exportAuditLogs(from: string, to: string) {
       },
     },
   });
-  fs.writeFileSync(EXPORT_FILENAME, "");
-  auditLogs.forEach((log) =>
-    fs.appendFileSync(EXPORT_FILENAME, log.data + "\n")
-  );
+  fs.writeFileSync(pathToFile, "");
+  auditLogs.forEach((log) => fs.appendFileSync(pathToFile, log.data + "\n"));
 }
 
 function usage() {
-  console.log("Usage: npm run audit:export from to");
-  console.log("Example: npm run audit:export 2022-05-13 2022-05-14");
+  console.log("Usage: npm run audit:export from to path-to-file");
+  console.log(
+    "Example: npm run audit:export 2022-05-13 2022-05-14 /tpm/encrypted_logs"
+  );
 }
 
 function main() {
   const args = process.argv.slice(2);
-  if (args.length < 2) {
+  if (args.length < 3) {
     usage();
   } else {
-    exportAuditLogs(args[0], args[1]);
+    exportAuditLogs(args[0], args[1], args[2]);
   }
 }
 
