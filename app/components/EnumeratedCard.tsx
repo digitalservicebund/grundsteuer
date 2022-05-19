46acee39805b3757c11b1eeb8b6047190d8a7364
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-05-19 13:42:11 +0200

    Add mobile version to enumerated card

--- app/components/EnumeratedCard.tsx
+++ app/components/EnumeratedCard.tsx
@@ -10,17 +10,29 @@ export default function EnumeratedCard(props: {
   className?: string;
 }) {
   return (
-    <div className={classNames("py-24 px-32 enumerate-card", props.className)}>
+    <div
+      className={classNames(
+        "py-24 lg:px-32 px-24 enumerate-card flex-col md:flex-row",
+        props.className
+      )}
+    >
       <img
         src={props.image}
         alt={props.imageAltText}
-        className="mr-24 w-[300px] h-[180px]"
+        className="mr-24 md:w-[300px] md:h-[180px] w-full"
       />
-      <div className="mr-8 enumerate-icon">{props.number}</div>
-      <dl>
-        <dt className="mb-8 mt-8 text-18">{props.heading}</dt>
-        <dd>{props.text}</dd>
-      </dl>
+      <div className="flex flex-col">
+        <dl>
+          <div className="flex flex-row mt-16 lg:mt-0">
+            <div className="mr-8 enumerate-icon">{props.number}</div>
+            <dt className="mb-8 mt-8 text-18">{props.heading}</dt>
+          </div>
+          <div className="flex flex-row mt-16 lg:mt-0">
+            <div className="hidden sm:block mr-8 w-[40px] min-w-[40px]" />
+            <dd>{props.text}</dd>
+          </div>
+        </dl>
+      </div>
     </div>
   );
 }
