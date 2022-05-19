96a9fa603c7a4391cfa01794ab33360e26e760f5
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-05-19 12:19:00 +0200

    Add tests for auth.server

---
+++ app/auth.server.test.ts
@@ -0,0 +1,99 @@
+import * as userModule from "~/domain/user";
+import bcrypt from "bcryptjs";
+import { authenticator } from "~/auth.server";
+
+describe("authenticator.isAuthenticated", () => {
+  const correctPassword = "12345678";
+  const email = "found@foo.com";
+
+  describe("with user not found", () => {
+    beforeAll(async () => {
+      await bcrypt.hash(correctPassword, 10);
+      jest
+        .spyOn(userModule, "findUserByEmail")
+        .mockImplementation(
+          jest.fn(() => Promise.resolve(undefined)) as jest.Mock
+        );
+    });
+
+    it("throws error", async () => {
+      const body = new FormData();
+      body.set("email", "unknown@foo.com");
+      body.set("password", correctPassword);
+      const request = new Request("", { body, method: "POST" });
+
+      await expect(
+        authenticator.authenticate("user-pass", request, {
+          successRedirect: "/",
+          throwOnError: true,
+        })
+      ).rejects.toThrow();
+    });
+  });
+
+  describe("with user found", () => {
+    beforeAll(async () => {
+      const password_hash = await bcrypt.hash(correctPassword, 10);
+      jest.spyOn(userModule, "findUserByEmail").mockImplementation(
+        jest.fn(() =>
+          Promise.resolve({
+            email: email,
+            id: 1,
+            identified: false,
+            password: password_hash,
+          })
+        ) as jest.Mock
+      );
+    });
+
+    it("throws error if incorrect password used", async () => {
+      const body = new FormData();
+      body.set("email", email);
+      body.set("password", "incorrectPassword");
+      const request = new Request("", { body, method: "POST" });
+
+      await expect(
+        authenticator.authenticate("user-pass", request, {
+          successRedirect: "/",
+          throwOnError: true,
+        })
+      ).rejects.toThrow();
+    });
+
+    it("returns redirect if correct password used", async () => {
+      const body = new FormData();
+      body.set("email", email);
+      body.set("password", correctPassword);
+      const request = new Request("", { body, method: "POST" });
+
+      await authenticator
+        .authenticate("user-pass", request, {
+          successRedirect: "/redirectionUrl",
+          throwOnError: true,
+        })
+        .catch((response) => {
+          console.log(response);
+          expect(response.status).toEqual(302);
+          expect(response.headers.get("Location")).toEqual("/redirectionUrl");
+        });
+    });
+
+    it("returns redirect if if correct password used and capitalized email", async () => {
+      const body = new FormData();
+      body.set("email", email.toUpperCase());
+      body.set("password", correctPassword);
+      const request = new Request("", { body, method: "POST" });
+
+      await authenticator
+        .authenticate("user-pass", request, {
+          successRedirect: "/redirectionUrl",
+          throwOnError: true,
+        })
+        .catch((response) => {
+          console.log(response);
+          expect(response.status).toEqual(302);
+          expect(response.headers.get("Location")).toEqual("/redirectionUrl");
+        });
+    });
+  });
+});
