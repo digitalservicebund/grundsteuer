ba3d1498b8278edc14b44c9f8969a0c37910576c
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-05-19 13:18:29 +0200

    Style fsc beantragen erfolg

--- app/routes/fsc/beantragen/erfolgreich.tsx
+++ app/routes/fsc/beantragen/erfolgreich.tsx
@@ -7,6 +7,9 @@ import {
   IntroText,
 } from "~/components";
 import { pageTitle } from "~/util/pageTitle";
+import UebersichtStep from "~/components/form/UebersichtStep";
+import erfolgImageSmall from "~/assets/images/erfolg-small.svg";
+import erfolgImageMedium from "~/assets/images/erfolg-medium.svg";
 
 export const meta: MetaFunction = () => {
   return { title: pageTitle("Freischaltcode erfolgreich beantragt") };
@@ -15,15 +18,20 @@ export const meta: MetaFunction = () => {
 export default function FscBeantragenErfolgreich() {
   return (
     <ContentContainer size="sm">
-      <BreadcrumbNavigation />
-      <Headline>Vielen Dank</Headline>
+      <UebersichtStep
+        imageSrc={erfolgImageMedium}
+        smallImageSrc={erfolgImageSmall}
+      >
+        <BreadcrumbNavigation />
+        <Headline>Vielen Dank</Headline>
 
-      <IntroText className="mb-80">
-        Sie haben ein Konto erstellt und Ihren persönlichen Freischaltcode
-        beantragt. Diesen erhalten Sie in den nächsten 14 Tagen per Post.
-      </IntroText>
+        <IntroText className="mb-80">
+          Sie haben ein Konto erstellt und Ihren persönlichen Freischaltcode
+          beantragt. Diesen erhalten Sie in den nächsten 14 Tagen per Post.
+        </IntroText>
 
-      <Button to="/formular/welcome">Weiter zum Formular</Button>
+        <Button to="/formular/welcome">Weiter zum Formular</Button>
+      </UebersichtStep>
     </ContentContainer>
   );
 }
