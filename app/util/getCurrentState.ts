3cd5a8c57071cc5d311e00ebf253812d162e0d3b
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-06-02 11:58:03 +0200

    Remove circular dependency

--- app/util/getCurrentState.ts
+++ app/util/getCurrentState.ts
@@ -1,6 +1,3 @@
-import { PREFIX as PRUEFEN_PREFIX } from "~/routes/pruefen/_step";
-import { PREFIX as FORMULAR_PREFIX } from "~/routes/formular/_step";
-
 export const getCurrentStateFromUrl = (url: string) => {
   return getCurrentStateFromPathname(new URL(url).pathname);
 };
@@ -8,7 +5,7 @@ export const getCurrentStateFromUrl = (url: string) => {
 export const getCurrentStateFromPathname = (pathname: string) => {
   return pathname
     .split("/")
-    .filter((e) => e && e !== FORMULAR_PREFIX && e !== PRUEFEN_PREFIX)
+    .filter((e) => e && e !== "formular" && e !== "pruefen")
     .join(".");
 };
 
