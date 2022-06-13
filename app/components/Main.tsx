191f08eaac50feb249bfaa24bc8c1dd98a30a465
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-06-13 14:53:31 +0200

    Increase main padding left

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Main(props: { children: ReactNode }) {
   return (
-    <div className="px-16 pt-16 pb-80 bg-gray-100 md:pl-128 lg:pt-32 lg:px-32">
+    <div className="px-16 pt-16 pb-80 bg-gray-100 md:pl-128 lg:pt-32 pr-32">
       {props.children}
     </div>
   );
