0d7b71b07b3d6b427be62da22f7292a5a1aae317
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-06-15 15:21:16 +0200

    Rename erfolg -> success

--- app/components/SuccessPageLayout.tsx
+++ app/components/SuccessPageLayout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import { UebersichtStep } from "~/components";
-import imageSrc from "~/assets/images/erfolg-medium.svg";
-import smallImageSrc from "~/assets/images/erfolg-small.svg";
+import imageSrc from "~/assets/images/success-medium.svg";
+import smallImageSrc from "~/assets/images/success-small.svg";
 
 export default function SuccessPageLayout(props: { children: ReactNode }) {
   return (
