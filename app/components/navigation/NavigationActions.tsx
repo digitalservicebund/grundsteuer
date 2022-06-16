62cc6f91e3ff7b230b010811725f53f99380026d
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-06-16 12:19:42 +0200

    Add menu for not-logged-in-user

--- app/components/navigation/NavigationActions.tsx
+++ app/components/navigation/NavigationActions.tsx
@@ -4,15 +4,38 @@ import { LogoutButton } from "~/components";
 import EmailOutlinedIcon from "~/components/icons/mui/EmailOutlined";
 import DriveFileRenameOutlineIcon from "~/components/icons/mui/DriveFileRenameOutline";
 import NavigationLink from "./NavigationLink";
+import PersonCircle from "~/components/icons/mui/PersonCircle";
+import LetterIcon from "~/components/icons/mui/LetterIcon";
 
 export default function NavigationActions(props: {
   userIsIdentified?: boolean;
   userIsLoggedIn?: boolean;
 }) {
-  if (!props.userIsLoggedIn) return null;
-
   const location = useLocation();
   const [currentLocation, setCurrentLocation] = useState(location.pathname);
+
+  if (!props.userIsLoggedIn) {
+    return (
+      <div className="px-8 mb-32">
+        <NavigationLink
+          to="/anmelden"
+          icon={<PersonCircle className="w-24 h-24 fill-blue-800" />}
+          isAllCaps
+          isActive={!!currentLocation.match(/\/anmelden/)}
+        >
+          Anmelden
+        </NavigationLink>
+        <NavigationLink
+          to="/hilfe"
+          icon={<LetterIcon className="w-24 h-24 fill-blue-800" />}
+          isAllCaps
+          isActive={!!currentLocation.match(/\/hilfe/)}
+        >
+          Kontakt
+        </NavigationLink>
+      </div>
+    );
+  }
 
   useEffect(() => {
     setCurrentLocation(location.pathname);
