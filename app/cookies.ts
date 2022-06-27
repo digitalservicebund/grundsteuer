899364acc488bb63e79f564d8945c9ad14c2e491
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-06-27 09:27:34 +0200

    Encrypt pruefen cookie

--- app/cookies.ts
+++ app/cookies.ts
@@ -1,5 +1,20 @@
 import { createCookie } from "@remix-run/node";
+import { useSecureCookie } from "~/util/useSecureCookie";
+import invariant from "tiny-invariant";
 
-export const pruefenStateCookie = createCookie("pruefen_state", {
-  maxAge: 604_800, // one week
-});
+const getPruefenStateCookie = () => {
+  invariant(
+    typeof process.env.FORM_COOKIE_SECRET === "string",
+    "environment variable FORM_COOKIE_SECRET is not set"
+  );
+
+  return createCookie("pruefen_state", {
+    path: "/",
+    maxAge: 604_800 * 13, // 13 weeks (1/4 year)
+    httpOnly: true,
+    sameSite: "strict",
+    secure: useSecureCookie,
+    secrets: [process.env.FORM_COOKIE_SECRET],
+  });
+};
+export const pruefenStateCookie = getPruefenStateCookie();
