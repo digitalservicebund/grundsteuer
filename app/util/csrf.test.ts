1cd6f06d8dd303549f24ebfef7fc1d1ef04f3874
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-06-30 17:28:28 +0200

    Mitigate http 400 error

---
+++ app/util/csrf.test.ts
@@ -0,0 +1,92 @@
+import {
+  appendCsrfToken,
+  createCsrfToken,
+  formTokenIsValid,
+  SESSION_KEY,
+} from "./csrf";
+import { createSession } from "@remix-run/node";
+
+describe("appendCsrfToken", () => {
+  describe("with empty session", () => {
+    test("token is added", () => {
+      const session = createSession({});
+      const token = "newToken";
+      appendCsrfToken(session, token);
+      expect(session.has(SESSION_KEY)).toBe(true);
+      expect(JSON.parse(session.get(SESSION_KEY))).toEqual([token]);
+    });
+  });
+
+  describe("with existing tokens in session", () => {
+    test("token is prepended and max 5 tokens are kept", () => {
+      const fourTokens = ["token1", "token2", "token3", "token4"];
+      const session = createSession({
+        [SESSION_KEY]: JSON.stringify([...fourTokens, "token5"]),
+      });
+      const token = "newToken";
+      appendCsrfToken(session, token);
+      expect(JSON.parse(session.get(SESSION_KEY))).toEqual([
+        token,
+        ...fourTokens,
+      ]);
+    });
+  });
+
+  describe("with an old existing string token in session", () => {
+    test("token is prepended", () => {
+      const existingToken = "existingToken";
+      const session = createSession({ [SESSION_KEY]: existingToken });
+      const token = "newToken";
+      appendCsrfToken(session, token);
+      expect(JSON.parse(session.get(SESSION_KEY))).toEqual([
+        token,
+        existingToken,
+      ]);
+    });
+  });
+});
+
+describe("createCsrfToken", () => {
+  test("token of length 36 is added to session", () => {
+    const session = createSession({});
+    const token = createCsrfToken(session);
+    expect(session.has(SESSION_KEY)).toBe(true);
+    expect(token.length).toEqual(36);
+  });
+});
+
+describe("formTokenIsValid", () => {
+  describe("with empty session", () => {
+    test("it is invalid", () => {
+      const session = createSession({});
+      const formToken = "formToken";
+      expect(formTokenIsValid(session, formToken)).toBe(false);
+    });
+  });
+
+  describe("with matching token in session", () => {
+    test("it is valid", () => {
+      const formToken = "formToken";
+      const session = createSession({
+        [SESSION_KEY]: JSON.stringify(["token1", "token2", formToken]),
+      });
+      expect(formTokenIsValid(session, formToken)).toBe(true);
+    });
+  });
+
+  describe("with an old matching string token in session", () => {
+    test("it is valid", () => {
+      const formToken = "formToken";
+      const session = createSession({ [SESSION_KEY]: formToken });
+      expect(formTokenIsValid(session, formToken)).toBe(true);
+    });
+  });
+
+  describe("with an old not matching string token in session", () => {
+    test("it is invalid", () => {
+      const formToken = "formToken";
+      const session = createSession({ [SESSION_KEY]: "notMatchingToken" });
+      expect(formTokenIsValid(session, formToken)).toBe(false);
+    });
+  });
+});
--- app/util/csrf.tsx
+++ app/util/csrf.tsx
@@ -2,17 +2,45 @@ import { Session } from "@remix-run/node";
 import { v4 as uuid } from "uuid";
 import { getSession } from "~/session.server";
 
-const SESSION_KEY = "csrf";
+export const SESSION_KEY = "csrf";
+const TOKENS_TO_STORE_COUNT = 5;
 
 export const CsrfToken = (props: { value?: string }) => {
   if (!props.value) return null;
   return <input type="hidden" value={props.value} name="csrf" />;
 };
 
+export const appendCsrfToken = (session: Session, token: string) => {
+  let existingTokens: string[] = [];
+  if (session.has(SESSION_KEY)) {
+    const sessionContent = session.get(SESSION_KEY);
+    try {
+      existingTokens = JSON.parse(sessionContent);
+    } catch {
+      // old session cookie with single string csrf token
+      existingTokens = [sessionContent];
+    }
+  }
+  const newTokens = [token, ...existingTokens].splice(0, TOKENS_TO_STORE_COUNT);
+  session.set(SESSION_KEY, JSON.stringify(newTokens));
+};
+
 export const createCsrfToken = (session: Session) => {
   const token = uuid();
-  session.set(SESSION_KEY, token);
+  appendCsrfToken(session, token);
   return token;
+};
+
+export const formTokenIsValid = (session: Session, formToken: string) => {
+  const sessionContent = session.get(SESSION_KEY);
+  let tokens: string[] = [];
+  try {
+    tokens = JSON.parse(sessionContent);
+  } catch {
+    // old session cookie with single string csrf token
+    tokens = [sessionContent];
+  }
+  return tokens.includes(formToken);
 };
 
 export const verifyCsrfToken = async (request: Request) => {
@@ -24,7 +52,7 @@ export const verifyCsrfToken = async (request: Request) => {
   if (
     !session.has(SESSION_KEY) ||
     !formToken ||
-    session.get(SESSION_KEY) !== formToken
+    !formTokenIsValid(session, formToken as string)
   ) {
     throw new Response("Cannot verify CSRF token.", { status: 400 });
   }
