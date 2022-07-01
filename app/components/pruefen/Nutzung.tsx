429f00b2006ae882266088e31ad9d21d94186bcb
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-07-01 14:42:51 +0200

    Revert "Temporarily adapt pruefen success text"
    This reverts commit 2e1885573d440b721722e3b76725a60efbc3b033.

--- app/components/pruefen/Nutzung.tsx
+++ app/components/pruefen/Nutzung.tsx
@@ -1,9 +1,11 @@
 import { StepComponentFunction } from "~/routes/pruefen/_step";
+import IntroText from "../IntroText";
 import successImage from "~/assets/images/pruefen-yes.svg";
 
-const Nutzung: StepComponentFunction = () => {
+const Nutzung: StepComponentFunction = ({ i18n }) => {
   return (
     <div>
+      <IntroText className="mb-32">{i18n.specifics.explanation}</IntroText>
       <img src={successImage} alt="" className="mb-80 mx-auto" />
     </div>
   );
