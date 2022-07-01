0dc4c59bf1bc939dfbf4d88892785fc06ec15a7d
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-07-01 11:45:04 +0200

    Hide pruefen button on pruefen page + styling

--- app/components/HomepageHeader.tsx
+++ app/components/HomepageHeader.tsx
@@ -23,7 +23,7 @@ export function HomepageHeader({
       : { to: "/pruefen/start" };
 
   return (
-    <div className="mb-10 md:mb-48 lg:mb-36 bg-white py-32">
+    <div className="mb-32 md:mb-64 bg-white py-32">
       <ContentContainer className="w-full flex flex-col md:flex-row md:justify-between">
         <div className="inline-block">
           <a href="/" title="Zur Startseite">
@@ -32,13 +32,13 @@ export function HomepageHeader({
         </div>
         <div className="inline-block">
           <div className="flex justify-center md:block">
-            <div className="lg:hidden inline-flex flex-col md:flex-row md:pt-32">
+            <div className="lg:hidden inline-flex flex-col md:flex-row md:pt-32 mt-32">
               <Button
                 size="medium"
                 look="tertiary"
                 disabled
                 className={classNames("mb-16 text-center md:mb-0 md:mr-24", {
-                  "bg-blue-200 shadow-none": pruefenActive,
+                  hidden: pruefenActive,
                 })}
                 {...pruefenButtonProps}
               >
@@ -48,12 +48,12 @@ export function HomepageHeader({
                 {t("homepage.buttonStart")}
               </Button>
             </div>
-            <div className="hidden lg:block pt-16">
+            <div className="hidden lg:block">
               <Button
                 look="tertiary"
                 disabled
                 className={classNames("mr-24", {
-                  "bg-blue-200 shadow-none": pruefenActive,
+                  hidden: pruefenActive,
                 })}
                 {...pruefenButtonProps}
               >
