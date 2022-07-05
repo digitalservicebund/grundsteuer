5770b01760a0e62a69f9d66602bdcdc3375ec47b
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-07-05 17:16:20 +0200

    Increase possible eigentuemer to 10

--- app/domain/steps/eigentuemer/anzahl.ts
+++ app/domain/steps/eigentuemer/anzahl.ts
@@ -1,7 +1,7 @@
 import { StepDefinition } from "~/domain/steps";
 
 export type EigentuemerAnzahlFields = {
-  anzahl: "1" | "2" | "3" | "4" | "5";
+  anzahl: "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10";
 };
 
 export const eigentuemerAnzahl: StepDefinition = {
@@ -17,6 +17,11 @@ export const eigentuemerAnzahl: StepDefinition = {
         { value: "3" },
         { value: "4" },
         { value: "5" },
+        { value: "6" },
+        { value: "7" },
+        { value: "8" },
+        { value: "9" },
+        { value: "10" },
       ],
     },
   },
