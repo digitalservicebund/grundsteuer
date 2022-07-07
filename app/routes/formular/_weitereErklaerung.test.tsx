b9f7303bb1616ed65ce83b2627c5c669703e9153
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-07-07 09:59:32 +0200

    Restructure weitereErklaerung tests

--- app/routes/formular/_weitereErklaerung.test.tsx
+++ app/routes/formular/_weitereErklaerung.test.tsx
@@ -18,98 +18,106 @@ describe("/weitereErklaerung action", () => {
     jest.restoreAllMocks();
   });
 
-  test("returns validation error if no data given", async () => {
-    const args = await mockActionArgs({
-      route: "/formular/weitereErklaerung",
-      formData: {},
-      context: {},
+  describe("without data given", () => {
+    test("returns validation error", async () => {
+      const args = await mockActionArgs({
+        route: "/formular/weitereErklaerung",
+        formData: {},
+        context: {},
+      });
+      const result = await action(args);
+      expect(result).toEqual({
+        errors: { datenUebernehmen: "errors.required" },
+      });
     });
-    const result = await action(args);
-    expect(result).toEqual({ errors: { datenUebernehmen: "errors.required" } });
   });
 
-  test("redirects to formular start if datenUebernehmen is true", async () => {
-    const args = await mockActionArgs({
-      route: "/formular/weitereErklaerung",
-      formData: { datenUebernehmen: "true" },
-      context: {},
+  describe("with datenUebernehmen true", () => {
+    test("redirects to formular start", async () => {
+      const args = await mockActionArgs({
+        route: "/formular/weitereErklaerung",
+        formData: { datenUebernehmen: "true" },
+        context: {},
+      });
+      const result = await action(args);
+      expect(result.status).toEqual(302);
+      expect(result.headers.get("Location")).toEqual("/formular/welcome");
     });
-    const result = await action(args);
-    expect(result.status).toEqual(302);
-    expect(result.headers.get("Location")).toEqual("/formular/welcome");
-  });
 
-  test("redirects to formular start if datenUebernehmen is false", async () => {
-    const args = await mockActionArgs({
-      route: "/formular/weitereErklaerung",
-      formData: { datenUebernehmen: "false" },
-      context: {},
-    });
-    const result = await action(args);
-    expect(result.status).toEqual(302);
-    expect(result.headers.get("Location")).toEqual("/formular/welcome");
-  });
-
-  test("clears formData if datenUebernehmen is false", async () => {
-    const args = await mockActionArgs({
-      route: "/formular/weitereErklaerung",
-      formData: { datenUebernehmen: "false" },
-      allData: grundModelFactory.full().build(),
-      context: {},
-    });
-    const result = await action(args);
-    const decodedCookieData = await decodeFormDataCookie({
-      cookieHeader: result.headers.get("Set-Cookie"),
-      user: sessionUserFactory.build({ id: "1" }),
-    });
-    expect(decodedCookieData).toEqual({});
-  });
-
-  test("keeps relevant formData if datenUebernehmen is true", async () => {
-    const person1ExpectedData = {
-      persoenlicheAngaben: {
-        anrede: "frau" as const,
-        titel: "",
-        vorname: "Vorname",
-        name: "Name",
-        geburtsdatum: "01.01.1980",
-      },
-      adresse: { ort: "Ort", plz: "12345" },
-      steuerId: { steuerId: "" },
-      gesetzlicherVertreter: { hasVertreter: "true" as const },
-      vertreter: {
+    test("keeps relevant formData", async () => {
+      const person1ExpectedData = {
         persoenlicheAngaben: {
-          anrede: "herr" as const,
-          titel: "Prof.",
-          vorname: "Vert Vorname",
-          name: "Vert Name",
-          geburtsdatum: "02.02.1980",
+          anrede: "frau" as const,
+          titel: "",
+          vorname: "Vorname",
+          name: "Name",
+          geburtsdatum: "01.01.1980",
         },
         adresse: { ort: "Ort", plz: "12345" },
-      },
-    };
-    const fullPerson1Data = {
-      ...person1ExpectedData,
-      ...{ anteil: { zaehler: "2", nenner: "3" } },
-    };
-    const args = await mockActionArgs({
-      route: "/formular/weitereErklaerung",
-      formData: { datenUebernehmen: "true" },
-      allData: grundModelFactory
-        .full()
-        .eigentuemerPerson({ list: [fullPerson1Data, fullPerson1Data] })
-        .build(),
-      context: {},
+        steuerId: { steuerId: "" },
+        gesetzlicherVertreter: { hasVertreter: "true" as const },
+        vertreter: {
+          persoenlicheAngaben: {
+            anrede: "herr" as const,
+            titel: "Prof.",
+            vorname: "Vert Vorname",
+            name: "Vert Name",
+            geburtsdatum: "02.02.1980",
+          },
+          adresse: { ort: "Ort", plz: "12345" },
+        },
+      };
+      const fullPerson1Data = {
+        ...person1ExpectedData,
+        ...{ anteil: { zaehler: "2", nenner: "3" } },
+      };
+      const args = await mockActionArgs({
+        route: "/formular/weitereErklaerung",
+        formData: { datenUebernehmen: "true" },
+        allData: grundModelFactory
+          .full()
+          .eigentuemerPerson({ list: [fullPerson1Data, fullPerson1Data] })
+          .build(),
+        context: {},
+      });
+      const result = await action(args);
+      const decodedCookieData = await decodeFormDataCookie({
+        cookieHeader: result.headers.get("Set-Cookie"),
+        user: sessionUserFactory.build({ id: "1" }),
+      });
+      expect(decodedCookieData).toEqual(
+        grundModelFactory
+          .eigentuemerPerson({ list: [person1ExpectedData] })
+          .build()
+      );
     });
-    const result = await action(args);
-    const decodedCookieData = await decodeFormDataCookie({
-      cookieHeader: result.headers.get("Set-Cookie"),
-      user: sessionUserFactory.build({ id: "1" }),
+  });
+
+  describe("with datenUebernehmen false", () => {
+    test("redirects to formular start if datenUebernehmen is false", async () => {
+      const args = await mockActionArgs({
+        route: "/formular/weitereErklaerung",
+        formData: { datenUebernehmen: "false" },
+        context: {},
+      });
+      const result = await action(args);
+      expect(result.status).toEqual(302);
+      expect(result.headers.get("Location")).toEqual("/formular/welcome");
     });
-    expect(decodedCookieData).toEqual(
-      grundModelFactory
-        .eigentuemerPerson({ list: [person1ExpectedData] })
-        .build()
-    );
+
+    test("clears formData if datenUebernehmen is false", async () => {
+      const args = await mockActionArgs({
+        route: "/formular/weitereErklaerung",
+        formData: { datenUebernehmen: "false" },
+        allData: grundModelFactory.full().build(),
+        context: {},
+      });
+      const result = await action(args);
+      const decodedCookieData = await decodeFormDataCookie({
+        cookieHeader: result.headers.get("Set-Cookie"),
+        user: sessionUserFactory.build({ id: "1" }),
+      });
+      expect(decodedCookieData).toEqual({});
+    });
   });
 });
