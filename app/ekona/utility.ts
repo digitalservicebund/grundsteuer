8546561e42159f7a287a2ca01907e5b1cfd56799
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-07-17 22:22:26 +0200

    Add SessionCacheItemProvider

---
+++ app/ekona/utility.ts
@@ -0,0 +1,31 @@
+import { CacheProvider } from "node-saml";
+import { Session } from "@remix-run/node";
+
+export class SessionCacheProvider implements CacheProvider {
+  session: Session;
+
+  constructor(session: Session) {
+    this.session = session;
+  }
+
+  // Store an item in the cache, using the specified key and value.
+  async saveAsync(key: string, value: string) {
+    this.session.set(key, value);
+    return Object.assign({
+      key: key,
+      value: value,
+      createdAt: new Date().getTime(),
+    });
+  }
+
+  // Returns the value of the specified key in the cache
+  async getAsync(key: string) {
+    return this.session.get(key);
+  }
+  // Removes an item from the cache if the key exists
+  async removeAsync(key: string) {
+    const oldValue = this.session.get(key);
+    this.session.unset(key);
+    return oldValue;
+  }
+}
