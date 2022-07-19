a8700024187f33ab5a2379c369f42a266d019a47
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-07-19 12:00:05 +0200

    Add information banner about email outage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -23,13 +23,19 @@ export default function Index() {
 
   return (
     <>
-      {false && (
-        <div className="flex-shrink-0 bg-yellow-300 border-l-[9px] border-l-yellow-500 py-16 lg:py-28">
-          <ContentContainer>
-            <div className="text-20 leading-26 lg:text-32 lg:leading-40"></div>
-          </ContentContainer>
-        </div>
-      )}
+      <div className="flex-shrink-0 bg-yellow-300 border-l-[9px] border-l-yellow-500 py-16">
+        <ContentContainer>
+          <p className="text-14 leading-20 lg:text-18 lg:leading-24">
+            <strong>Hinweis:</strong> E-Mails mit Links zur Anmeldung an
+            Adressen mit den Endungen <em>@live.de/.com</em>,{" "}
+            <em>@hotmail.de/.com</em>, <em>@outlook.de/.com</em> und{" "}
+            <em>@msn.de/.com</em> konnten leider temporär nicht zugestellt
+            werden. Das Problem wurde inzwischen behoben. Bitte melden Sie sich
+            erneut an. Es kann allerdings weiterhin zu Verzögerungen im Versand
+            kommen. Dies bedauern wir und danken für Ihre Geduld.
+          </p>
+        </ContentContainer>
+      </div>
       <HomepageHeader />
       <main className="flex-grow">
         <BreadcrumbNavigation />
