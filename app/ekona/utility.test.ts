ffe38deded1d11a70c21075f2ae93aca4a726e73
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-07-21 09:46:38 +0200

    Add functions to extract data from validated Ekona profile

---
+++ app/ekona/utility.test.ts
@@ -0,0 +1,297 @@
+import _ from "lodash";
+import {
+  checkDataForAttributes,
+  extractIdentData,
+  validatedEkonaData,
+} from "~/ekona/utility";
+
+const ekonaDataInland = {
+  issuer: "https://e4k-portal.een.elster.de",
+  inResponseTo: "_cb378be91188da32e3c6",
+  sessionIndex: "_946b9a312742e1f2963d0341f5f2f6c622b9c0e7",
+  IstTestkonto: "false",
+  DatenkranzTyp: "IdNr",
+  ElsterVertrauensniveauIdentifizierung: "substanziell",
+  ElsterVertrauensniveauAuthentifizierung: "substanziell",
+  IdNr: "59477301287",
+  Vorname: "Maré",
+  Name: "Renièr",
+  Geburtsdatum: "01.01.1989",
+  Anschrift: {
+    $: {
+      xmlnsekona: "http://www.elster.de/schema/ekona/saml/extensions",
+      xmlnsxsi: "http://www.w3.org/2001/XMLSchema-instance",
+      xsitype: "ekona:AdresseType",
+    },
+    Typ: [{ _: "Inland" }],
+    Strasse: [{ _: "Theaterstraße" }],
+    Hausnummer: [{ _: "58" }],
+    PLZ: [{ _: "90762" }],
+    Ort: [{ _: "Fürth" }],
+    Land: [{ _: "DE" }],
+    Adressergaenzung: [{ _: "EG" }],
+  },
+  attributes: {
+    IstTestkonto: "false",
+    DatenkranzTyp: "IdNr",
+    ElsterVertrauensniveauIdentifizierung: "substanziell",
+    ElsterVertrauensniveauAuthentifizierung: "substanziell",
+    IdNr: "59477301287",
+    Vorname: "Maré",
+    Name: "Renièr",
+    Geburtsdatum: "01.01.1989",
+    Anschrift: {
+      $: {
+        xmlnsekona: "http://www.elster.de/schema/ekona/saml/extensions",
+        xmlnsxsi: "http://www.w3.org/2001/XMLSchema-instance",
+        xsitype: "ekona:AdresseType",
+      },
+      Typ: [{ _: "Inland" }],
+      Strasse: [{ _: "Theaterstraße" }],
+      Hausnummer: [{ _: "58" }],
+      PLZ: [{ _: "90762" }],
+      Ort: [{ _: "Fürth" }],
+      Land: [{ _: "DE" }],
+      Adressergaenzung: [{ _: "EG" }],
+    },
+  },
+};
+const ekonaDataAusland = {
+  issuer: "https://e4k-portal.een.elster.de",
+  inResponseTo: "_7a130d8bacb6cceb6696",
+  sessionIndex: "_de58d799be53cc9f124ea6c899dcc28fc6115059",
+  IstTestkonto: "false",
+  DatenkranzTyp: "IdNr",
+  ElsterVertrauensniveauIdentifizierung: "substanziell",
+  ElsterVertrauensniveauAuthentifizierung: "substanziell",
+  IdNr: "21374069801",
+  Vorname: "Børge",
+  Name: "Nielsen",
+  Geburtsdatum: "01.01.1900",
+  Anschrift: {
+    $: {
+      xmlnsekona: "http://www.elster.de/schema/ekona/saml/extensions",
+      xmlnsxsi: "http://www.w3.org/2001/XMLSchema-instance",
+      xsitype: "ekonaAdresseType",
+    },
+    Typ: [{ _: "Ausland" }],
+    Strasse: [{ _: "Egtoftevej" }],
+    Hausnummer: [{ _: "55" }],
+    PLZ: [{ _: "2950" }],
+    Ort: [{ _: "Vedbæk" }],
+    Land: [{ _: "DK" }],
+  },
+  attributes: {
+    IstTestkonto: "false",
+    DatenkranzTyp: "IdNr",
+    ElsterVertrauensniveauIdentifizierung: "substanziell",
+    ElsterVertrauensniveauAuthentifizierung: "substanziell",
+    IdNr: "21374069801",
+    Vorname: "Børge",
+    Name: "Nielsen",
+    Geburtsdatum: "01.01.1900",
+    Anschrift: {
+      $: {
+        xmlnsekona: "http//www.elster.de/schema/ekona/saml/extensions",
+        xmlnsxsi: "http//www.w3.org/2001/XMLSchema-instance",
+        xsitype: "ekonaAdresseType",
+      },
+      Typ: [{ _: "Ausland" }],
+      Strasse: [{ _: "Egtoftevej" }],
+      Hausnummer: [{ _: "55" }],
+      PLZ: [{ _: "2950" }],
+      Ort: [{ _: "Vedbæk" }],
+      Land: [{ _: "DK" }],
+    },
+  },
+};
+
+const ekonaDataWithoutPLZ = {
+  issuer: "https://e4k-portal.een.elster.de",
+  inResponseTo: "_7a130d8bacb6cceb6696",
+  sessionIndex: "_de58d799be53cc9f124ea6c899dcc28fc6115059",
+  IstTestkonto: "false",
+  DatenkranzTyp: "IdNr",
+  ElsterVertrauensniveauIdentifizierung: "substanziell",
+  ElsterVertrauensniveauAuthentifizierung: "substanziell",
+  IdNr: "21374069801",
+  Vorname: "Børge",
+  Name: "Nielsen",
+  Geburtsdatum: "01.01.1900",
+  Anschrift: {
+    $: {
+      xmlnsekona: "http://www.elster.de/schema/ekona/saml/extensions",
+      xmlnsxsi: "http://www.w3.org/2001/XMLSchema-instance",
+      xsitype: "ekonaAdresseType",
+    },
+    Typ: [{ _: "Ausland" }],
+    Strasse: [{ _: "Egtoftevej" }],
+    Hausnummer: [{ _: "55" }],
+    Ort: [{ _: "Vedbæk" }],
+    Land: [{ _: "DK" }],
+  },
+  attributes: {
+    IstTestkonto: "false",
+    DatenkranzTyp: "IdNr",
+    ElsterVertrauensniveauIdentifizierung: "substanziell",
+    ElsterVertrauensniveauAuthentifizierung: "substanziell",
+    IdNr: "21374069801",
+    Vorname: "Børge",
+    Name: "Nielsen",
+    Geburtsdatum: "01.01.1900",
+    Anschrift: {
+      $: {
+        xmlnsekona: "http//www.elster.de/schema/ekona/saml/extensions",
+        xmlnsxsi: "http//www.w3.org/2001/XMLSchema-instance",
+        xsitype: "ekonaAdresseType",
+      },
+      Typ: [{ _: "Ausland" }],
+      Strasse: [{ _: "Egtoftevej" }],
+      Hausnummer: [{ _: "55" }],
+      Ort: [{ _: "Vedbæk" }],
+      Land: [{ _: "DK" }],
+    },
+  },
+};
+
+describe("extractIdentData", () => {
+  it("extracts correct data if all data available", () => {
+    const extractedData = extractIdentData(ekonaDataInland);
+    expect(extractedData).toEqual({
+      idnr: "59477301287",
+      firstName: "Maré",
+      lastName: "Renièr",
+      street: "Theaterstraße",
+      housenumber: "58",
+      postalCode: "90762",
+      city: "Fürth",
+      country: "DE",
+      addressSupplement: "EG",
+    });
+  });
+
+  it("extracts correct data if auslandsadresse", () => {
+    const extractedData = extractIdentData(ekonaDataAusland);
+    expect(extractedData).toEqual({
+      idnr: "21374069801",
+      firstName: "Børge",
+      lastName: "Nielsen",
+      street: "Egtoftevej",
+      housenumber: "55",
+      postalCode: "2950",
+      city: "Vedbæk",
+      country: "DK",
+      addressSupplement: undefined,
+    });
+  });
+
+  it("extracts no postal code if none given", () => {
+    const extractedData = extractIdentData(ekonaDataWithoutPLZ);
+    expect(extractedData.postalCode).toBeUndefined();
+  });
+});
+
+describe("checkDataForAttributes", () => {
+  const removeAttribute = (
+    inputData: validatedEkonaData,
+    attributeToDelete: string
+  ) => {
+    const dataCopy: Partial<validatedEkonaData> = _.cloneDeep(ekonaDataInland);
+    const attributesList = attributeToDelete.split(".");
+    if (attributesList.length == 2)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      delete (dataCopy as any)[attributesList[0]][attributesList[1]];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    delete (dataCopy as any)[attributeToDelete];
+    return dataCopy;
+  };
+  const casesWithoutAllAttributes = [
+    {
+      inputData: removeAttribute(ekonaDataInland, "IdNr"),
+      missingValue: "IdNr",
+    },
+    {
+      inputData: removeAttribute(ekonaDataInland, "Vorname"),
+      missingValue: "Vorname",
+    },
+    {
+      inputData: removeAttribute(ekonaDataInland, "Name"),
+      missingValue: "Name",
+    },
+    {
+      inputData: removeAttribute(ekonaDataInland, "Anschrift.Strasse"),
+      missingValue: "Strasse",
+    },
+    {
+      inputData: removeAttribute(ekonaDataInland, "Anschrift.Hausnummer"),
+      missingValue: "Hausnummer",
+    },
+    {
+      inputData: removeAttribute(ekonaDataInland, "Anschrift.Ort"),
+      missingValue: "Ort",
+    },
+    {
+      inputData: removeAttribute(ekonaDataInland, "Anschrift.Land"),
+      missingValue: "Land",
+    },
+  ];
+
+  test.each(casesWithoutAllAttributes)(
+    "Should throw if value is '$missingValue' is missing.",
+    ({ inputData }) => {
+      expect(() => {
+        checkDataForAttributes(inputData);
+      }).toThrow();
+    }
+  );
+
+  const removeElementsFromList = (
+    inputData: validatedEkonaData,
+    attributeToDelete: string
+  ) => {
+    const dataCopy: Partial<validatedEkonaData> = _.cloneDeep(ekonaDataInland);
+    const attributesList = attributeToDelete.split(".");
+    if (attributesList.length == 2)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (dataCopy as any)[attributesList[0]][attributesList[1]] = [];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (dataCopy as any)[attributeToDelete] = [];
+    return dataCopy;
+  };
+  const casesWithEmptyLists = [
+    {
+      inputData: removeElementsFromList(ekonaDataInland, "Anschrift.Strasse"),
+      missingValue: "Strasse",
+    },
+    {
+      inputData: removeElementsFromList(
+        ekonaDataInland,
+        "Anschrift.Hausnummer"
+      ),
+      missingValue: "Hausnummer",
+    },
+    {
+      inputData: removeElementsFromList(ekonaDataInland, "Anschrift.Ort"),
+      missingValue: "Ort",
+    },
+    {
+      inputData: removeElementsFromList(ekonaDataInland, "Anschrift.Land"),
+      missingValue: "Land",
+    },
+  ];
+
+  test.each(casesWithEmptyLists)(
+    "Should throw if value is '$missingValue' is empty list.",
+    ({ inputData }) => {
+      expect(() => {
+        checkDataForAttributes(inputData);
+      }).toThrow();
+    }
+  );
+
+  it("should throw no error if all needed attributes are set", () => {
+    expect(() => {
+      checkDataForAttributes(ekonaDataInland);
+    }).not.toThrow();
+  });
+});
--- app/ekona/utility.ts
+++ app/ekona/utility.ts
@@ -1,5 +1,67 @@
 import { CacheProvider } from "node-saml";
 import { Session } from "@remix-run/node";
+import invariant from "tiny-invariant";
+
+export type validatedEkonaData = {
+  IdNr: string;
+  Vorname: string;
+  Name: string;
+  Anschrift: {
+    Strasse: { _: string }[];
+    Hausnummer: { _: string }[];
+    PLZ?: { _: string }[];
+    Ort: { _: string }[];
+    Land: { _: string }[];
+    Adressergaenzung?: { _: string }[];
+  };
+};
+export const extractIdentData = (validatedData: validatedEkonaData) => {
+  checkDataForAttributes(validatedData);
+  return {
+    idnr: validatedData.IdNr,
+    firstName: validatedData.Vorname,
+    lastName: validatedData.Name,
+    street: validatedData.Anschrift.Strasse[0]._,
+    housenumber: validatedData.Anschrift.Hausnummer[0]._,
+    postalCode: validatedData.Anschrift.PLZ
+      ? validatedData.Anschrift.PLZ[0]._
+      : undefined,
+    addressSupplement: validatedData.Anschrift.Adressergaenzung
+      ? validatedData.Anschrift.Adressergaenzung[0]._
+      : undefined,
+    city: validatedData.Anschrift.Ort[0]._,
+    country: validatedData.Anschrift.Land[0]._,
+  };
+};
+
+export const checkDataForAttributes = (
+  validatedData: Partial<validatedEkonaData>
+) => {
+  invariant(validatedData.IdNr, "Validated ekona data did not contain IdNr");
+  invariant(
+    validatedData.Vorname,
+    "Validated ekona data did not contain Vorname"
+  );
+  invariant(validatedData.Name, "Validated ekona data did not contain Name");
+  invariant(
+    validatedData.Anschrift?.Strasse &&
+      validatedData.Anschrift?.Strasse.length >= 1,
+    "Validated ekona data did not contain Strasse"
+  );
+  invariant(
+    validatedData.Anschrift?.Hausnummer &&
+      validatedData.Anschrift?.Hausnummer.length >= 1,
+    "Validated ekona data did not contain Hausnummer"
+  );
+  invariant(
+    validatedData.Anschrift?.Ort && validatedData.Anschrift?.Ort.length >= 1,
+    "Validated ekona data did not contain Ort"
+  );
+  invariant(
+    validatedData.Anschrift?.Land && validatedData.Anschrift?.Land.length >= 1,
+    "Validated ekona data did not contain Land"
+  );
+};
 
 export class SessionCacheProvider implements CacheProvider {
   session: Session;
