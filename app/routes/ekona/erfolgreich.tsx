039a5a745e25e6fdb9689b9a70a70b36548eadcf
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-07-21 11:45:56 +0200

    Hide ekona/erfolgreich behind feature flag

--- app/routes/ekona/erfolgreich.tsx
+++ app/routes/ekona/erfolgreich.tsx
@@ -2,6 +2,7 @@ import { LoaderFunction, MetaFunction } from "@remix-run/node";
 import { ContentContainer } from "~/components";
 import { pageTitle } from "~/util/pageTitle";
 import { authenticator } from "~/auth.server";
+import { testFeaturesEnabled } from "~/util/testFeaturesEnabled";
 
 export const meta: MetaFunction = () => {
   return { title: pageTitle("Freischaltcode erfolgreich eingegeben") };
@@ -11,6 +12,11 @@ export const loader: LoaderFunction = async ({ request }) => {
   await authenticator.isAuthenticated(request, {
     failureRedirect: "/anmelden",
   });
+  if (!testFeaturesEnabled) {
+    throw new Response("Not Found", {
+      status: 404,
+    });
+  }
   return null;
 };
 
