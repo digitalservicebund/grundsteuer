bf0992d391b53e1979a3d5db5272bc433b0ddda5
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-07-22 17:28:44 +0200

    Hide Ekona option on FSC entry behind feature toggle.

--- app/routes/fsc/eingeben/index.tsx
+++ app/routes/fsc/eingeben/index.tsx
@@ -53,6 +53,7 @@ import { createCsrfToken, CsrfToken, verifyCsrfToken } from "~/util/csrf";
 import FreischaltcodeHelp from "~/components/form/help/Freischaltcode";
 import ArrowRight from "~/components/icons/mui/ArrowRight";
 import { getRedirectionParams } from "~/routes/identifikation";
+import { testFeaturesEnabled } from "~/util/testFeaturesEnabled";
 
 const isEricaRequestInProgress = async (userData: User) => {
   return (
@@ -235,6 +236,7 @@ export const loader: LoaderFunction = async ({ request, context }) => {
       showSpinner:
         ericaActivationRequestIsInProgress ||
         ericaRevocationRequestIsInProgress,
+      showNewIdent: testFeaturesEnabled,
     },
     {
       headers: {
@@ -378,11 +380,11 @@ export default function FscEingeben() {
       <h2 className="mt-80 mb-32 text-24 font-bold">
         Keinen Freischaltcode erhalten?
       </h2>
-      <p className="mb-16">
+      <p className="mb-32">
         Zwei Wochen sind um und Sie haben noch keinen Brief mit dem
         Freischaltcode erhalten?
       </p>
-      <div className="flex items-center mb-32">
+      <div className="flex items-center mb-48">
         <ArrowRight className="inline-block mr-16" />
         <a
           href={"/fsc/neuBeantragen?index" + redirectionParams}
@@ -391,20 +393,25 @@ export default function FscEingeben() {
           Freischaltcode neu beantragen
         </a>
       </div>
-      <p className="mb-16">
-        Personen mit einem ELSTER Konto erhalten in der Regel keinen Brief mit
-        einem Freischaltcode. Sie können Ihre ELSTER Zugangsdaten nutzen, um
-        sich zu identifizieren.
-      </p>
-      <div className="flex items-center">
-        <ArrowRight className="inline-block mr-16" />
-        <a
-          href={"/ekona?index" + redirectionParams}
-          className="font-bold underline text-18 text-blue-800"
-        >
-          Mit ELSTER Zugang identifizieren
-        </a>
-      </div>
+      {loaderData.showNewIdent && (
+        <>
+          <p className="mb-32">
+            Personen mit einem ELSTER Konto erhalten in der Regel keinen Brief
+            mit einem Freischaltcode. Sie können Ihre ELSTER Zugangsdaten
+            nutzen, um sich zu identifizieren.
+          </p>
+          <div className="flex items-center">
+            <ArrowRight className="inline-block mr-16" />
+            <a
+              href={"/ekona?index" + redirectionParams}
+              className="font-bold underline text-18 text-blue-800"
+            >
+              Mit ELSTER Zugang identifizieren
+            </a>
+          </div>
+        </>
+      )}
+
       {showSpinner && (
         <Spinner
           initialText={"Ihr Freischaltcode wird überprüft."}
