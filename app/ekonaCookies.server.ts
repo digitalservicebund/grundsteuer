cc0995f10ddbbdb0e963aaa030c82afa1999e64c
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-07-22 13:04:44 +0200

    Use secure cookie for ekona

--- app/ekonaCookies.server.ts
+++ app/ekonaCookies.server.ts
@@ -3,11 +3,11 @@ import { useSecureCookie } from "~/util/useSecureCookie";
 
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
-    name: `${useSecureCookie ? "__Host-" : ""}ekonaSession`,
+    name: `${useSecureCookie ? "__Secure-" : ""}ekonaSession`,
     maxAge: 1800, //Restrict to 30 minutes (time limit of Ekona session)
     path: "/ekona",
     httpOnly: true,
-    sameSite: "none",
+    sameSite: "none", //We need to set sameSite none, because we explicitly want that this cookie is also sent with CrossSiteRequests
     secure: true,
     secrets: [process.env.SESSION_COOKIE_SECRET as string],
   },
