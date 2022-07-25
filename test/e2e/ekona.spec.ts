3ad5ec85e237628edbf8ef4911d86339d3b24c09
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-07-25 08:56:11 +0200

    Add ekona e2e test

---
+++ test/e2e/ekona.spec.ts
@@ -0,0 +1,56 @@
+/// <reference types="../../cypress/support" />
+describe("/ekona", () => {
+  it("/ekona routes should only be accessible for logged-in users", () => {
+    cy.visit("/ekona", { failOnStatusCode: false });
+    cy.url().should("include", "/anmelden");
+    cy.visit("/ekona/erfolgreich", { failOnStatusCode: false });
+    cy.url().should("include", "/anmelden");
+  });
+
+  describe("logged-in user but not identified", () => {
+    beforeEach(() => {
+      cy.task("setUserIdentifiedAttribute", {
+        userEmail: "foo@bar.com",
+        identified: false,
+      });
+      cy.login();
+    });
+
+    it("should redirect to /ekona page from erfolgreich", () => {
+      cy.visit("/ekona/erfolgreich", { failOnStatusCode: false });
+      cy.url().should("include", "/ekona");
+    });
+
+    it("should identify user if correct certificat provided", () => {
+      cy.visit("/ekona/", { failOnStatusCode: false });
+      cy.url().should("include", "/ekona");
+      cy.get(
+        "form[action='https://e4k-portal.een.elster.de/ekona/sso'] button"
+      ).click();
+
+      cy.origin("https://e4k-portal.een.elster.de/", () => {
+        cy.url().should(
+          "include",
+          "https://e4k-portal.een.elster.de/ekona/login/softpse"
+        );
+      });
+    });
+  });
+
+  describe("identified user", () => {
+    beforeEach(() => {
+      cy.task("setUserIdentifiedAttribute", {
+        userEmail: "foo@bar.com",
+        identified: true,
+      });
+      cy.login();
+    });
+
+    it("should redirect to /formular", () => {
+      cy.visit("/ekona", { failOnStatusCode: false });
+      cy.url().should("include", "/formular");
+    });
+  });
+});
+
+export {};
