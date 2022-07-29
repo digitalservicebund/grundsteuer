ee03c6b67fbb90c6fc536db707e05270078d071c
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-07-29 16:34:09 +0200

    Fixed access to and redirects from ekona pages.

--- app/routes/ekona.tsx
+++ app/routes/ekona.tsx
@@ -1,14 +1,24 @@
 import { Outlet } from "@remix-run/react";
 import { UserLayout } from "~/components";
-import { LoaderFunction } from "@remix-run/node";
+import { LoaderFunction, redirect } from "@remix-run/node";
 import { testFeaturesEnabled } from "~/util/testFeaturesEnabled";
+import { authenticator } from "~/auth.server";
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  const sessionUser = await authenticator.isAuthenticated(request, {
+    failureRedirect: "/anmelden",
+  });
+
   if (!testFeaturesEnabled) {
     throw new Response("Not Found", {
       status: 404,
     });
   }
+
+  if (sessionUser.identified && request.url.includes("/ekona/callback")) {
+    return redirect("/ekona/erfolgreich");
+  }
+
   return {};
 };
 
