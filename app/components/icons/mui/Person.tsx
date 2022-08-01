fa29748ce040493157ff3af7e2d5d41885c125cf
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-08-01 14:41:18 +0200

    Fix hardcoded colors in two svg icons for better reusability

--- app/components/icons/mui/Person.tsx
+++ app/components/icons/mui/Person.tsx
@@ -5,7 +5,7 @@ const Person = (props: SVGProps<SVGSVGElement>) => (
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    fill="black"
+    fill="currentColor"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
--- app/components/icons/mui/Plus.tsx
+++ app/components/icons/mui/Plus.tsx
@@ -5,7 +5,7 @@ const Plus = (props: SVGProps<SVGSVGElement>) => (
     width="36"
     height="36"
     viewBox="0 0 36 36"
-    fill="#fff"
+    fill="currentColor"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
