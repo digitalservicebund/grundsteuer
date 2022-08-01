bfc2e0ab931cbb763c1daf3dcc3b00e404ed537b
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-08-01 14:43:42 +0200

    Add three new icons

---
+++ app/components/icons/mui/DeleteOutline.tsx
@@ -0,0 +1,19 @@
+import { SVGProps } from "react";
+
+const DeleteOutline = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="1em"
+    viewBox="0 0 24 24"
+    width="1em"
+    {...props}
+  >
+    <path d="M0 0h24v24H0V0z" fill="none" />
+    <path
+      d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export default DeleteOutline;
