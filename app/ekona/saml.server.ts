9bf33e8bc38831b454460a24f0a5574d77da3caa
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-08-01 14:13:52 +0200

    Reduce duplication.

--- app/ekona/saml.server.ts
+++ app/ekona/saml.server.ts
@@ -3,7 +3,7 @@ import { Session } from "@remix-run/node";
 import { SessionCacheProvider } from "~/ekona/SessionCacheProvider";
 import { ValidateInResponseTo } from "@node-saml/node-saml/lib/types";
 
-export const createSamlRequest = async (session: Session) => {
+function getSamlConfig(session: Session) {
   const samlOptions: SamlConfig = {
     issuer: process.env.EKONA_ISSUER as string,
     cert: process.env.EKONA_IDP_CERT as string,
@@ -22,7 +22,11 @@ export const createSamlRequest = async (session: Session) => {
     cacheProvider: new SessionCacheProvider(session),
     validateInResponseTo: ValidateInResponseTo.always,
   };
+  return samlOptions;
+}
 
+export const createSamlRequest = async (session: Session) => {
+  const samlOptions = getSamlConfig(session);
   const saml = new SAML(samlOptions);
   return saml.getAuthorizeFormAsync("", samlOptions.entryPoint);
 };
@@ -31,25 +35,7 @@ export const validateSamlResponse = async (
   response: string,
   session: Session
 ) => {
-  const samlOptions: SamlConfig = {
-    issuer: process.env.EKONA_ISSUER as string,
-    cert: process.env.EKONA_IDP_CERT as string,
-    protocol: "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
-    callbackUrl: process.env.BASE_URL + "/ekona/callback",
-    entryPoint: process.env.EKONA_ENTRY_POINT,
-    signatureAlgorithm: "sha256",
-    digestAlgorithm: "sha256",
-    signMetadata: true,
-    decryptionPvk: process.env.EKONA_ENC_KEY,
-    privateKey: process.env.EKONA_SIGNING_KEY,
-    forceAuthn: true,
-    wantAssertionsSigned: false,
-    skipRequestCompression: true,
-    identifierFormat: "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent",
-    cacheProvider: new SessionCacheProvider(session),
-    validateInResponseTo: ValidateInResponseTo.always,
-  };
-
+  const samlOptions = getSamlConfig(session);
   const saml = new SAML(samlOptions);
   return saml.validatePostResponseAsync({ SAMLResponse: response });
 };
