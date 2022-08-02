db5207b5cd0cef686ae0c651a1166e6746038132
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-08-02 13:28:04 +0200

    Remove mistakenly overridden (test) env var from production file.

--- app/routes/ekona/_callback.test.ts
+++ app/routes/ekona/_callback.test.ts
@@ -1,3 +1,6 @@
+process.env.FORM_COOKIE_SECRET = "secret";
+process.env.FORM_COOKIE_ENC_SECRET = "26d011bcbb9db8c4673b7fcd90c9ec6d";
+
 import * as auditLogModule from "~/audit/auditLog";
 import * as userModule from "~/domain/user";
 import * as samlServerModule from "~/ekona/saml.server";
--- app/routes/ekona/callback.tsx
+++ app/routes/ekona/callback.tsx
@@ -1,6 +1,3 @@
-process.env.FORM_COOKIE_SECRET = "secret";
-process.env.FORM_COOKIE_ENC_SECRET = "26d011bcbb9db8c4673b7fcd90c9ec6d";
-
 import { ActionFunction, redirect, Session } from "@remix-run/node";
 import { validateSamlResponse } from "~/ekona/saml.server";
 import {
