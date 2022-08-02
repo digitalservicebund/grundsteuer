b0de8b5ca6ca05d8fa785be9db7be0eda9378980
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-08-02 15:09:05 +0200

    Make anzahlAction more readable

--- app/routes/formular/_anzahlAction.ts
+++ app/routes/formular/_anzahlAction.ts
@@ -7,6 +7,8 @@ import {
 import { authenticator } from "~/auth.server";
 import { verifyCsrfToken } from "~/util/csrf";
 import { action as stepAction, PREFIX } from "~/routes/formular/_step";
+import invariant from "tiny-invariant";
+import { Flurstueck, Person } from "~/domain/steps";
 
 export const GRUNDSTUECK_ANZAHL_MAX = 20;
 export const EIGENTUEMER_ANZAHL_MAX = 10;
@@ -30,21 +32,36 @@ export const action: ActionFunction = async (args) => {
 
   const storedFormData = await getStoredFormData({ request, user });
 
-  const section = request.url.match(/grundstueck\/anzahl/)
-    ? "grundstueck"
-    : "eigentuemer";
-  const items = section === "grundstueck" ? "flurstueck" : "person";
-  const maxAnzahl =
-    section === "grundstueck" ? GRUNDSTUECK_ANZAHL_MAX : EIGENTUEMER_ANZAHL_MAX;
+  let section: string | null = null;
+  if (request.url.match(/grundstueck\/anzahl/)) {
+    section = "grundstueck";
+  } else if (request.url.match(/eigentuemer\/anzahl/)) {
+    section = "eigentuemer";
+  }
+  invariant(
+    section,
+    "Dear developer, currently the code only works for /grundstueck/anzahl and /eigentuemer/anzahl."
+  );
 
-  const currentAnzahl =
+  const items = { grundstueck: "flurstueck", eigentuemer: "person" }[section];
+  const maxAnzahl = {
+    grundstueck: GRUNDSTUECK_ANZAHL_MAX,
+    eigentuemer: EIGENTUEMER_ANZAHL_MAX,
+  }[section];
+
+  const storedAnzahl =
     Number(
       storedFormData[section as "grundstueck" | "eigentuemer"]?.anzahl?.anzahl
     ) || 1;
 
-  const newAnzahl = formData.deleteButton
-    ? Math.max(1, currentAnzahl - 1)
-    : Math.min(maxAnzahl, currentAnzahl + 1);
+  let newAnzahl = 1;
+  if (formData.deleteButton) {
+    // decrease, but not smaller than 1
+    newAnzahl = Math.max(1, storedAnzahl - 1);
+  } else if (formData.increaseButton) {
+    // increase, but not larger than <maxAnzahl>
+    newAnzahl = Math.min(maxAnzahl as number, storedAnzahl + 1);
+  }
 
   let formDataToBeStored = _.set(
     _.cloneDeep(storedFormData),
@@ -53,15 +70,18 @@ export const action: ActionFunction = async (args) => {
   );
 
   if (formData.deleteButton) {
-    const currentList =
-      section === "grundstueck"
-        ? storedFormData.grundstueck?.flurstueck
-        : storedFormData.eigentuemer?.person;
+    let listOfItems: Flurstueck[] | Person[] | undefined;
+    if (section === "grundstueck") {
+      listOfItems = storedFormData.grundstueck?.flurstueck;
+    } else if (section === "eigentuemer") {
+      listOfItems = storedFormData.eigentuemer?.person;
+    }
 
-    const newList = currentList;
-
-    if (newList) {
-      const index = Number((formData.deleteButton as string).split("/")[0]) - 1;
+    if (listOfItems) {
+      const deleteButtonValue = formData.deleteButton as string; // eg. 1/3
+      const idToBeDeleted = deleteButtonValue.split("/")[0];
+      const anzahlWhenDeleteRequestWasIssued = deleteButtonValue.split("/")[1];
+      const indexToBeDeleted = Number(idToBeDeleted as string) - 1;
       // We don't have IDs, only indices.
       // Imagine having the page open in 2 browser tabs it could easily happen
       // that a delete request for one item is sent twice. With indices alone it
@@ -70,18 +90,15 @@ export const action: ActionFunction = async (args) => {
       // E.g. 2/5 (delete 2nd item of 5 items total)
       // Sending this request ("2/5") twice allows us to ignore the second request
       // as we can detect that the total number has changed.
-      const expectedCurrentAnzahl = Number(
-        (formData.deleteButton as string).split("/")[1]
-      );
-      if (expectedCurrentAnzahl === currentAnzahl) {
-        newList.splice(index, 1); // in-place!
+      if (Number(anzahlWhenDeleteRequestWasIssued as string) === storedAnzahl) {
+        listOfItems.splice(indexToBeDeleted, 1); // splice works in-place; mutates the list!
       }
     }
 
     formDataToBeStored = _.set(
       _.cloneDeep(formDataToBeStored),
       `${section}.${items}`,
-      newList
+      listOfItems
     );
   }
 
