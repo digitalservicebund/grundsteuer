9a967ccadced948d0b6232069cb1cc5345c6dbfc
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-08-22 12:16:26 +0200

    Wrap scheduled deletion logic in try-catch blocks.

--- app/cron.server.ts
+++ app/cron.server.ts
@@ -29,88 +29,100 @@ const scheduleAccountCleanup = (cronExpression: string) => {
 };
 
 export const deleteExpiredFscs = async () => {
-  const now = new Date();
-  const ninetyDaysAgo = new Date(now.setDate(now.getDate() - 90));
-  const queryResult = await db.fscRequest.deleteMany({
-    where: {
-      createdAt: {
-        lte: ninetyDaysAgo,
+  try {
+    const now = new Date();
+    const ninetyDaysAgo = new Date(now.setDate(now.getDate() - 90));
+    const queryResult = await db.fscRequest.deleteMany({
+      where: {
+        createdAt: {
+          lte: ninetyDaysAgo,
+        },
       },
-    },
-  });
-  console.log("Deleted %d expired FSC requests.", queryResult.count);
+    });
+    console.log("Deleted %d expired FSC requests.", queryResult.count);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const deleteExpiredPdfs = async () => {
-  const now = new Date();
-  const oneHourAgo = new Date(now.setHours(now.getHours() - 1));
-  const queryResult = await db.pdf.deleteMany({
-    where: {
-      createdAt: {
-        lte: oneHourAgo,
+  try {
+    const now = new Date();
+    const oneHourAgo = new Date(now.setHours(now.getHours() - 1));
+    const queryResult = await db.pdf.deleteMany({
+      where: {
+        createdAt: {
+          lte: oneHourAgo,
+        },
       },
-    },
-  });
-  console.log("Deleted %d expired PDFs.", queryResult.count);
+    });
+    console.log("Deleted %d expired PDFs.", queryResult.count);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const deleteExpiredAccounts = async () => {
-  const now = new Date();
-  const fourMonthsAgo = new Date(now.setMonth(now.getMonth() - 4));
-  const accountsToDelete = await db.user.findMany({
-    where: {
-      OR: [
-        // Declaration sent
-        {
-          lastDeclarationAt: {
-            lte: fourMonthsAgo,
+  try {
+    const now = new Date();
+    const fourMonthsAgo = new Date(now.setMonth(now.getMonth() - 4));
+    const accountsToDelete = await db.user.findMany({
+      where: {
+        OR: [
+          // Declaration sent
+          {
+            lastDeclarationAt: {
+              lte: fourMonthsAgo,
+            },
           },
-        },
-        // identified
-        {
-          identifiedAt: {
-            lte: fourMonthsAgo,
+          // identified
+          {
+            identifiedAt: {
+              lte: fourMonthsAgo,
+            },
+            lastDeclarationAt: null,
           },
-          lastDeclarationAt: null,
-        },
-        // simple account
-        {
-          createdAt: {
-            lte: fourMonthsAgo,
+          // simple account
+          {
+            createdAt: {
+              lte: fourMonthsAgo,
+            },
+            identifiedAt: null,
+            lastDeclarationAt: null,
           },
-          identifiedAt: null,
-          lastDeclarationAt: null,
-        },
-      ],
-    },
-    include: {
-      fscRequest: true,
-    },
-  });
-
-  for (const account of accountsToDelete) {
-    if (account.fscRequest) {
-      await revokeFscForUser(account);
-      await deleteFscRequest(account.email, account.fscRequest.requestId);
-    }
-  }
-
-  const queryResult = await db.user.deleteMany({
-    where: {
-      email: {
-        in: accountsToDelete.map((account) => account.email),
+        ],
       },
-    },
-  });
-  console.log("Deleted %d expired Accounts.", queryResult.count);
-
-  for (const account of accountsToDelete) {
-    await saveAuditLog({
-      eventName: AuditLogEvent.ACCOUNT_DELETED,
-      ipAddress: "cron",
-      timestamp: Date.now(),
-      username: account.email,
+      include: {
+        fscRequest: true,
+      },
     });
+
+    for (const account of accountsToDelete) {
+      if (account.fscRequest) {
+        await revokeFscForUser(account);
+        await deleteFscRequest(account.email, account.fscRequest.requestId);
+      }
+    }
+
+    const queryResult = await db.user.deleteMany({
+      where: {
+        email: {
+          in: accountsToDelete.map((account) => account.email),
+        },
+      },
+    });
+    console.log("Deleted %d expired Accounts.", queryResult.count);
+
+    for (const account of accountsToDelete) {
+      await saveAuditLog({
+        eventName: AuditLogEvent.ACCOUNT_DELETED,
+        ipAddress: "cron",
+        timestamp: Date.now(),
+        username: account.email,
+      });
+    }
+  } catch (error) {
+    console.error(error);
   }
 };
 
