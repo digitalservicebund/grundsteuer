47c692c735f6ca4c1eb11e5323bfdedfff05995a
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-08-22 17:28:49 +0200

    Hardcode error page texts directly in the component.
    This protects error pages from being affected by i18n-related errors.

--- app/components/ErrorPage.tsx
+++ app/components/ErrorPage.tsx
@@ -1,4 +1,4 @@
-import { Trans, useTranslation } from "react-i18next";
+import { Trans } from "react-i18next";
 import { Button, SimplePageLayout } from "~/components";
 import illustrationImage404 from "~/assets/images/404.svg";
 import illustrationImage500 from "~/assets/images/500.svg";
@@ -7,8 +7,22 @@ type ErrorPageProps = {
   statusCode: 404 | 500;
 };
 
+const texts = {
+  404: {
+    backButton: "Zur Startseite",
+    headline: "Seite konnte nicht gefunden werden.",
+    body: "Es tut uns leid. Diese Seite scheint es nicht zu geben. Sie wurde vielleicht entfernt, im Namen geändert oder ist auf andere Weise nicht erreichbar.",
+    smallprint:
+      "Wenn Sie die URL direkt eingegeben haben, überprüfen Sie die Schreibweise.",
+  },
+  500: {
+    backButton: "Zur Startseite",
+    headline: "Ein unerwarteter Fehler ist aufgetreten.",
+    body: "Es tut uns leid! Ein Fehler ist aufgetreten. Bitte laden Sie die Seite neu oder versuchen Sie es zu einem anderen Zeitpunkt noch einmal. ",
+  },
+};
+
 export default function ErrorPage(props: ErrorPageProps) {
-  const { t } = useTranslation("all");
   const { statusCode } = props;
 
   return (
@@ -16,11 +30,11 @@ export default function ErrorPage(props: ErrorPageProps) {
       <h1 className="text-32 leading-40 mb-32 max-w-screen-sm md:text-64 md:leading-68 md:mb-48">
         {statusCode}
         <br />
-        {t(`${statusCode}.headline`)}
+        {texts[statusCode].headline}
       </h1>
 
       <p className="text-20 leading-26 md:text-32 md:leading-40 max-w-screen-md mb-24 md:mb-32">
-        {t(`${statusCode}.text`)}
+        {texts[statusCode].body}
       </p>
 
       {statusCode === 404 && (
@@ -31,13 +45,13 @@ export default function ErrorPage(props: ErrorPageProps) {
               nowrap: <span className="whitespace-nowrap" />,
             }}
           >
-            {t(`${statusCode}.smallprint`)}
+            {texts[statusCode].smallprint}
           </Trans>
         </p>
       )}
 
       <Button to="/formular" className="mb-64">
-        {t(`${statusCode}.backButton`)}
+        {texts[statusCode].backButton}
       </Button>
 
       <img
