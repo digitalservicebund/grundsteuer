fc0fbb482a1c0ac83fd3511999579bf5e622b716
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-09-08 15:22:57 +0200

    Use interactive transactions for lifecycle event

--- app/domain/lifecycleEvents.server.ts
+++ app/domain/lifecycleEvents.server.ts
@@ -1,50 +1,72 @@
-import {db} from "~/db.server";
-import {AuditLogEvent, encryptAuditLogData} from "~/audit/auditLog";
-import {findUserByEmail} from "~/domain/user";
+import { db } from "~/db.server";
+import { AuditLogEvent, saveAuditLog } from "~/audit/auditLog";
+import { findUserByEmail } from "~/domain/user";
 
-export const saveSuccessfullFscRequestData = async (email: string, ericaRequestIdFscBeantragen: string, clientIp: string, requestId: string, transferticket: string, steuerId: string) => {
+export const saveSuccessfullFscRequestData = async (
+  email: string,
+  ericaRequestIdFscBeantragen: string,
+  clientIp: string,
+  requestId: string,
+  transferticket: string,
+  steuerId: string
+) => {
   const user = await findUserByEmail(email);
   if (!user) {
     throw new Error("User not found.");
   }
 
-  const _deleteEricaRequestIdFscBeantragen = db.user.updateMany({
-    where: {
-      id: user.id,
-      ericaRequestIdFscBeantragen: ericaRequestIdFscBeantragen
-    },
-    data: {ericaRequestIdFscBeantragen: null},
-  });
-  const _deleteFscRequest = db.fscRequest.deleteMany({
-    where: {
-      userId: user.id,
-    },
-  });
-  const _saveFscRequest = db.fscRequest.create({
-    data: {
-      requestId,
-      User: {connect: {email: user.email}},
-    },
-  });
-  const auditLogData = {
-    eventName: AuditLogEvent.FSC_REQUESTED,
-    timestamp: Date.now(),
-    ipAddress: clientIp,
-    username: user.email,
-    eventData: {
-      transferticket,
-      steuerId,
-    },
+  const _deleteEricaRequestIdFscBeantragen = () => {
+    return db.user.updateMany({
+      where: {
+        id: user.id,
+        ericaRequestIdFscBeantragen: ericaRequestIdFscBeantragen,
+      },
+      data: { ericaRequestIdFscBeantragen: null },
+    });
+  };
+
+  const _createNewFscRequest = async () => {
+    await db.fscRequest.deleteMany({
+      where: {
+        userId: user.id,
+      },
+    });
+    await db.fscRequest.create({
+      data: {
+        requestId,
+        User: { connect: { email: user.email } },
+      },
+    });
+  };
+
+  try {
+    await db.$transaction(async () => {
+      const updatedUsersWithEricaId =
+        await _deleteEricaRequestIdFscBeantragen();
+
+      if (updatedUsersWithEricaId.count != 1) {
+        throw Error("ericaRequestId of user does not match");
+      }
+
+      await _createNewFscRequest();
+      await saveAuditLog({
+        eventName: AuditLogEvent.FSC_REQUESTED,
+        timestamp: Date.now(),
+        ipAddress: clientIp,
+        username: user.email,
+        eventData: {
+          transferticket,
+          steuerId,
+        },
+      });
+    });
+  } catch (error) {
+    if (
+      !("message" in (error as object)) ||
+      (error as { message: string }).message !==
+        "ericaRequestId of user does not match"
+    ) {
+      throw error;
+    }
   }
-  const _saveAuditLog = db.auditLog.create({
-    data: {
-      data: encryptAuditLogData(auditLogData),
-    },
-  });
-  await db.$transaction([
-      _deleteEricaRequestIdFscBeantragen,
-      _deleteFscRequest,
-      _saveFscRequest,
-      _saveAuditLog,
-    ]);
-}
+};
