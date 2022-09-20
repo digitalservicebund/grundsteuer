82fbdbc32337c3808f5173e5ef64ae24365846f7
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-09-20 18:17:33 +0200

    Setup + teardown redis in integration tests

--- private/jest/setupAfterEnv.js
+++ private/jest/setupAfterEnv.js
@@ -1,1 +1,17 @@
 require("@testing-library/jest-dom");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Redis = require("ioredis");
+
+if (!global.ioredis) {
+  global.ioredis = new Redis(process.env.REDIS_URL);
+  console.log("Redis connection opened");
+}
+
+// eslint-disable-next-line no-undef
+afterAll(async () => {
+  if (global.ioredis) {
+    await global.ioredis.quit();
+    global.ioredis = undefined;
+    console.log("Redis connection closed");
+  }
+});
