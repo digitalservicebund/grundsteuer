9481f0d3a5fd3f9eaff42e42ddec9b72b348e637
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-09-22 17:05:23 +0200

    Revert "Move try-catch into cron schedules"
    This reverts commit fc926645f8cd0e703198b90cf6da8c2383dde082.

--- app/cron.server.ts
+++ app/cron.server.ts
@@ -12,13 +12,7 @@ const scheduleFscCleanup = (cronExpression: string) => {
     "Schedule deleting expired FSC requests with cron expression: %s",
     cronExpression
   );
-  schedule(cronExpression, async () => {
-    try {
-      await deleteExpiredFscs();
-    } catch (e) {
-      console.error("Failed to delete expired FSCs", e);
-    }
-  });
+  schedule(cronExpression, async () => deleteExpiredFscs());
 };
 
 const schedulePdfCleanup = (cronExpression: string) => {
@@ -26,13 +20,7 @@ const schedulePdfCleanup = (cronExpression: string) => {
     "Schedule deleting expired PDFs with cron expression: %s",
     cronExpression
   );
-  schedule(cronExpression, async () => {
-    try {
-      await deleteExpiredPdfs();
-    } catch (e) {
-      console.error("Failed to delete expired PDFs", e);
-    }
-  });
+  schedule(cronExpression, async () => deleteExpiredPdfs());
 };
 
 const scheduleAccountCleanup = (cronExpression: string) => {
@@ -40,109 +28,115 @@ const scheduleAccountCleanup = (cronExpression: string) => {
     "Schedule deleting expired accounts with cron expression: %s",
     cronExpression
   );
-  schedule(cronExpression, async () => {
-    try {
-      await deleteExpiredAccounts();
-    } catch (e) {
-      console.error("Failed to delete expired Accounts", e);
-    }
-  });
+  schedule(cronExpression, async () => deleteExpiredAccounts());
 };
 
 export const deleteExpiredFscs = async () => {
-  const now = new Date();
-  const ninetyDaysAgo = new Date(now.setDate(now.getDate() - 90));
-  const queryResult = await db.fscRequest.deleteMany({
-    where: {
-      createdAt: {
-        lte: ninetyDaysAgo,
+  try {
+    const now = new Date();
+    const ninetyDaysAgo = new Date(now.setDate(now.getDate() - 90));
+    const queryResult = await db.fscRequest.deleteMany({
+      where: {
+        createdAt: {
+          lte: ninetyDaysAgo,
+        },
       },
-    },
-  });
-  console.log("Deleted %d expired FSC requests.", queryResult.count);
+    });
+    console.log("Deleted %d expired FSC requests.", queryResult.count);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const deleteExpiredPdfs = async () => {
-  const now = new Date();
-  const oneHourAgo = new Date(now.setHours(now.getHours() - 1));
-  const queryResult = await db.pdf.deleteMany({
-    where: {
-      createdAt: {
-        lte: oneHourAgo,
+  try {
+    const now = new Date();
+    const oneHourAgo = new Date(now.setHours(now.getHours() - 1));
+    const queryResult = await db.pdf.deleteMany({
+      where: {
+        createdAt: {
+          lte: oneHourAgo,
+        },
       },
-    },
-  });
-  console.log("Deleted %d expired PDFs.", queryResult.count);
+    });
+    console.log("Deleted %d expired PDFs.", queryResult.count);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const deleteExpiredAccounts = async () => {
-  const now = new Date();
-  const fourMonthsAgo = new Date(now.setMonth(now.getMonth() - 4));
-  const accountsToDelete = await db.user.findMany({
-    where: {
-      OR: [
-        // Declaration sent
-        {
-          lastDeclarationAt: {
-            lte: fourMonthsAgo,
+  try {
+    const now = new Date();
+    const fourMonthsAgo = new Date(now.setMonth(now.getMonth() - 4));
+    const accountsToDelete = await db.user.findMany({
+      where: {
+        OR: [
+          // Declaration sent
+          {
+            lastDeclarationAt: {
+              lte: fourMonthsAgo,
+            },
           },
-        },
-        // identified
-        {
-          identifiedAt: {
-            lte: fourMonthsAgo,
+          // identified
+          {
+            identifiedAt: {
+              lte: fourMonthsAgo,
+            },
+            lastDeclarationAt: null,
           },
-          lastDeclarationAt: null,
-        },
-        // simple account
-        {
-          createdAt: {
-            lte: fourMonthsAgo,
+          // simple account
+          {
+            createdAt: {
+              lte: fourMonthsAgo,
+            },
+            identifiedAt: null,
+            lastDeclarationAt: null,
           },
-          identifiedAt: null,
-          lastDeclarationAt: null,
-        },
-      ],
-    },
-    include: {
-      fscRequest: true,
-    },
-  });
-
-  for (const account of accountsToDelete) {
-    if (account.fscRequest) {
-      await revokeFscForUser(account);
-      await deleteFscRequest(account.email, account.fscRequest.requestId);
-    }
-  }
-  const deleteAccounts = db.user.deleteMany({
-    where: {
-      email: {
-        in: accountsToDelete.map((account) => account.email),
+        ],
       },
-    },
-  });
+      include: {
+        fscRequest: true,
+      },
+    });
 
-  const logsToCreate = accountsToDelete.map((account) => {
-    return {
-      data: encryptAuditLogData({
-        eventName: AuditLogEvent.ACCOUNT_DELETED,
-        ipAddress: "cron",
-        timestamp: Date.now(),
-        username: account.email,
-      }),
-    };
-  });
+    for (const account of accountsToDelete) {
+      if (account.fscRequest) {
+        await revokeFscForUser(account);
+        await deleteFscRequest(account.email, account.fscRequest.requestId);
+      }
+    }
+    const deleteAccounts = db.user.deleteMany({
+      where: {
+        email: {
+          in: accountsToDelete.map((account) => account.email),
+        },
+      },
+    });
 
-  const createAuditLogs = db.auditLog.createMany({
-    data: logsToCreate,
-  });
+    const logsToCreate = accountsToDelete.map((account) => {
+      return {
+        data: encryptAuditLogData({
+          eventName: AuditLogEvent.ACCOUNT_DELETED,
+          ipAddress: "cron",
+          timestamp: Date.now(),
+          username: account.email,
+        }),
+      };
+    });
 
-  const [resultDeletedAccounts] = await db.$transaction([
-    deleteAccounts,
-    createAuditLogs,
-  ]);
-  console.log(`Deleted ${resultDeletedAccounts.count} expired accounts`);
+    const createAuditLogs = db.auditLog.createMany({
+      data: logsToCreate,
+    });
+
+    const [resultDeletedAccounts] = await db.$transaction([
+      deleteAccounts,
+      createAuditLogs,
+    ]);
+    console.log(`Deleted ${resultDeletedAccounts.count} expired accounts`);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const scheduleUpdateEricaRequest = (cronExpression: string) => {
