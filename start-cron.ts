84a1acfe117c32552b410babffc65dc6f6de4153
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-09-22 17:00:55 +0200

    Require erica url for cron job

--- start-cron.ts
+++ start-cron.ts
@@ -2,8 +2,9 @@ import invariant from "tiny-invariant";
 import { jobs } from "./app/cron.server";
 
 invariant(process.env.DATABASE_URL, "DATABASE_URL is not set.");
-invariant(process.env.AUDIT_PUBLIC_KEY, "DATABASE_URL is not set.");
-invariant(process.env.REDIS_URL, "DATABASE_URL is not set.");
+invariant(process.env.AUDIT_PUBLIC_KEY, "AUDIT_PUBLIC_KEY is not set.");
+invariant(process.env.REDIS_URL, "REDIS_URL is not set.");
+invariant(process.env.ERICA_URL, "ERICA_URL is not set.");
 // run once every hour
 jobs.scheduleFscCleanup("0 * * * *");
 jobs.schedulePdfCleanup("30 * * * *");
