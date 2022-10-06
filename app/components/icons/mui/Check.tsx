9c75dff5f62f1d1b7d2f441db18f1d8759fe18f8
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-10-06 17:38:06 +0200

    Success page redesign

--- app/components/icons/mui/Check.tsx
+++ app/components/icons/mui/Check.tsx
@@ -1,16 +1,10 @@
 import { SVGProps } from "react";
 
 const Check = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    width="55"
-    height="43"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
+  <svg width="55" height="43" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path
       d="M17.5 33.688 4.375 20.562 0 24.938l17.5 17.5L55 4.938 50.625.563 17.5 33.688Z"
-      fill="#000"
+      fill="#01854A"
     />
   </svg>
 );
--- app/routes/formular/erfolg.tsx
+++ app/routes/formular/erfolg.tsx
@@ -1,15 +1,11 @@
 import { LoaderFunction, MetaFunction, redirect } from "@remix-run/node";
 import { pageTitle } from "~/util/pageTitle";
-import UebersichtStep from "~/components/form/UebersichtStep";
-import erfolgMedium from "~/assets/images/erfolg-medium.svg";
-import erfolgSmall from "~/assets/images/erfolg-small.svg";
 import transferticketImage from "~/assets/images/erfolg-transferticket.svg";
 import erklaerungImage from "~/assets/images/erfolg-erklaerung.svg";
 import {
   BreadcrumbNavigation,
   Button,
   ContentContainer,
-  Headline,
   IntroText,
 } from "~/components";
 import invariant from "tiny-invariant";
@@ -21,6 +17,7 @@ import { ReactNode } from "react";
 import Hint from "~/components/Hint";
 import EnumeratedList from "~/components/EnumeratedList";
 import Plus from "~/components/icons/mui/Plus";
+import Check from "~/components/icons/mui/Check";
 
 export const meta: MetaFunction = () => {
   return { title: pageTitle("Erklärung abgeschickt") };
@@ -52,12 +49,12 @@ const DownloadCard = (props: {
   children: ReactNode;
 }) => {
   return (
-    <div className="bg-blue-200 p-32 flex flex-col lg:flex-row gap-x-16 xl:gap-x-64 gap-y-32 mb-24">
-      <div className="flex justify-center lg:items-start">
+    <div className="bg-blue-200 p-32 flex flex-col lg:flex-row xl:gap-x-4 gap-y-32 mb-24">
+      <div className="flex justify-center md:min-w-[200px] lg:items-start">
         <img
           src={props.image}
           alt={props.imageAltText}
-          className="max-w-[220px] min-w-0"
+          className="min-w-[200px] lg:min-w-[120px]"
         />
       </div>
       <div className="flex flex-col lg:max-w-[400px]">{props.children}</div>
@@ -73,52 +70,49 @@ export default function Erfolg() {
   const pdfButtonProps = pdf ? { href: "/download/pdf" } : { disabled: true };
   return (
     <>
-      <ContentContainer size="sm">
+      <ContentContainer size="sm-md">
         <BreadcrumbNavigation />
-        <UebersichtStep imageSrc={erfolgMedium} smallImageSrc={erfolgSmall}>
-          <Headline>
+        <div className="flex gap-32 items-center mb-48">
+          <div>
+            <Check />
+          </div>
+          <h1 className="text-30 leading-36 w-[24rem]">
             Ihre Grundsteuererklärung wurde erfolgreich versendet.
-          </Headline>
-
-          <h2 className="text-24 mb-16">Wie geht es jetzt weiter?</h2>
-          <EnumeratedList
-            items={[
-              "Ihre Erklärung wird nun von Ihrem Finanzamt bearbeitet",
-              <div>
-                Zwischen 2022 und 2024 bekommen Sie <strong>drei Briefe</strong>
-                :
-                <ul className="list-disc ml-20">
-                  <li>Grundsteuerwertbescheid</li>
-                  <li>Grundsteuermessbescheid</li>
-                  <li>den neuen Grundsteuerbescheid</li>
-                </ul>
-              </div>,
-              "Bis zum Jahr 2025 zahlen Sie die alte Grundsteuer. Ab 2025 zahlen Sie die neue Grundsteuer.",
-            ]}
-            className="mb-32"
-          />
-          <p>
-            Sie können sich für Ihre Unterlagen Ihre Grundsteuererklärung und
-            das Transferticket als PDF herunterladen. Wenn Sie keine weitere
-            Erklärung abgeben wollen, können Sie diese Seite verlassen.
-          </p>
-          <p className="font-bold mb-32">
-            Ihr Nutzerkonto wird 4 Monate nach Absenden der Erklärung
-            automatisch gelöscht.
-          </p>
-        </UebersichtStep>
+          </h1>
+        </div>
       </ContentContainer>
       <ContentContainer size="lg">
-        <h2 className="text-24 mb-24">Für Ihre Unterlagen</h2>
+        <DownloadCard image={erklaerungImage} imageAltText="">
+          <h3 className="font-bold text-18 mb-8">
+            Ihre Grundsteuererklärung als PDF
+          </h3>
+          <p className="mb-24">
+            Wir haben Ihre Angaben für den Versand an ELSTER aufbereitet. Das
+            PDF enthält auch das Transferticket.
+          </p>
+          <Button
+            target={"_blank"}
+            download
+            look="primary"
+            iconRight={<Download />}
+            className="mb-40"
+            {...pdfButtonProps}
+          >
+            Erklärung als PDF herunterladen
+          </Button>
+          <Hint className="mb-0">
+            Auch Sicherheitsgründen können Sie die PDF der Grundsteuererklärung
+            nur innerhalb von 24 Stunden nach Erstellung herunterladen.
+          </Hint>
+        </DownloadCard>
+
         <DownloadCard image={transferticketImage} imageAltText="">
-          <h3 className="text-18 mb-8">
+          <h3 className="font-bold text-18 mb-8">
             Transferticket: Beweis der Übermittlung an ELSTER
           </h3>
           <p className="mb-32">
-            Das sogenannte Transferticket ist der Beweis, dass Ihre Erklärung an
-            ELSTER erfolgreich übermittelt wurde. Bitte bewahren Sie es gut auf
-            – notieren Sie sich das Ticket oder laden sie sich das
-            Transferticket als Textdatei herunter.
+            Notieren Sie sich die Zeichenkette oder laden Sie das Transferticket
+            als Textdatei herunter. Bewahren Sie es gut auf.
           </p>
           {transferticket && (
             <div className="mb-16 p-16 border-blue-800 border-2 text-center">
@@ -138,42 +132,45 @@ export default function Erfolg() {
             Transferticket als .txt herunterladen
           </Button>
           <Hint className="mb-0">
-            Sie können das Transferticket nur innerhalb der ersten 24 Stunden
-            nach Erstellung herunterladen.
-          </Hint>
-        </DownloadCard>
-
-        <DownloadCard image={erklaerungImage} imageAltText="">
-          <h3 className="text-18 mb-8">Ihre Grundsteuererklärung</h3>
-          <p className="mb-24">
-            Hier finden Sie Ihre Grundsteuererklärung als PDF. Wir haben Ihre
-            Angaben für den Versand an ELSTER aufbereitet. Das PDF enthält auch
-            das Transferticket.
-          </p>
-          <Button
-            target={"_blank"}
-            download
-            look="primary"
-            iconRight={<Download />}
-            className="mb-40"
-            {...pdfButtonProps}
-          >
-            Angaben als PDF herunterladen
-          </Button>
-          <Hint className="mb-0">
-            Sie können die Grundsteuererklärung als PDF nur innerhalb der ersten
-            24 Stunden nach Erstellung herunterladen.
+            Auch Sicherheitsgründen können Sie das Transferticket nur innerhalb
+            von 24 Stunden nach Erstellung herunterladen.
           </Hint>
         </DownloadCard>
 
         <div className="bg-white p-32 mb-32">
           <ContentContainer size="sm">
+            <h2 className="text-24 mb-16">Wie geht es jetzt weiter?</h2>
+            <EnumeratedList
+              items={[
+                "Ihre Erklärung wird nun von Ihrem Finanzamt bearbeitet",
+                <div>
+                  Zwischen 2022 und 2024 bekommen Sie{" "}
+                  <strong>drei Briefe</strong>:
+                  <ul className="list-disc ml-20">
+                    <li>Grundsteuerwertbescheid</li>
+                    <li>Grundsteuermessbescheid</li>
+                    <li>den neuen Grundsteuerbescheid</li>
+                  </ul>
+                </div>,
+                "Bis zum Jahr 2025 zahlen Sie die alte Grundsteuer. Ab 2025 zahlen Sie die neue Grundsteuer.",
+              ]}
+              className="mb-32"
+            />
+            <p className="font-bold">
+              Ihr Nutzerkonto wird 4 Monate nach Absenden der Erklärung
+              automatisch gelöscht.
+            </p>
+          </ContentContainer>
+        </div>
+
+        <div className="bg-white p-32 mb-32">
+          <ContentContainer size="sm">
             <h2 className="text-30 mb-32 leading-36">
-              Weitere Grundsteuererklärung abgeben{" "}
+              Weitere Grundsteuererklärung abgeben
             </h2>
             <IntroText>
-              Sie können hier im Anschluss eine weitere Erklärung starten. Dafür
-              müssen Sie keinen neuen Freischaltcode beantragen.
+              Wenn Sie mögen, können Sie gleich eine weitere Erklärung starten —
+              dafür müssen Sie sich nicht erneut identifizieren.
             </IntroText>
             <Button
               href="/formular/weitereErklaerung"
@@ -184,16 +181,6 @@ export default function Erfolg() {
             </Button>
           </ContentContainer>
         </div>
-
-        <ContentContainer size="sm">
-          <h2 className="text-24 mb-16">
-            Was passiert mit meinem Nutzerkonto?
-          </h2>
-          <IntroText>
-            Wir werden Ihr Konto automatisch 4 Monate nach Abgabe der
-            Grundsteuerklärung löschen.
-          </IntroText>
-        </ContentContainer>
       </ContentContainer>
     </>
   );
