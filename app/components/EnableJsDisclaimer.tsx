0ca9c814847983f4d54478f3775a42af0085446d
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-10-17 15:41:26 +0200

    Style mobile and js disclaimer

--- app/components/EnableJsDisclaimer.tsx
+++ app/components/EnableJsDisclaimer.tsx
@@ -1,8 +1,26 @@
+import { ContentContainer, Headline, IntroText } from "~/components/index";
+import noJsImage from "~/assets/images/no-js.svg";
+
 export default function EnableJsDisclaimer() {
   return (
-    <div>
-      Um sich mit BundesIdent ausweisen zu können, müssen Sie JavaScript in
-      Ihrem Browser aktivieren.
+    <div className="mt-16 sm:mt-32">
+      <ContentContainer size="sm">
+        <Headline>
+          Um sich mit BundesIdent ausweisen zu können, müssen Sie JavaScript in
+          Ihrem Browser aktivieren
+        </Headline>
+        <div className="w-full">
+          <img
+            src={noJsImage}
+            alt="Schematisches Bild eines Smartphones"
+            className="mb-32 ml-auto"
+          />
+        </div>
+        <IntroText>
+          Ändern Sie Ihre Einstellungen und laden Sie diese Seite neu um sich
+          mit BundesIdent auszuweisen.
+        </IntroText>
+      </ContentContainer>
     </div>
   );
 }
--- app/components/OnlyMobileDisclaimer.tsx
+++ app/components/OnlyMobileDisclaimer.tsx
@@ -1,8 +1,20 @@
+import usePhoneImage from "~/assets/images/use-phone.svg";
+import { ContentContainer, Headline, IntroText } from "~/components/index";
+
 export default function OnlyMobileDisclaimer() {
   return (
-    <div>
-      Diese Seite ist nur auf einem mobilen Gerät aufrufbar. Verwenden Sie
-      stattdessen Ihr Smartphone oder Tablet.
+    <div className="mt-16 sm:mt-32">
+      <ContentContainer size="sm">
+        <Headline>
+          Auf diese Seite können Sie nur über ein mobiles Gerät zugreifen
+        </Headline>
+        <IntroText>
+          Die Identifizierung mit dem Personalausweis über die BundesIdent App
+          ist nur auf mobilen Geräten möglich. Für eine andere Identifikation
+          kehren Sie zurück zur Auswahl.
+        </IntroText>
+      </ContentContainer>
+      <img src={usePhoneImage} alt="" className="max-w-[300px]" />
     </div>
   );
 }
