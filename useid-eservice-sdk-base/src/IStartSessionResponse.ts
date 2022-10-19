8718e067af624b9178e47cd5cb62e787cf5d3ffb
Frederike Ramin <frederike.ramin@digitalservice.bund.de>
2022-10-19 08:50:04 +0200

    Use sdk as library instead of as file

--- useid-eservice-sdk-base/src/IStartSessionResponse.ts
+++
@@ -1,3 +0,0 @@
-export interface IStartSessionResponse {
-  tcTokenUrl: string;
-}
--- useid-eservice-sdk-base/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { DataGroup } from "./DataGroup";
-export { Place } from "./Identity";
-export { UseIdAPI, IUseIdAPI } from "./UseIdAPI";
