a951a84cecdf1fab9b80d8293c2a25bf35698996
Shahrooz Afsharipour <shrzaf@gmail.com>
2022-10-21 13:17:51 +0200

    Display error banner contents responsively.

--- app/components/ErrorBanner.tsx
+++ app/components/ErrorBanner.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react";
 import ExclamationMark from "~/components/icons/mui/ExclamationMark";
 import classNames from "classnames";
 import SettingsOutlined from "~/components/icons/mui/SettingsOutlined";
+import { ContentContainer } from "~/components/index";
 
 type ErrorBannerProps = {
   heading?: ReactNode | string;
@@ -19,18 +20,23 @@ export default class ErrorBanner extends React.Component<ErrorBannerProps> {
     return (
       <div
         className={classNames(
-          "border-l-[12px] pl-32 pr-32 py-20 flex flex-row gap-4",
+          "border-l-[12px] pr-32 py-20",
           style === "warning"
             ? "bg-yellow-200 border-l-yellow-600"
             : "bg-red-200 border-l-red-900",
           this.props.className
         )}
       >
-        <IconComponent className="mt-6 mr-10 min-w-[25px] text-red-900" />
-        <div className="flex flex-col max-w-[55rem]" aria-live="polite">
-          <div className="text-24 leading-30 mb-8">{heading}</div>
-          <div className="text-16">{this.props.children}</div>
-        </div>
+        <ContentContainer>
+          <div className="flex flex-row gap-4 lg:ml-[-25px]">
+            <IconComponent className="mt-6 mr-10 min-w-[25px] text-red-900" />
+
+            <div className="flex flex-col max-w-[55rem]" aria-live="polite">
+              <div className="text-24 leading-30 mb-8">{heading}</div>
+              <div className="text-16">{this.props.children}</div>
+            </div>
+          </div>
+        </ContentContainer>
       </div>
     );
   }
