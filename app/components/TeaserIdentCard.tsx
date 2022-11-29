cc564996dcdb7802e596f1136808f983824dba04
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2022-11-29 17:13:37 +0100

    Add special mobile view to identity teaser

--- app/components/TeaserIdentCard.tsx
+++ app/components/TeaserIdentCard.tsx
@@ -6,23 +6,35 @@ export default function TeaserIdentCard(props: { className?: string }) {
   return (
     <div
       className={classNames(
-        "mt-48 md:mt-0 p-16 lg:py-24 lg:px-32 !bg-blue-300 lg:enumerate-card flex-col lg:flex-row max-w-[970px]",
+        "rounded-t-lg !bg-blue-800 text-white lg:text-black mt-48 lg:mt-0 p-16 lg:py-24 lg:px-32 lg:!bg-blue-300 lg:enumerate-card flex flex-col lg:flex-row max-w-[970px]",
         props.className
       )}
     >
-      <img
-        src={ClockIcon}
-        alt={"Weißer Wekcer mit blauem Display"}
-        className="hidden lg:flex mr-36 md:h-[87px] md:w-[71px] w-full"
-      />
-      <div className="flex flex-col">
+      <div className="flex flex-row">
+        <img
+          src={ClockIcon}
+          alt={"Weißer Wecker mit blauem Display"}
+          className="h-[42px] lg:flex mr-36 lg:h-auto lg:w-full"
+        />
+        <div className="flex">
+          <p className="mb-8 text-18 lg:hidden">
+            <strong className="inline-block  align-center">
+              Abgabefrist ist der 31.01.2023.
+            </strong>
+          </p>
+        </div>
+      </div>
+
+      <div className="lg:flex lg:flex-col">
         <ContentContainer size="sm-md">
-          <div className="flex flex-row mt-16 md:mt-0">
+          <div className="mt-16 lg:mt-0">
             <p className="mb-8 text-18">
-              <strong>Abgabefrist ist der 31.01.2023.</strong> Für die Option
-              Identifikation mit Freischaltcode diesen jetzt beantragen, damit
-              er noch rechtzeitg vor Abgabefrist per Post zugestellt werden
-              kann.
+              <strong className="hidden lg:inline">
+                Abgabefrist ist der 31.01.2023.
+              </strong>{" "}
+              Für die Option Identifikation mit Freischaltcode diesen jetzt
+              beantragen, damit er noch rechtzeitg vor Abgabefrist per Post
+              zugestellt werden kann.
             </p>
           </div>
         </ContentContainer>
