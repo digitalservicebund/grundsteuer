202f962684020b00b1bc2be8c4c3e516d9fc6bc4
Joschka de Cuveland <joschka.decuveland@digitalservice.bund.de>
2022-12-07 18:58:14 +0100

    Fix queue workers not closed in after test runs

--- private/jest/setupAfterEnv.js
+++ private/jest/setupAfterEnv.js
@@ -19,6 +19,14 @@ global.__unleash = {
 
 // eslint-disable-next-line no-undef
 afterAll(async () => {
+  if (global.__registeredQueues) {
+    const names = Object.keys(global.__registeredQueues);
+    for (const name of names) {
+      const worker = global.__registeredQueues[name].worker;
+      await worker.close();
+      console.log(`Worker ${name} stopped`);
+    }
+  }
   if (global.ioredis) {
     await global.ioredis.quit();
     global.ioredis = undefined;
