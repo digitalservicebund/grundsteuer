d80f47306da28d798079cb89e2567f90f51a3c6f
Julian Klusmann-Rösner <julian.roesner@digitalservice.bund.de>
2023-02-27 15:15:30 +0100

    Add export from v1 to auditlog scripts

--- audit-export.server.ts
+++ audit-export.server.ts
@@ -1,6 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 import fs from "fs";
 
+async function exportAuditLogsFromAuditLogV1(
+  from: string,
+  to: string,
+  pathToFile: string
+) {
+  console.log(`Exporting audit logs from ${from} to ${to}`);
+  const db = new PrismaClient();
+  const auditLogs = await db.auditLog.findMany({
+    where: {
+      createdAt: {
+        gte: new Date(from),
+        lte: new Date(to),
+      },
+    },
+  });
+  fs.writeFileSync(pathToFile, "");
+  auditLogs.forEach((log) => fs.appendFileSync(pathToFile, log.data + "\n"));
+}
+
 async function exportAuditLogs(from: string, to: string, pathToFile: string) {
   console.log(`Exporting audit logs from ${from} to ${to}`);
   const db = new PrismaClient();
@@ -21,14 +40,19 @@ function usage() {
   console.log(
     "Example: npm run audit:export 2022-05-13 2022-05-14 /tpm/encrypted_logs"
   );
+  console.log(
+    "For V2: npm run audit:export 2022-05-13 2022-05-14 /tpm/encrypted_logs v2"
+  );
 }
 
 function main() {
   const args = process.argv.slice(2);
   if (args.length < 3) {
     usage();
-  } else {
+  } else if (args.length == 4 && args[3] == "v2") {
     exportAuditLogs(args[0], args[1], args[2]);
+  } else {
+    exportAuditLogsFromAuditLogV1(args[0], args[1], args[2]);
   }
 }
 
