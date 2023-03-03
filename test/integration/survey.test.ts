bb11e98d88382eb3191aaef7b0b54af24a9e7859
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2023-03-03 10:46:26 +0100

    USEID-843: Fix test

--- test/integration/survey.test.ts
+++ test/integration/survey.test.ts
@@ -1,8 +1,10 @@
 import { createSurvey } from "~/domain/survey";
 
 describe("Survey", () => {
-  it("should not return when survey is submitted", async () => {
+  it("should return when survey is submitted", async () => {
     const newSurvey = await createSurvey("dropout", "nice survey");
-    expect(newSurvey).toBeUndefined();
+    expect(newSurvey).not.toBeUndefined();
+    expect(newSurvey.category).toEqual("dropout");
+    expect(newSurvey.content).toEqual("nice survey");
   });
 });
