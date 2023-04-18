fe1d4825d343bc3fbfed7e10c410efbec313c53a
Pram Gurusinga <pratama.gurusinga@digitalservice.bund.de>
2023-04-18 16:57:16 +0200

    Fix the bundesident url in FSC context

--- app/routes/bundesIdent/_bundesIdentUrl.ts
+++ app/routes/bundesIdent/_bundesIdentUrl.ts
@@ -2,5 +2,5 @@ import { isMobileUserAgent } from "~/util/isMobileUserAgent";
 
 export const getBundesIdentUrl = (request: Request): string => {
   const isMobile = isMobileUserAgent(request);
-  return "/bundesIdent/" + (isMobile ? "voraussetzung" : "desktop");
+  return "/bundesIdent/" + (isMobile ? "voraussetzung" : "primaryoption");
 };
